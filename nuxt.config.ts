b1b81f23940b1b2646026cfada3764837592c2c7
Jimmy Chen <jimmychen12138@gmail.com>
2022-06-28 13:24:13 +1000

    Added nuxtjs, vuetify and unocss

---
+++ nuxt.config.ts
@@ -0,0 +1,15 @@
+import { defineNuxtConfig } from "nuxt";
+
+// https://v3.nuxtjs.org/api/configuration/nuxt.config
+export default defineNuxtConfig({
+  css: ["vuetify/lib/styles/main.sass", "@mdi/font/css/materialdesignicons.min.css"],
+  build: {
+    transpile: ["vuetify"],
+  },
+  vite: {
+    define: {
+      "process.env.DEBUG": false,
+    },
+  },
+  modules: ["@unocss/nuxt"],
+});
