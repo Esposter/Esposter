bd22bebbf251eba67dcc390b856f8055b86cde8e
Jimmy Chen <jimmychen12138@gmail.com>
2022-07-07 22:28:09 +1000

    Made toolbar transparent

--- components/Chat/ChatContentHeader.vue
+++ components/Chat/ChatContentHeader.vue
@@ -5,7 +5,7 @@ const { name } = useRoomStore();
 </script>
 
 <template>
-  <v-toolbar bg="transparent" tag="div" height="56" border>
+  <v-toolbar bg="transparent!" tag="div" height="56" border>
     <v-toolbar-title font="bold!">{{ name }}</v-toolbar-title>
     <template #append>
       <v-btn icon="mdi-phone" size="small" />
