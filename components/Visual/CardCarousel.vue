381bbb4fc3cba23b221b271f422209cafdcd8163
Jimmy Chen <jimmychen12138@gmail.com>
2022-07-27 22:24:46 +1000

    Add royalt1es for big braedon

--- components/Visual/CardCarousel.vue
+++ components/Visual/CardCarousel.vue
@@ -1,3 +1,5 @@
+<!-- @BIG CREDIT TO Braedon Wooding for providing the base animation code for this AMAZING card carousel -->
+
 <script setup lang="ts">
 import BaseCard from "@/components/Visual/BaseCard.vue";
 import type { Card } from "@/components/Visual/types";
