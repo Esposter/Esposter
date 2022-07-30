895fd6d221ea3d859af586687ca44de4224fb961
Jimmy Chen <jimmychen12138@gmail.com>
2022-07-30 18:14:05 +1000

    Change to postgres

---
+++ prisma/migrations/20220730081331_new_db/migration.sql
@@ -0,0 +1,11 @@
+-- CreateTable
+CREATE TABLE "Room" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "avatar" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "deletedAt" TIMESTAMP(3),
+
+    CONSTRAINT "Room_pkey" PRIMARY KEY ("id")
+);
