4aa62fea51cb4fd9c215a20c779d9e51eeac9710
Jimmy Chen <jimmychen12138@gmail.com>
2022-08-18 18:10:01 +1000

    Fix up errors

--- services/azure/queue.ts
+++ services/azure/queue.ts
@@ -12,7 +12,8 @@ export const getQueueClient = async (queueName: AzureQueue) => {
 };
 
 export const sendMessage = async (queueClient: QueueClient, ...args: Parameters<typeof queueClient["sendMessage"]>) => {
-  const response = await queueClient.sendMessage(...args);
+  const cleanedArgs: typeof args = [Buffer.from(args[0]).toString("base64"), args[1]];
+  const response = await queueClient.sendMessage(...cleanedArgs);
   const error = response._response.status >= 400;
   if (error) console.error(`Failed to send message for queue ${queueClient.name}`);
   return !error;
