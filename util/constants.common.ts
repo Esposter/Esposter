f51138af727b7698f4006853f0df8bf2972640f9
Jimmy Chen <jimmychen12138@gmail.com>
2022-11-02 11:31:52 +1100

    refactor: move chatbot constant down

--- util/constants.common.ts
+++ util/constants.common.ts
@@ -16,9 +16,9 @@ export const FETCH_LIMIT = 20;
 export const USER_MAX_USERNAME_LENGTH = 100;
 export const ROOM_MAX_NAME_LENGTH = 100;
 export const MESSAGE_MAX_LENGTH = 100;
-export const CHATBOT_PROMPT_MAX_LENGTH = 100;
 export const POST_MAX_TITLE_LENGTH = 300;
 export const POST_MAX_DESCRIPTION_LENGTH = 1000;
+export const CHATBOT_PROMPT_MAX_LENGTH = 100;
 
 export const AZURE_BLOB_URL_PROD = "https://pshpstespauea001.blob.core.windows.net";
 export const AZURE_BLOB_URL_DEV = "https://dshpstespauea001.blob.core.windows.net";
