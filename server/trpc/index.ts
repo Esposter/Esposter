9a0f1f6007167e792363e8562584ef8e135fb835
jimmy chen <jimmychen12138@hotmail.com>
2022-11-14 12:36:50 +1100

    feat: Add error formatting + package nuxt prepare after install

--- server/trpc/index.ts
+++ server/trpc/index.ts
@@ -1,10 +1,21 @@
 import { initTRPC } from "@trpc/server";
 import superjson from "superjson";
+import { ZodError } from "zod";
+import type { Context } from "@/server/trpc/context";
 
 // Avoid exporting the entire t-object since it's not very
 // descriptive and can be confusing to newcomers used to t
 // meaning translation in i18n libraries
-const t = initTRPC.create({ transformer: superjson });
+const t = initTRPC.context<Context>().create({
+  transformer: superjson,
+  errorFormatter: ({ shape, error }) => ({
+    ...shape,
+    data: {
+      ...shape.data,
+      zodError: error.code === "BAD_REQUEST" && error.cause instanceof ZodError ? error.cause?.flatten() : null,
+    },
+  }),
+});
 
 export const middleware = t.middleware;
 export const router = t.router;
