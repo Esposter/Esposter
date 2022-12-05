0bbbd00235f580f396516cc5152db8f4aa19a529
Jimmy Chen <jimmychen12138@gmail.com>
2022-12-05 12:56:46 +1100

    feat: Apply upgrades to cursor

--- store/clicker/useCursorStore.ts
+++ store/clicker/useCursorStore.ts
@@ -1,7 +1,15 @@
+import { useGameStore } from "@/store/clicker/useGameStore";
 import { defineStore } from "pinia";
 
 export const useCursorStore = defineStore("clicker/cursor", () => {
+  const gameStore = useGameStore();
   const baseCursorPower = ref(1);
-  const cursorPower = computed(() => baseCursorPower.value);
+  const cursorPower = computed(() => {
+    if (!gameStore.game) return;
+    let resultPower = baseCursorPower.value;
+    const multiplicativeUpgrades = gameStore.game.boughtUpgradeList ?? [];
+    for (const multiplicativeUpgrade of multiplicativeUpgrades) resultPower *= multiplicativeUpgrade.value;
+    return resultPower;
+  });
   return { cursorPower };
 });
