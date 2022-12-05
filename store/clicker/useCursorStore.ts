889c0ce4abd2cb770f5e9bf17d80596298b1c925
Jimmy Chen <jimmychen12138@gmail.com>
2022-12-05 20:36:10 +1100

    fix: type errors

--- store/clicker/useCursorStore.ts
+++ store/clicker/useCursorStore.ts
@@ -5,8 +5,9 @@ export const useCursorStore = defineStore("clicker/cursor", () => {
   const gameStore = useGameStore();
   const baseCursorPower = ref(1);
   const cursorPower = computed(() => {
-    if (!gameStore.game) return;
     let resultPower = baseCursorPower.value;
+
+    if (!gameStore.game) return resultPower;
     const multiplicativeUpgrades = gameStore.game.boughtUpgradeList ?? [];
     for (const multiplicativeUpgrade of multiplicativeUpgrades) resultPower *= multiplicativeUpgrade.value;
     return resultPower;
