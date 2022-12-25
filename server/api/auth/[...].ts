e61708b8bc0ab15842b86095401316030d70da0c
Jimmy Chen <jimmychen12138@gmail.com>
2022-12-25 18:26:51 +1100

    fix: google client secret

--- server/api/auth/[...].ts
+++ server/api/auth/[...].ts
@@ -32,7 +32,7 @@ export default NuxtAuthHandler({
     }),
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID,
-      clientSecret: process.env.GITHUB_CLIENT_SECRET,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }),
   ],
   pages: {
