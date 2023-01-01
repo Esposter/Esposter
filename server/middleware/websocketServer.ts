1e716c9d3328a7c29908eeec7ee4f23550f83e8c
Jimmy Chen <jimmychen12138@gmail.com>
2023-01-01 11:56:49 +1100

    refactor: made messages more informative

--- server/middleware/websocketServer.ts
+++ server/middleware/websocketServer.ts
@@ -22,8 +22,8 @@ export default defineEventHandler(() => {
   const handler = applyWSSHandler({ wss, router: appRouter, createContext });
 
   wss.on("connection", (ws) => {
-    console.log(`Connection opened (${wss.clients.size})`);
-    ws.once("close", () => console.log(`Connection closed (${wss.clients.size})`));
+    console.log(`Connection opened, client size: ${wss.clients.size}`);
+    ws.once("close", () => console.log(`Connection closed, client size: ${wss.clients.size}`));
   });
   process.on("SIGTERM", () => {
     handler.broadcastReconnectNotification();
