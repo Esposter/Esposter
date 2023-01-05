e136cc09ddec180c5cdce4c2f4ba15e0fe684c16
Jimmy Chen <jimmyc@resonatesolutions.com.au>
2023-01-05 14:53:59 +1100

    fix: type errors

--- store/clicker/useMouseStore.ts
+++ store/clicker/useMouseStore.ts
@@ -4,6 +4,8 @@ import { defineStore } from "pinia";
 
 export const useMouseStore = defineStore("clicker/mouse", () => {
   const gameStore = useGameStore();
-  const mousePower = computed(() => applyMouseUpgrades(1, gameStore.boughtUpgrades, gameStore.boughtBuildings));
+  const mousePower = computed(() =>
+    applyMouseUpgrades(1, gameStore.game.boughtUpgrades, gameStore.game.boughtBuildings)
+  );
   return { mousePower };
 });
