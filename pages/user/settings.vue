56369aac60ce994c03ea8bcea319e50b2581402a
Jimmy Chen <jimmychen12138@gmail.com>
2023-01-08 22:13:24 +1100

    fix: make user profile card fill page on mobile

--- pages/user/settings.vue
+++ pages/user/settings.vue
@@ -1,12 +1,16 @@
 <script setup lang="ts">
+import { useDisplay } from "vuetify";
+
 definePageMeta({ middleware: "auth" });
+
+const { mobile } = $(useDisplay());
 </script>
 
 <template>
   <NuxtLayout>
     <v-container>
       <v-row>
-        <v-col cols="8">
+        <v-col :cols="mobile ? 12 : 8">
           <UserProfileCard />
         </v-col>
       </v-row>
