6b6b6ff0bf4f7d3896674f2a80fbab7e5ccc3cdd
Jimmy Chen <jimmychen12138@gmail.com>
2023-02-13 21:08:34 +1100

    fix: add reaction list

---
+++ components/Chat/Model/Message/ReactionList.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import { useEmojiStore } from "@/store/chat/useEmojiStore";
+
+interface MessageReactionListProps {
+  messageRowKey: string;
+}
+
+const props = defineProps<MessageReactionListProps>();
+const { messageRowKey } = toRefs(props);
+const emojiStore = useEmojiStore();
+const { getEmojiList } = emojiStore;
+const emojis = computed(() => getEmojiList(messageRowKey.value));
+</script>
+
+<template>
+  <v-list>
+    <v-list-item v-for="emoji in emojis" :key="emoji.rowKey">
+      {{ emoji }}
+    </v-list-item>
+  </v-list>
+</template>
