a492ad9bdfbd744f56dc537fb5dbdba7e094b703
Jimmy Chen <jimmychen12138@gmail.com>
2023-03-08 23:34:15 +1100

    fix: Add files

---
+++ utils/json.ts
@@ -0,0 +1,22 @@
+const reISO = /^(\d{4})-(\d{2})-(\d{2})T(\d{2}):(\d{2}):(\d{2}(?:\.{0,1}\d*))(?:Z|(\+|-)([\d|:]*))?$/;
+const reMsAjax = /^\/Date\((d|-|.*)\)[/|\\]$/;
+
+export const jsonDateParser = (_: string, value: any) => {
+  let parsedValue = value;
+
+  if (typeof value === "string") {
+    let a = reISO.exec(value);
+
+    if (a) parsedValue = new Date(value);
+    else {
+      a = reMsAjax.exec(value);
+
+      if (a) {
+        const b = a[1].split(/[-+,.]/);
+        parsedValue = new Date(b[0] ? +b[0] : 0 - +b[1]);
+      }
+    }
+  }
+
+  return parsedValue;
+};
