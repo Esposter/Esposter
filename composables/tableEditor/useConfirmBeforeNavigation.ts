78367134e8efe7fc7d577e9f7b1677f20c266570
Jimmy Chen <jimmyc@resonatesolutions.com.au>
2023-04-28 14:55:56 +1000

    fix: route

--- composables/tableEditor/useConfirmBeforeNavigation.ts
+++ composables/tableEditor/useConfirmBeforeNavigation.ts
@@ -4,9 +4,8 @@ export const useConfirmBeforeNavigation = () => {
   const tableEditorStore = useTableEditorStore()();
   const { isSavable } = storeToRefs(tableEditorStore);
 
-  onBeforeRouteLeave((_, __, next) => {
+  onBeforeRouteLeave(() => {
     if (isSavable.value && !window.confirm("Changes that you made may not be saved.")) return false;
-    next();
   });
 
   const refreshListener = (e: BeforeUnloadEvent) => {
