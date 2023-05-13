1f607afa994a9eb8548ddd60bbce686c6f2fe536
Q16solver <jimmychen12138@gmail.com>
2023-05-13 22:37:52 +1000

    refactor: fix unnecessary type error

--- models/tableEditor/vuetifyComponent/VuetifyComponentType.ts
+++ models/tableEditor/vuetifyComponent/VuetifyComponentType.ts
@@ -1,8 +1,7 @@
 import { VuetifyComponentMap } from "@/services/tableEditor/vuetifyComponent/constants";
 
+export type VuetifyComponentType = { [Property in keyof VuetifyComponentMap]: Property };
 export const VuetifyComponentType = Object.keys(VuetifyComponentMap).reduce<Record<string, string>>((acc, curr) => {
   acc[curr] = curr;
   return acc;
-  // eslint-disable-next-line no-use-before-define
-}, {}) as { [Property in keyof VuetifyComponentMap]: Property };
-export type VuetifyComponentType = typeof VuetifyComponentType;
+}, {}) as VuetifyComponentType;
