68e8c39861988a3725108bbf86bbb2df1ff26bdc
Jimmy Chen <jimmychen12138@gmail.com>
2023-05-21 19:50:22 +1000

    feat: Add validation for max items

--- models/tableEditor/TableEditor.ts
+++ models/tableEditor/TableEditor.ts
@@ -1,4 +1,5 @@
 import type { Item } from "@/models/tableEditor/Item";
+import { TABLE_EDITOR_ITEMS_MAX_LENGTH } from "@/utils/validation";
 import { z } from "zod";
 
 export class TableEditor<T extends Item> {
@@ -6,4 +7,4 @@ export class TableEditor<T extends Item> {
 }
 
 export const createTableEditorSchema = <T extends z.ZodTypeAny = z.ZodType<Item>>(schema: T) =>
-  z.object({ items: z.array(schema) });
+  z.object({ items: z.array(schema).max(TABLE_EDITOR_ITEMS_MAX_LENGTH) });
