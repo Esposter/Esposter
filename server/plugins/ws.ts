5ed0f9455255eb1abc2e4ffd43d385af05a23045
Jimmy Chen <jimmyc@resonatesolutions.com.au>
2023-05-22 13:12:07 +1000

    feat: Use official solution to create ws

---
+++ server/plugins/ws.ts
@@ -0,0 +1,23 @@
+import { createContext } from "@/server/trpc/context";
+import { appRouter } from "@/server/trpc/routers";
+import { applyWSSHandler } from "@trpc/server/adapters/ws";
+import chalk from "chalk";
+import WebSocket, { WebSocketServer as WSWebSocketServer } from "ws";
+
+export default defineNitroPlugin((nitro) => {
+  const WebSocketServer = WebSocket.Server || WSWebSocketServer;
+  const wss = new WebSocketServer();
+  const handler = applyWSSHandler({ wss, router: appRouter, createContext });
+
+  wss.on("connection", (ws) => {
+    console.log(`Connection opened, client size: ${wss.clients.size}`);
+    ws.once("close", () => console.log(`Connection closed, client size: ${wss.clients.size}`));
+  });
+
+  console.log(chalk.yellow("WebSocket Server is listening"));
+
+  nitro.hooks.hookOnce("close", () => {
+    handler.broadcastReconnectNotification();
+    wss.close();
+  });
+});
