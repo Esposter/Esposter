936077e3b56f599500dd380848c23bf2af4eda23
Jimmy Chen <jimmyc@resonatesolutions.com.au>
2023-05-23 13:50:45 +1000

    fix: not sortable

--- services/tableEditor/headers.ts
+++ services/tableEditor/headers.ts
@@ -1,6 +1,6 @@
 import type { DataTableHeader } from "@/services/vuetify/dataTable";
 
 export const tableEditorHeaders: DataTableHeader[] = [
-  { title: "", key: "type", width: 0 },
+  { title: "", key: "type", width: 0, sortable: false },
   { title: "Name", key: "name" },
 ];
