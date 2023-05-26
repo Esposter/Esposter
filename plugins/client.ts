626bd15a1a35bdfc54aa51f426252eb29a1b7874
Jimmy Chen <jimmyc@resonatesolutions.com.au>
2023-05-26 16:06:39 +1000

    fix: plugin

--- plugins/client.ts
+++ plugins/client.ts
@@ -1,11 +1,11 @@
 import type { AppRouter } from "@/server/trpc/routers";
 import { WS_PORT } from "@/services/trpc/constants";
 import { errorLink } from "@/services/trpc/errorLink";
-import type { TRPCLink } from "@trpc/client";
+import type { TRPCLink, WebSocketClientOptions } from "@trpc/client";
 import { createTRPCProxyClient, createWSClient, httpBatchLink, loggerLink, splitLink, wsLink } from "@trpc/client";
 import SuperJSON from "superjson";
 
-export default defineNuxtPlugin(() => {
+export default defineNuxtPlugin(async () => {
   const url = useClientUrl();
   const links: TRPCLink<AppRouter>[] = [
     // Log to your console in development and only log errors in production
@@ -15,11 +15,16 @@ export default defineNuxtPlugin(() => {
     errorLink,
     splitLink({
       condition: (op) => op.type === "subscription",
-      true: (() => {
+      true: await (async () => {
         if (isServer()) return httpBatchLink({ url });
 
         const wsProtocol = window.location.protocol === "https:" ? "wss:" : "ws:";
-        const wsClient = createWSClient({ url: `${wsProtocol}//${window.location.host}:${WS_PORT}` });
+        const wsClient = createWSClient({
+          url: `${wsProtocol}//${window.location.host}:${WS_PORT}`,
+          WebSocket: (isServer()
+            ? await import("ws").then((r) => r.default)
+            : WebSocket) as WebSocketClientOptions["WebSocket"],
+        });
         return wsLink({ client: wsClient });
       })(),
       false: httpBatchLink({ url }),
