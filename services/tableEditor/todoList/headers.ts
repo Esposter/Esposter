8b791279cee1079ac3480262c110a661279fc083
Jimmy Chen <jimmyc@resonatesolutions.com.au>
2023-08-01 12:57:47 +1000

    fix: make notes not sortable

--- services/tableEditor/todoList/headers.ts
+++ services/tableEditor/todoList/headers.ts
@@ -1,4 +1,7 @@
 import { tableEditorHeaders } from "@/services/tableEditor/headers";
 import type { DataTableHeader } from "@/services/vuetify/dataTable";
 
-export const todoListHeaders: DataTableHeader[] = [...tableEditorHeaders, { title: "Notes", key: "notes" }];
+export const todoListHeaders: DataTableHeader[] = [
+  ...tableEditorHeaders,
+  { title: "Notes", key: "notes", sortable: false },
+];
