a8fae7eb0e006e73f1c12ceba451d900847bb1e9
Q16solver <jimmychen12138@gmail.com>
2023-08-08 12:54:14 +1000

    refactor: create + survey dialog buttons

--- components/Surveyer/CrudView.vue
+++ components/Surveyer/CrudView.vue
@@ -8,7 +8,6 @@ import dayjs from "dayjs";
 type SurveyItem = { raw: Survey };
 
 const surveyerStore = useSurveyerStore();
-const { deleteSurvey } = surveyerStore;
 const { surveyerConfiguration, searchQuery } = storeToRefs(surveyerStore);
 </script>
 
@@ -36,18 +35,7 @@ const { surveyerConfiguration, searchQuery } = storeToRefs(surveyerStore);
       </template>
       <template #[`item.actions`]="{ item }">
         <SurveyerChangeGroupDialogButton :survey="(item as SurveyItem).raw" />
-        <StyledConfirmDeleteDialogButton
-          :card-props="{
-            title: 'Delete Survey',
-            text: 'Are you sure you want to delete this survey?',
-          }"
-          @delete="
-            (onComplete) => {
-              deleteSurvey((item as SurveyItem).raw.id);
-              onComplete();
-            }
-          "
-        />
+        <SurveyerDeleteSurveyDialogButton :survey-id="(item as SurveyItem).raw.id" />
       </template>
     </StyledDataTable>
   </v-container>
---
+++ components/Surveyer/DeleteSurveyDialogButton.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import { useSurveyerStore } from "@/store/surveyer";
+
+interface SurveyerDeleteSurveyButtonDialogProps {
+  surveyId: string;
+}
+
+const props = defineProps<SurveyerDeleteSurveyButtonDialogProps>();
+const { surveyId } = toRefs(props);
+const surveyerStore = useSurveyerStore();
+const { deleteSurvey } = surveyerStore;
+</script>
+
+<template>
+  <StyledConfirmDeleteDialogButton
+    :card-props="{
+      title: 'Delete Survey',
+      text: 'Are you sure you want to delete this survey?',
+    }"
+    @delete="
+      (onComplete) => {
+        deleteSurvey(surveyId);
+        onComplete();
+      }
+    "
+  />
+</template>
