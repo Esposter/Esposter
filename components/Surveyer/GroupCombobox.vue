2b2daaeb1efa918f6af490077e841531fb50a3f0
Q16solver <jimmychen12138@gmail.com>
2023-08-08 13:23:59 +1000

    fix: changing group

--- components/Surveyer/GroupCombobox.vue
+++ components/Surveyer/GroupCombobox.vue
@@ -10,7 +10,13 @@ const modelValue = defineModel<string | null>();
 const { autocompleteProps } = defineProps<GroupAutocompleteProps>();
 const surveyerStore = useSurveyerStore();
 const { surveyerConfiguration } = storeToRefs(surveyerStore);
-const groups = computed(() => (surveyerConfiguration.value ? surveyerConfiguration.value.map((s) => s.group) : []));
+const groups = computed(() => {
+  if (!surveyerConfiguration.value) return [];
+
+  const results = new Set<string>();
+  for (const { group } of surveyerConfiguration.value) if (group) results.add(group);
+  return [...results];
+});
 </script>
 
 <template>
