2609095fee40beb7adffe00c4c5c53917f0c2ff6
Q16solver <jimmychen12138@gmail.com>
2023-09-02 22:15:18 +1000

    chore: Add github issue for comments for fixing db relational type issues

--- prisma/types.ts
+++ prisma/types.ts
@@ -1,5 +1,5 @@
 import type { Prisma } from "@prisma/client";
 
 export const PostRelationsIncludeDefault = { creator: true, likes: true } satisfies Prisma.PostInclude;
-
+// @NOTE: https://github.com/drizzle-team/drizzle-orm/issues/695
 export type PostWithRelations = Prisma.PostGetPayload<{ include: typeof PostRelationsIncludeDefault }>;
