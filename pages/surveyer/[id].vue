4680577ec1a9bf22f1e8bb62de80cb1d11d11cf2
Jimmy Chen <jimmyc@resonatesolutions.com.au>
2023-09-07 09:39:01 +1000

    fix: remove comment for surveyer + update dependencies

--- pages/surveyer/[id].vue
+++ pages/surveyer/[id].vue
@@ -1,52 +1,49 @@
 <script setup lang="ts">
-// @NOTE: Issue with survey-creator-core not building properly
-// with Nuxt atm
-// import { useSurveyStore } from "@/store/surveyer/survey";
-// import "survey-core/defaultV2.min.css";
-// import "survey-creator-core/survey-creator-core.min.css";
-// import { SurveyCreator } from "survey-creator-knockout";
+import { useSurveyStore } from "@/store/surveyer/survey";
+import "survey-core/defaultV2.min.css";
+import "survey-creator-core/survey-creator-core.min.css";
+import { SurveyCreator } from "survey-creator-knockout";
 
-// const readMoreSurveys = await useReadSurveys();
-// const { status } = useAuth();
-// const route = useRoute();
-// const surveyerStore = useSurveyStore();
-// const { updateSurvey, unauthedSave } = surveyerStore;
-// const { surveyList } = storeToRefs(surveyerStore);
-// const surveyCreatorId = "surveyCreator";
+await useReadSurveys();
+const { status } = useAuth();
+const route = useRoute();
+const surveyerStore = useSurveyStore();
+const { updateSurvey, unauthedSave } = surveyerStore;
+const { surveyList } = storeToRefs(surveyerStore);
+const surveyCreatorId = "surveyCreator";
 
-// onMounted(() => {
-//   if (!(typeof route.params.id === "string" && uuidValidateV4(route.params.id)))
-//     throw createError({ statusCode: 404, statusMessage: "Survey id is invalid" });
+onMounted(() => {
+  if (!(typeof route.params.id === "string" && uuidValidateV4(route.params.id)))
+    throw createError({ statusCode: 404, statusMessage: "Survey id is invalid" });
 
-//   const creator = new SurveyCreator({
-//     showLogicTab: true,
-//     isAutoSave: true,
-//   });
-//   const survey = surveyList.value.find((s) => s.rowKey === route.params.id);
-//   if (!survey) throw createError({ statusCode: 404, statusMessage: "Survey could not be found" });
+  const creator = new SurveyCreator({
+    showLogicTab: true,
+    isAutoSave: true,
+  });
+  const survey = surveyList.value.find((s) => s.rowKey === route.params.id);
+  if (!survey) throw createError({ statusCode: 404, statusMessage: "Survey could not be found" });
 
-//   creator.text = survey.model;
-//   creator.saveSurveyFunc = async (saveNo: number, callback: Function) => {
-//     survey.model = creator.text;
-//     survey.updatedAt = new Date();
+  creator.text = survey.model;
+  creator.saveSurveyFunc = async (saveNo: number, callback: Function) => {
+    survey.model = creator.text;
+    survey.updatedAt = new Date();
 
-//     if (status.value === "authenticated") {
-//       try {
-//         await updateSurvey(survey);
-//         callback(saveNo, true);
-//       } catch {
-//         callback(saveNo, false);
-//       }
-//     } else callback(saveNo, unauthedSave(survey));
-//   };
-//   creator.render(surveyCreatorId);
-// });
-throw createError({ statusCode: 404 });
+    if (status.value === "authenticated") {
+      try {
+        await updateSurvey(survey);
+        callback(saveNo, true);
+      } catch {
+        callback(saveNo, false);
+      }
+    } else callback(saveNo, unauthedSave(survey));
+  };
+  creator.render(surveyCreatorId);
+});
 </script>
 
 <template>
   <NuxtLayout>
-    <!-- <div :id="surveyCreatorId" /> -->
+    <div :id="surveyCreatorId" />
   </NuxtLayout>
 </template>
 
