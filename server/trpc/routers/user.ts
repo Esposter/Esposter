26a8bb59bece671fc1572bfce1c6c6e402e82d47
Q16solver <jimmychen12138@gmail.com>
2023-09-14 23:06:23 +1000

    fix: user query

--- server/trpc/routers/user.ts
+++ server/trpc/routers/user.ts
@@ -12,9 +12,19 @@ export const userRouter = router({
   readUser: authedProcedure.input(readUserInputSchema).query(
     ({ input, ctx }) =>
       db.query.users.findFirst({
+        // We'll assume no input as user reading their own info
         where: (users, { eq }) => eq(users.id, input ?? ctx.session.user.id),
-        // Exclude email unless it's their own user reading it
-        columns: { email: input === ctx.session.user.id },
+        columns: {
+          id: true,
+          name: true,
+          // Exclude email unless it's their own user reading it
+          email: !input || input === ctx.session.user.id,
+          emailVerified: true,
+          image: true,
+          createdAt: true,
+          updatedAt: true,
+          deletedAt: true,
+        },
         // @NOTE: https://github.com/drizzle-team/drizzle-orm/issues/1163
       }) as Promise<User | undefined>,
   ),
