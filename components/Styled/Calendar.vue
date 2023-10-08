2a6f8743a5ddf1bdd8eb8d61671aca94452c3e2a
Q16solver <jimmychen12138@gmail.com>
2023-10-08 14:53:28 +1100

    fix: styles

--- components/Styled/Calendar.vue
+++ components/Styled/Calendar.vue
@@ -28,7 +28,7 @@ const { calendarOptions } = defineProps<StyledCalendarProps>();
     <template #eventContent="{ event, timeText }">
       <v-tooltip>
         <template #activator="{ props }">
-          <div flex items-center overflow-hidden :="props">
+          <div w-full flex items-center overflow-hidden :="props">
             <div class="fc-daygrid-event-dot" />
             <div class="fc-event-time">{{ timeText }}</div>
             <div class="fc-event-title">{{ event.title }}</div>
