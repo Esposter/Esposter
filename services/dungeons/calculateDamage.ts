2d71c3daee1267167b356a841f34d7ef2f7478e8
Q16solver <jimmychen12138@gmail.com>
2023-10-20 19:50:52 +1100

    fix: Add lower bound

--- services/dungeons/calculateDamage.ts
+++ services/dungeons/calculateDamage.ts
@@ -1,4 +1,4 @@
 import { randomNumber } from "@/services/math/randomNumber";
 
 export const calculateDamage = (attack: number, armor: number) =>
-  Math.ceil(randomNumber(0.85, 1.01) * (attack - armor));
+  Math.ceil(randomNumber(0.85, 1.01) * Math.min(attack - armor, 0));
