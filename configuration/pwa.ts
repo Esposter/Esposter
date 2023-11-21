ca6e828cdb7c514c5852df3c19583d005a111da5
Jimmy Chen <jimmyc@resonatesolutions.com.au>
2023-11-21 16:40:28 +1100

    fix: package & pwa manifest config

---
+++ configuration/pwa.ts
@@ -0,0 +1,28 @@
+import type { NuxtConfig } from "nuxt/schema";
+
+export const pwa: NuxtConfig["pwa"] = {
+  manifest: {
+    name: "Esposter",
+    short_name: "Esposter",
+    description: "A nice and casual place for posting random things.",
+    icons: [
+      {
+        src: "/icons/manifest-icon-192.png",
+        sizes: "192x192",
+        type: "image/png",
+        purpose: "maskable any",
+      },
+      {
+        src: "/icons/manifest-icon-512.png",
+        sizes: "512x512",
+        type: "image/png",
+        purpose: "maskable any",
+      },
+    ],
+    theme_color: "#fff",
+    background_color: "#fff",
+    start_url: "/",
+    display: "standalone",
+    orientation: "portrait",
+  },
+};
