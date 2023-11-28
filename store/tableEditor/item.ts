c4767819c0fd3a55c899b477a634ec3557fa4e9f
Jimmy Chen <jimmyc@resonatesolutions.com.au>
2023-11-28 15:42:41 +1100

    refactor: item

--- store/tableEditor/item.ts
+++ store/tableEditor/item.ts
@@ -3,26 +3,27 @@ import { useTableEditorStore } from "@/store/tableEditor";
 
 export const useItemStore = defineStore("tableEditor/item", () => {
   const tableEditorStore = useTableEditorStore()();
+  const { tableEditor } = storeToRefs(tableEditorStore);
 
   const pushItemList = (items: Item[]) => {
-    tableEditorStore.tableEditor.items.push(...items);
+    tableEditor.value.items.push(...items);
   };
   const initialiseItemList = (items: Item[]) => {
-    tableEditorStore.tableEditor.items = items;
+    tableEditor.value.items = items;
   };
   const createItem = (newItem: Item) => {
-    tableEditorStore.tableEditor.items.push(newItem);
+    tableEditor.value.items.push(newItem);
   };
   const updateItem = (updatedItem: Item) => {
-    const index = tableEditorStore.tableEditor.items.findIndex((r) => r.id === updatedItem.id);
+    const index = tableEditor.value.items.findIndex((r) => r.id === updatedItem.id);
     if (index > -1)
-      tableEditorStore.tableEditor.items[index] = {
-        ...tableEditorStore.tableEditor.items[index],
+      tableEditor.value.items[index] = {
+        ...tableEditor.value.items[index],
         ...updatedItem,
       };
   };
   const deleteItem = (id: string) => {
-    tableEditorStore.tableEditor.items = tableEditorStore.tableEditor.items.filter((r) => r.id !== id);
+    tableEditor.value.items = tableEditor.value.items.filter((r) => r.id !== id);
   };
 
   return {
