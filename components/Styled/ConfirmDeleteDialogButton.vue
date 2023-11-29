af2011fa52c96f27f310db53f99015c3d73d4fd4
Q16solver <jimmychen12138@gmail.com>
2023-11-29 17:34:38 +1100

    chore: Update dependencies

--- components/Styled/ConfirmDeleteDialogButton.vue
+++ components/Styled/ConfirmDeleteDialogButton.vue
@@ -8,7 +8,7 @@ const emit = defineEmits<{ delete: [onComplete: () => void] }>();
 <template>
   <StyledDeleteDialog :card-props="cardProps" @delete="(onComplete) => emit('delete', onComplete)">
     <template #activator="{ updateIsOpen }">
-      <v-tooltip :text="cardProps?.title">
+      <v-tooltip :text="cardProps?.title?.toString()">
         <template #activator="{ props: tooltipProps }">
           <v-btn m-0="!" rd-0="!" icon="mdi-delete" size="small" :="tooltipProps" @click.stop="updateIsOpen(true)" />
         </template>
