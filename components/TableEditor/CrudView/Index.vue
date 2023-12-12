31e6aee141c71937d9dada4aedd4b2d8493d2a55
Jimmy Chen <jimmyc@resonatesolutions.com.au>
2023-12-12 11:32:03 +1100

    fix: tableeditor doesn't need server data table

--- components/TableEditor/CrudView/Index.vue
+++ components/TableEditor/CrudView/Index.vue
@@ -2,7 +2,6 @@
 import { defaultPropsMap } from "@/services/tableEditor/defaultPropsMap";
 import { useTableEditorStore } from "@/store/tableEditor";
 
-const { status } = useAuth();
 const tableEditorStore = useTableEditorStore()();
 const { editItem } = tableEditorStore;
 const { tableEditor, tableEditorType, searchQuery } = storeToRefs(tableEditorStore);
@@ -13,28 +12,7 @@ const itemTypeKey = computed(() => props.value.headers[0].key);
 
 <template>
   <v-container h-full flex flex-col fluid>
-    <StyledDataTableServer
-      v-if="status === 'authenticated'"
-      flex
-      flex-1
-      flex-col
-      height="100%"
-      :headers="props.headers"
-      :items="tableEditor.items"
-      :items-length="tableEditor.items.length"
-      :search="searchQuery"
-      :sort-by="[{ key: 'name', order: 'asc' }]"
-      @click:row="(_, { item }) => editItem(item.id)"
-    >
-      <template #top>
-        <TableEditorCrudViewTopSlot />
-      </template>
-      <template #[`item.${itemTypeKey}`]="{ item }">
-        <TableEditorCrudViewItemSlot :item="item" />
-      </template>
-    </StyledDataTableServer>
     <StyledDataTable
-      v-else-if="status === 'unauthenticated'"
       flex
       flex-1
       flex-col
