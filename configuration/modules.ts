f78148b4757d5961080b819c6bec402a72a5c9da
Jimmy Chen <jimmyc@resonatesolutions.com.au>
2023-12-13 11:19:15 +1100

    fix: migrate nuxt-vitest to nuxt test utils

--- configuration/modules.ts
+++ configuration/modules.ts
@@ -3,10 +3,10 @@ import type { NuxtConfig } from "nuxt/schema";
 export const modules: NuxtConfig["modules"] = [
   "@hebilicious/authjs-nuxt",
   "@nuxtjs/google-fonts",
+  "@nuxt/test-utils/module",
   "@pinia/nuxt",
   "@unocss/nuxt",
   "@vite-pwa/nuxt",
   "@vueuse/nuxt",
-  "nuxt-vitest",
   "vuetify-nuxt-module",
 ];
