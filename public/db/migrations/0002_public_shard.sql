ea4a8b57ac38da71062a32d3129fd31db427244a
Q16solver <jimmychen12138@gmail.com>
2023-12-13 20:17:57 +1100

    fix: migrations

--- public/db/migrations/0002_public_shard.sql
+++ public/db/migrations/0002_public_shard.sql
@@ -1,2 +1,15 @@
-ALTER TABLE "Survey" ALTER COLUMN "id" SET DATA TYPE serial;--> statement-breakpoint
-ALTER TABLE "Survey" ALTER COLUMN "id" DROP DEFAULT;
+DROP TABLE "Survey";
+
+CREATE TABLE IF NOT EXISTS "Survey" (
+	"createdAt" timestamp DEFAULT now() NOT NULL,
+	"updatedAt" timestamp DEFAULT now() NOT NULL,
+	"deletedAt" timestamp,
+	"id" serial PRIMARY KEY NOT NULL,
+	"creatorId" uuid NOT NULL,
+	"name" text DEFAULT '' NOT NULL,
+	"group" text,
+	"model" text DEFAULT '' NOT NULL,
+	"modelVersion" integer DEFAULT 0 NOT NULL,
+	"publishVersion" integer DEFAULT 0 NOT NULL,
+	"publishedAt" timestamp
+);
