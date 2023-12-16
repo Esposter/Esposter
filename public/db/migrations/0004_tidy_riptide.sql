906b4c2877361bce3d91f408ea4ba26856c7e154
Q16solver <jimmychen12138@gmail.com>
2023-12-17 10:26:31 +1100

    chore: Update dependencies

--- public/db/migrations/0004_tidy_riptide.sql
+++ public/db/migrations/0004_tidy_riptide.sql
@@ -1,2 +1,3 @@
 ALTER TABLE "Post" ALTER COLUMN "parentId" SET DATA TYPE uuid USING (gen_random_uuid());--> statement-breakpoint
+UPDATE "Post" SET "parentId" = NULL;--> statement-breakpoint
 ALTER TABLE "Survey" ALTER COLUMN "name" DROP DEFAULT;
