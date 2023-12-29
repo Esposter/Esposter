88546311a2b6cb6bb47183edfc514ef8fb08ffb7
Q16solver <jimmychen12138@gmail.com>
2023-12-29 23:44:30 +1100

    refactor: abstract crud like logic

--- store/post/comment/like.ts
+++ store/post/comment/like.ts
@@ -1,56 +1,10 @@
-import { type CreateLikeInput, type DeleteLikeInput, type UpdateLikeInput } from "@/server/trpc/routers/like";
+import { createCrudLike } from "@/services/post/createCrudLike";
 import { useCommentStore } from "@/store/post/comment";
 
 export const useCommentLikeStore = defineStore("post/comment/like", () => {
-  const { $client } = useNuxtApp();
-  const { session } = useAuth();
   const commentStore = useCommentStore();
   const { currentPost, commentList } = storeToRefs(commentStore);
   // We need to track all posts/comments to avoid missing out on updating them on the UI
   const allPosts = computed(() => (currentPost.value ? [currentPost.value, ...commentList.value] : commentList.value));
-
-  const createLike = async (input: CreateLikeInput) => {
-    const newLike = await $client.like.createLike.mutate(input);
-    if (!newLike) return;
-
-    const post = allPosts.value.find((p) => p.id === newLike.postId);
-    if (!post) return;
-
-    post.likes.push(newLike);
-    post.noLikes += newLike.value;
-  };
-  const updateLike = async (input: UpdateLikeInput) => {
-    const updatedLike = await $client.like.updateLike.mutate(input);
-    if (!updatedLike) return;
-
-    const post = allPosts.value.find((p) => p.id === updatedLike.postId);
-    if (!post) return;
-
-    const index = post.likes.findIndex((l) => l.userId === updatedLike.userId && l.postId === updatedLike.postId);
-    if (index > -1) {
-      post.likes[index] = { ...post.likes[index], ...updatedLike };
-      post.noLikes += 2 * updatedLike.value;
-    }
-  };
-  const deleteLike = async (postId: DeleteLikeInput) => {
-    const userId = session.value?.user.id;
-    if (!userId) return;
-
-    await $client.like.deleteLike.mutate(postId);
-
-    const post = allPosts.value.find((p) => p.id === postId);
-    if (!post) return;
-
-    const deletedLike = post.likes.find((l) => l.userId === userId && l.postId === postId);
-    if (!deletedLike) return;
-
-    post.likes = post.likes.filter((l) => !(l.userId === deletedLike.userId && l.postId === deletedLike.postId));
-    post.noLikes -= deletedLike.value;
-  };
-
-  return {
-    createLike,
-    updateLike,
-    deleteLike,
-  };
+  return createCrudLike(allPosts);
 });
