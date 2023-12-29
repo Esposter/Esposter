f728468fb3c060ce9726928770c1f3bda1e39d4e
Q16solver <jimmychen12138@gmail.com>
2023-12-29 23:37:47 +1100

    fix: comment out surveyjs for now

--- pages/surveyer/[id].vue
+++ pages/surveyer/[id].vue
@@ -1,35 +1,35 @@
 <script setup lang="ts">
 // @TODO: Nuxt cannot build survey-creator-core yet
-import { useSurveyStore } from "@/store/surveyer/survey";
-import "survey-core/defaultV2.min.css";
-import "survey-core/survey.i18n.js";
-import { SurveyCreatorModel } from "survey-creator-core";
-import "survey-creator-core/survey-creator-core.i18n.js";
-import "survey-creator-core/survey-creator-core.min.css";
+// import { useSurveyStore } from "@/store/surveyer/survey";
+// import "survey-core/defaultV2.min.css";
+// import "survey-core/survey.i18n.js";
+// import { SurveyCreatorModel } from "survey-creator-core";
+// import "survey-creator-core/survey-creator-core.i18n.js";
+// import "survey-creator-core/survey-creator-core.min.css";
 
 // defineRouteRules({ ssr: false });
 definePageMeta({ middleware: "auth" });
 
-const survey = await useReadSurveyFromRoute();
-const { status } = useAuth();
-const surveyerStore = useSurveyStore();
-const { updateSurvey, autoSave } = surveyerStore;
+// const survey = await useReadSurveyFromRoute();
+// const { status } = useAuth();
+// const surveyerStore = useSurveyStore();
+// const { updateSurvey, autoSave } = surveyerStore;
 
-const creator = new SurveyCreatorModel({ showLogicTab: true, isAutoSave: true });
-creator.text = survey.model;
-creator.saveSurveyFunc = async (saveNo: number, callback: Function) => {
-  survey.model = creator.text;
-  survey.updatedAt = new Date();
+// const creator = new SurveyCreatorModel({ showLogicTab: true, isAutoSave: true });
+// creator.text = survey.model;
+// creator.saveSurveyFunc = async (saveNo: number, callback: Function) => {
+//   survey.model = creator.text;
+//   survey.updatedAt = new Date();
 
-  if (status.value === "authenticated")
-    try {
-      await updateSurvey(survey);
-      callback(saveNo, true);
-    } catch {
-      callback(saveNo, false);
-    }
-  else if (status.value === "unauthenticated") callback(saveNo, autoSave(survey));
-};
+//   if (status.value === "authenticated")
+//     try {
+//       await updateSurvey(survey);
+//       callback(saveNo, true);
+//     } catch {
+//       callback(saveNo, false);
+//     }
+//   else if (status.value === "unauthenticated") callback(saveNo, autoSave(survey));
+// };
 
 throw createError({ statusCode: 404, statusMessage: "Not implemented" });
 </script>
