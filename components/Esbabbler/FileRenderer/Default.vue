d39e905546a251505d3285c39ebfafb093d59499
Q16solver <jimmychen12138@gmail.com>
2023-12-30 14:03:44 +1100

    chore: Update vue dep + fix up comment

--- components/Esbabbler/FileRenderer/Default.vue
+++ components/Esbabbler/FileRenderer/Default.vue
@@ -3,8 +3,9 @@ import { type FileRendererProps } from "@/models/esbabbler/file/FileRendererProp
 
 const { url } = defineProps<FileRendererProps>();
 /**
- * Intentional double decode, the first decode is to unwrap the fact that we need to encode the path
- * the second decode is to decode the filename which is encoded to support utf8.
+ * Intentional double decode:
+ * the first decode is to unwrap the fact that we need to encode the path
+ * the second decode is to decode the filename which is encoded to support utf8
  */
 const uniqueFilename = computed(() => decodeURIComponent(decodeURIComponent(url.substring(url.lastIndexOf("/") + 1))));
 const niceFilename = computed(() => uniqueFilename.value.substring(uniqueFilename.value.indexOf(":") + 1));
