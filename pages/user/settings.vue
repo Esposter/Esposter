bc13bd42fdcd8e0dc3fbaae461ac8a810b889e1b
Q16solver <jimmychen12138@gmail.com>
2024-01-06 00:04:31 +1100

    feat: Add single column + sticky css

--- pages/user/settings.vue
+++ pages/user/settings.vue
@@ -6,6 +6,7 @@ import { useUserStore } from "@/store/user";
 definePageMeta({ middleware: "auth" });
 
 const { $client } = useNuxtApp();
+const { smAndDown } = useDisplay();
 const userStore = useUserStore();
 const { authUser } = storeToRefs(userStore);
 authUser.value = await $client.user.readUser.query();
@@ -22,10 +23,10 @@ const sections: SideBarItem[] = [{ title: "General", href: RoutePath.UserSetting
         </v-col>
       </v-row>
       <v-row>
-        <v-col cols="5">
-          <UserSideBar sticky="!" :items="sections" />
+        <v-col :cols="smAndDown ? 12 : 5">
+          <UserSideBar class="sidebar" sticky="!" :items="sections" />
         </v-col>
-        <v-col cols="7">
+        <v-col :cols="smAndDown ? 12 : 7">
           <UserProfileCard />
         </v-col>
       </v-row>
