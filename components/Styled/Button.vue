c2056cd39a9842749e02006035e0226d6dcbcb15
Q16solver <jimmychen12138@gmail.com>
2024-01-06 16:50:44 +1100

    fix: remove unnecessary useAttrs on button

--- components/Styled/Button.vue
+++ components/Styled/Button.vue
@@ -1,13 +1,11 @@
 <script setup lang="ts">
 import { VBtn } from "vuetify/components";
 
-const attrs = useAttrs();
 const slots = defineSlots<Record<keyof VBtn["$slots"], Function>>();
 </script>
 
 <template>
-  <!-- @TODO: Using attrs here to inherit behaviour, no type-safety since Vue doesn't support defineProps<VBtn["$props"]> yet -->
-  <v-btn class="button" un-color="#fff!" case-normal="!" :="attrs">
+  <v-btn class="button" un-color="#fff!" case-normal="!">
     <template v-for="(_, slot) of slots" #[slot]>
       <slot :name="slot" />
     </template>
