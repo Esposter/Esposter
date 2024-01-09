4fe830929966bf7660d70d44ffb11b08fc907179
Jimmy Chen <jimmyc@resonatesolutions.com.au>
2024-01-09 11:50:08 +1100

    fix: remove dayjs config

--- configuration/dayjs.ts
+++
@@ -1,5 +0,0 @@
-import { type NuxtConfig } from "nuxt/schema";
-
-export const dayjs: NuxtConfig["dayjs"] = {
-  plugins: ["advancedFormat", "duration", "isToday", "isYesterday", "relativeTime"],
-};
