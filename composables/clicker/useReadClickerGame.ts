db6f77fd2a95532a9a945048705cf3bb5df8c00e
Jimmy Chen <jimmyc@resonatesolutions.com.au>
2024-01-10 16:43:28 +1100

    feat: Add tracking and saving game state other than noPoints

--- composables/clicker/useReadClickerGame.ts
+++ composables/clicker/useReadClickerGame.ts
@@ -1,12 +1,21 @@
 import { Game } from "@/models/clicker/Game";
 import { CLICKER_STORE } from "@/services/clicker/constants";
 import { useGameStore } from "@/store/clicker/game";
+import { isDiff } from "@/util/isDiff";
 import { jsonDateParse } from "@/util/json";
 
 export const useReadClickerGame = async () => {
   const { $client } = useNuxtApp();
   const gameStore = useGameStore();
+  const { saveGame } = gameStore;
   const { game } = storeToRefs(gameStore);
+  // This is used for tracking when we should save the game
+  // i.e. every time we update the game state aside from noPoints
+  // @TODO: Track boughtBuildings without producedValue after implementing omitDeep
+  const gameTracker = computed<Omit<Game, "noPoints">>((oldGameTracker) => {
+    const { noPoints, ...rest } = game.value;
+    return !oldGameTracker || isDiff(oldGameTracker, rest) ? rest : oldGameTracker;
+  });
 
   await useReadData(
     () => {
@@ -18,4 +27,6 @@ export const useReadClickerGame = async () => {
       game.value = await $client.clicker.readGame.query();
     },
   );
+
+  watch(gameTracker, saveGame);
 };
