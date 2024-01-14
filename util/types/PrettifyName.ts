ada9cf3837f52eb73f5d3f72066b7c65e0a0fbd2
Q16solver <jimmychen12138@gmail.com>
2024-01-14 22:42:13 +1100

    fix: prettify name edge cases

--- util/types/PrettifyName.ts
+++ util/types/PrettifyName.ts
@@ -1,10 +1,13 @@
 import { type Trim } from "type-fest";
+import { type UpperCaseToLowerCase } from "type-fest/source/delimiter-case";
 import { type UpperCaseCharacters } from "type-fest/source/internal";
 
-export type PrettifyName<T extends string> = Trim<
-  T extends `${infer Start}${infer UpperCaseCharacter}${infer Rest}`
+type BasePrettifyName<T extends string> = T extends `${infer Start}${infer UpperCaseCharacter}${infer Rest}`
+  ? Start extends UpperCaseToLowerCase<UpperCaseCharacters>
     ? UpperCaseCharacter extends UpperCaseCharacters
-      ? `${Start} ${UpperCaseCharacter}${PrettifyName<Rest>}`
-      : `${Start}${UpperCaseCharacter}${PrettifyName<Rest>}`
-    : T
->;
+      ? `${Start} ${UpperCaseCharacter}${BasePrettifyName<Rest>}`
+      : `${Start}${BasePrettifyName<`${UpperCaseCharacter}${Rest}`>}`
+    : `${Start}${UpperCaseCharacter}${BasePrettifyName<Rest>}`
+  : T;
+
+export type PrettifyName<T extends string> = Trim<BasePrettifyName<T>>;
