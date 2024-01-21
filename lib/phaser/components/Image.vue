f549b9d17da82470a67993a1faafc2cc79388de1
Q16solver <jimmychen12138@gmail.com>
2024-01-21 19:54:13 +1100

    feat: Port over image

---
+++ lib/phaser/components/Image.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import { usePhaserStore } from "@/lib/phaser/store/phaser";
+import { initializeGameObject } from "@/lib/phaser/util/initializeGameObject";
+import { ImageSetterMap } from "@/lib/phaser/util/setterMap/ImageSetterMap";
+import { type Types } from "phaser";
+
+interface ImageProps {
+  configuration: Types.GameObjects.Sprite.SpriteConfig;
+}
+
+const props = defineProps<ImageProps>();
+const { configuration } = toRefs(props);
+const phaserStore = usePhaserStore();
+const { gameObjectCreator } = storeToRefs(phaserStore);
+initializeGameObject((configuration) => gameObjectCreator.value.image(configuration), configuration, ImageSetterMap);
+</script>
