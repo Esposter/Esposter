7016934698df7d30b3841766b90d77509d2b0405
Q16solver <jimmychen12138@gmail.com>
2024-01-23 20:45:29 +1100

    fix: remove game predefined scenes to fix green square issue

--- pages/dungeons.vue
+++ pages/dungeons.vue
@@ -1,7 +1,5 @@
 <script setup lang="ts">
 import Game from "@/lib/phaser/components/Game.vue";
-import { BattleScene } from "@/models/dungeons/scenes/BattleScene";
-import { GameScene } from "@/models/dungeons/scenes/GameScene";
 import { GridEngine } from "grid-engine";
 import { AUTO, Scale } from "phaser";
 import VirtualJoystickPlugin from "phaser3-rex-plugins/plugins/virtualjoystick-plugin.js";
@@ -21,7 +19,6 @@ defineRouteRules({ ssr: false });
           mode: Scale.FIT,
           autoCenter: Scale.CENTER_BOTH,
         },
-        scene: [GameScene, BattleScene],
         input: {
           keyboard: true,
           touch: true,
