96b815d5059e0d39ba18566ab8d343ed3db4aa03
Q16solver <jimmychen12138@gmail.com>
2024-02-08 20:42:56 +1100

    feat: Add fade in for world scene transition

--- components/Dungeons/World/Scene.vue
+++ components/Dungeons/World/Scene.vue
@@ -4,6 +4,7 @@ import Scene from "@/lib/phaser/components/Scene.vue";
 import { ImageKey } from "@/models/dungeons/keys/ImageKey";
 import { SceneKey } from "@/models/dungeons/keys/SceneKey";
 import { SceneWithPlugins } from "@/models/dungeons/scene/plugins/SceneWithPlugins";
+import { dayjs } from "@/services/dayjs";
 import { useGameStore } from "@/store/dungeons/game";
 import { usePlayerStore } from "@/store/dungeons/world/player";
 import { type Position } from "grid-engine";
@@ -19,6 +20,11 @@ const position = ref<Position>({ x: 1, y: 1 });
   <Scene
     :scene-key="SceneKey.World"
     :cls="SceneWithPlugins"
+    @create="
+      (scene) => {
+        scene.cameras.main.fadeIn(dayjs.duration(0.6, 'seconds').asMilliseconds(), 0, 0, 0);
+      }
+    "
     @update="
       () => {
         position = controls.move(position);
--- store/dungeons/settings.ts
+++ store/dungeons/settings.ts
@@ -1,4 +1,6 @@
+import { IS_DEVELOPMENT } from "@/util/environment/constants";
+
 export const useSettingsStore = defineStore("dungeons/settings", () => {
-  const isSkipBattleAnimations = ref(false);
+  const isSkipBattleAnimations = ref(IS_DEVELOPMENT);
   return { isSkipBattleAnimations };
 });
