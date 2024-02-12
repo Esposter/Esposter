100c624e591ec33b0975c9c4b58ea7426bb6fc50
Q16solver <jimmychen12138@gmail.com>
2024-02-12 23:23:01 +1100

    feat: move player pos to store

--- components/Dungeons/World/Character/Player.vue
+++ components/Dungeons/World/Character/Player.vue
@@ -5,13 +5,11 @@ import { DESTROY_SCENE_EVENT_KEY } from "@/lib/phaser/util/constants";
 import { SceneKey } from "@/models/dungeons/keys/SceneKey";
 import { CharacterId } from "@/models/dungeons/world/CharacterId";
 import { usePlayerStore } from "@/store/dungeons/world/player";
-import { type Position } from "grid-engine";
 
 const phaserStore = usePhaserStore();
 const { scene } = storeToRefs(phaserStore);
 const playerStore = usePlayerStore();
-const { character } = storeToRefs(playerStore);
-const position = ref<Position>({ x: 6, y: 21 });
+const { character, position } = storeToRefs(playerStore);
 const destroyListener = () => {
   scene.value.cameras.main.stopFollow();
 };
--- store/dungeons/world/player.ts
+++ store/dungeons/world/player.ts
@@ -1,5 +1,6 @@
 import { SpritesheetKey } from "@/models/dungeons/keys/SpritesheetKey";
 import { type Character } from "@/models/dungeons/world/Character";
+import { type Position } from "grid-engine";
 
 export const usePlayerStore = defineStore("dungeons/world/player", () => {
   const character = ref<Character>({
@@ -8,8 +9,9 @@ export const usePlayerStore = defineStore("dungeons/world/player", () => {
       frame: 7,
     },
   });
-
+  const position = ref<Position>({ x: 6, y: 21 });
   return {
     character,
+    position,
   };
 });
