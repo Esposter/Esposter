f1cf3bb33e71071bdccffcb95b80b7284d8e28e1
Jimmy Chen <jimmyc@resonatesolutions.com.au>
2024-02-14 15:08:28 +1100

    fix: make offsets smaller

--- services/dungeons/joystick/getJoystickY.ts
+++ services/dungeons/joystick/getJoystickY.ts
@@ -1,4 +1,4 @@
 import { JOYSTICK_RADIUS } from "@/services/dungeons/joystick/constants";
 import { type Scene } from "phaser";
 
-export const getJoystickY = (scene: Scene) => scene.scale.height - JOYSTICK_RADIUS * 1.25;
+export const getJoystickY = (scene: Scene) => scene.scale.height - JOYSTICK_RADIUS;
