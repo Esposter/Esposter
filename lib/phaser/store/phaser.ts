42186ca8ce54fdfcf19cbbe3c2ea844cafd75980
Q16solver <jimmychen12138@gmail.com>
2024-02-15 17:59:30 +1100

    chore: remove outdated comment

--- lib/phaser/store/phaser.ts
+++ lib/phaser/store/phaser.ts
@@ -6,7 +6,6 @@ import { type Game } from "phaser";
 
 export const usePhaserStore = defineStore("phaser", () => {
   const game = ref<Game | null>(null);
-  // All our scene transitions will be handled by watching the current scene key
   const sceneKey = ref<SceneKey | null>(null);
   // We will create the scene in the game and ensure that the scene will always exist
   // for the child components by using v-if for the scene value
