aee7ee5fc90f537499890875ef0e8cb816ce1515
Q16solver <jimmychen12138@gmail.com>
2024-02-18 21:41:11 +1100

    chore: Add better comment

--- components/Dungeons/World/Character/Npc.vue
+++ components/Dungeons/World/Character/Npc.vue
@@ -1,7 +1,8 @@
 <script setup lang="ts">
 import { type Npc } from "@/models/dungeons/world/Npc";
 import { type Direction, type Position } from "grid-engine";
-// Vue doesn't support complex prop types
+// Vue doesn't support complex prop types, so we have to use 'Omit'
+// which is banned by our eslint in favor of stricter type 'Except'
 // eslint-disable-next-line @typescript-eslint/ban-types
 const { asset, id: characterId, ...rest } = defineProps<Omit<Npc, "position" | "direction">>();
 const position = defineModel<Position>("position", { required: true });
