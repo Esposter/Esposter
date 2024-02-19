e27c0e68a4eae18db2f67a4703fa516ed3c1ed65
Jimmy Chen <jimmyc@resonatesolutions.com.au>
2024-02-19 11:18:58 +1100

    chore: fix comment

--- store/dungeons/world/scene.ts
+++ store/dungeons/world/scene.ts
@@ -1,7 +1,7 @@
 import { type Tilemaps } from "phaser";
 
 export const useWorldSceneStore = defineStore("dungeons/world/scene", () => {
-  // We will create the layers in the world scene
+  // We will create the tilemap and its layers in the world scene
   const tilemap = ref() as Ref<Tilemaps.Tilemap>;
   const encounterLayer = ref() as Ref<Tilemaps.TilemapLayer>;
   const signLayer = ref() as Ref<Tilemaps.ObjectLayer>;
