8f50b9d2c047268d7cf9f0ce7ef478095a04d4ac
Q16solver <jimmychen12138@gmail.com>
2024-02-29 22:29:30 +1100

    fix: change back to cache

--- composables/tableEditor/useEditFormComponent.ts
+++ composables/tableEditor/useEditFormComponent.ts
@@ -1,13 +1,15 @@
 import { useTableEditorStore } from "@/store/tableEditor";
 
+const editFormComponentFilepathEntries = Object.entries<Component>(
+  import.meta.glob("@/components/TableEditor/**/Item/**/EditForm.vue", { eager: true, import: "default" }),
+);
+
 export const useEditFormComponent = <T extends string>(itemType: T) => {
   const tableEditorStore = useTableEditorStore()();
   const { tableEditorType } = storeToRefs(tableEditorStore);
   let editFormComponent: Component | null = null;
 
-  for (const [filepath, component] of Object.entries<Component>(
-    import.meta.glob("@/components/TableEditor/**/Item/**/EditForm.vue", { eager: true, import: "default" }),
-  ))
+  for (const [filepath, component] of editFormComponentFilepathEntries)
     if (filepath.includes(`${tableEditorType.value}/Item/${itemType}`)) editFormComponent = component;
 
   return editFormComponent;
