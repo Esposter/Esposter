dc4e19706dbb342beb76180ac1dddf8648b670c7
Q16solver <jimmychen12138@gmail.com>
2024-02-29 19:59:20 +1100

    fix: settings text

--- services/dungeons/settings/InfoContainerTextMap.ts
+++ services/dungeons/settings/InfoContainerTextMap.ts
@@ -3,7 +3,7 @@ import { PlayerSettingsOption } from "@/models/dungeons/settings/PlayerSettingsO
 export const InfoContainerTextMap = {
   [PlayerSettingsOption["Text Speed"]]: "Choose one of three text display speeds.",
   [PlayerSettingsOption.Animations]: "Choose to display animations and effects or not.",
-  [PlayerSettingsOption["Battle Style"]]: "'Choose to allow your monster to be recalled between rounds.",
+  [PlayerSettingsOption["Battle Style"]]: "Choose to allow your monster to be recalled between rounds.",
   [PlayerSettingsOption.Sound]: "Choose to enable or disable the sound.",
   [PlayerSettingsOption.Volume]: "Choose the volume for the music and sound effects of the game.",
   [PlayerSettingsOption["Menu Color"]]: "Choose one of the three menu color options.",
