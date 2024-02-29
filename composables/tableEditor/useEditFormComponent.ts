b40d591eeec0178d39ef5427d4ff9e26dc7c9241
Q16solver <jimmychen12138@gmail.com>
2024-02-29 22:29:08 +1100

    chore: Make lint stricter

--- composables/tableEditor/useEditFormComponent.ts
+++ composables/tableEditor/useEditFormComponent.ts
@@ -1,15 +1,13 @@
 import { useTableEditorStore } from "@/store/tableEditor";
 
-const EditFormComponentFilepathEntries = Object.entries<Component>(
-  import.meta.glob("@/components/TableEditor/**/Item/**/EditForm.vue", { eager: true, import: "default" }),
-);
-
 export const useEditFormComponent = <T extends string>(itemType: T) => {
   const tableEditorStore = useTableEditorStore()();
   const { tableEditorType } = storeToRefs(tableEditorStore);
   let editFormComponent: Component | null = null;
 
-  for (const [filepath, component] of EditFormComponentFilepathEntries)
+  for (const [filepath, component] of Object.entries<Component>(
+    import.meta.glob("@/components/TableEditor/**/Item/**/EditForm.vue", { eager: true, import: "default" }),
+  ))
     if (filepath.includes(`${tableEditorType.value}/Item/${itemType}`)) editFormComponent = component;
 
   return editFormComponent;
