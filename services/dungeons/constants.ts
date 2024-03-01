5e7090970d35e08a6c923fae7bc905625519f0f9
Q16solver <jimmychen12138@gmail.com>
2024-03-01 22:05:29 +1100

    chore: remove unnecessary variable

--- services/dungeons/constants.ts
+++ services/dungeons/constants.ts
@@ -1,6 +1,5 @@
 export const SAVE_FILENAME = "save.json";
 export const DUNGEONS_LOCAL_STORAGE_KEY = "dungeons-store";
-export const FPS = 60;
 
 export const BLANK_VALUE = "-";
 export const MESSAGE_SEPARATOR = "::";
