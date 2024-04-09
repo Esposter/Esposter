461f141aa3f1c76478b23ed35e704ac3756c7e70
Jimmy Chen <jimmyc@resonatesolutions.com.au>
2024-04-09 18:12:03 +1000

    fix: remove useless setter functions

--- lib/phaser/util/setterMap/global/TweenSetterMap.ts
+++ lib/phaser/util/setterMap/global/TweenSetterMap.ts
@@ -1,34 +1,26 @@
 import type { TweenConfiguration } from "@/lib/phaser/models/configuration/global/TweenConfiguration";
-import type { TweenBuilderConfiguration } from "@/lib/phaser/models/configuration/shared/TweenBuilderConfiguration";
 import type { TweenEventEmitsOptions } from "@/lib/phaser/models/emit/global/TweenEventEmitsOptions";
 import type { SetterMap } from "@/lib/phaser/models/setterMap/SetterMap";
-import type { GameObjects, Tweens } from "phaser";
+import type { GameObjects } from "phaser";
 
 export const TweenSetterMap = {
-  tween: (gameObject, emit) =>
-    createTween<Tweens.Tween, TweenBuilderConfiguration>((configuration) => {
-      const tween = gameObject.scene.add.tween({ ...configuration, targets: gameObject });
-      tween.on("complete", () => {
-        emit("update:tween", undefined);
-      });
-      return tween;
-    }),
-  tweenchain: (gameObject, emit) =>
-    createTween<Tweens.TweenChain, TweenBuilderConfiguration[]>((configurations) => {
-      const tweenchain = gameObject.scene.add.tweenchain(configurations.map((c) => ({ ...c, targets: gameObject })));
-      tweenchain.on("complete", () => {
-        emit("update:tweenchain", undefined);
-      });
-      return tweenchain;
-    }),
-} as const satisfies SetterMap<TweenConfiguration, GameObjects.GameObject, TweenEventEmitsOptions>;
+  tween: (gameObject, emit) => (value) => {
+    if (!value) return;
 
-const createTween = <TTween extends { destroy: () => void }, TConfiguration extends object>(
-  callback: (configuration: TConfiguration) => TTween,
-) => {
-  let previousTween: TTween | undefined;
-  return (configuration?: TConfiguration) => {
-    if (previousTween) previousTween.destroy();
-    previousTween = configuration ? callback(configuration) : undefined;
-  };
-};
+    const tween = gameObject.scene.add.tween({ ...value, targets: gameObject });
+    tween.on("complete", () => {
+      emit("update:tween", undefined);
+      tween.destroy();
+    });
+  },
+  tweenchain: (gameObject, emit) => (value) => {
+    if (!value) return;
+
+    const tweenchain = gameObject.scene.add.tweenchain(value.map((c) => ({ ...c, targets: gameObject })));
+    tweenchain.on("complete", () => {
+      emit("update:tweenchain", undefined);
+      tweenchain.destroy();
+    });
+    return tweenchain;
+  },
+} as const satisfies SetterMap<TweenConfiguration, GameObjects.GameObject, TweenEventEmitsOptions>;
--- lib/phaser/util/setterMap/shared/AnimationSetterMap.ts
+++ lib/phaser/util/setterMap/shared/AnimationSetterMap.ts
@@ -5,20 +5,20 @@ import type { GameObjects } from "phaser";
 import { Animations } from "phaser";
 
 export const AnimationSetterMap = {
-  animations: (gameObject, emit) =>
-    createAnimations<Animations.Animation, AnimationConfiguration["animations"]>((configurations) => {
-      const animations: Animations.Animation[] = [];
-      for (const configuration of configurations) {
-        const animation = gameObject.scene.anims.create(configuration);
-        if (!animation) throw new Error(`Invalid animation key: ${configuration.key}`);
-        const event = `${Animations.Events.ANIMATION_COMPLETE_KEY}${configuration.key}`;
-        gameObject.once(event, () => {
-          emit(event);
-        });
-        animations.push(animation);
-      }
-      return animations;
-    }),
+  animations: (gameObject, emit) => (configurations) => {
+    if (!(configurations && configurations.length > 0)) return;
+
+    const animations: Animations.Animation[] = [];
+    for (const configuration of configurations) {
+      const animation = gameObject.scene.anims.create(configuration);
+      if (!animation) throw new Error(`Invalid animation key: ${configuration.key}`);
+      const event = `${Animations.Events.ANIMATION_COMPLETE_KEY}${configuration.key}`;
+      gameObject.once(event, () => {
+        emit(event);
+      });
+      animations.push(animation);
+    }
+  },
   playAnimationKey: (gameObject) => (value) => {
     if (value === undefined) {
       gameObject.stop();
@@ -28,13 +28,3 @@ export const AnimationSetterMap = {
     gameObject.play(value);
   },
 } as const satisfies SetterMap<AnimationConfiguration, GameObjects.Sprite, SpriteEventEmitsOptions>;
-
-const createAnimations = <TAnimation extends { destroy: () => void }, TConfigurations extends object[]>(
-  callback: (configurations: TConfigurations) => TAnimation[],
-) => {
-  let previousAnimations: TAnimation[] = [];
-  return (configurations?: TConfigurations) => {
-    for (const previousAnimation of previousAnimations) previousAnimation.destroy();
-    previousAnimations = configurations ? callback(configurations) : [];
-  };
-};
