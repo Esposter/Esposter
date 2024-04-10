6236330d6d57ac72170a9724b80bee637fec3ae0
Jimmy Chen <jimmyc@resonatesolutions.com.au>
2024-04-10 10:00:38 +1000

    fix: make animations cleanup after unmount

--- lib/phaser/composables/useAnimations.ts
+++ lib/phaser/composables/useAnimations.ts
@@ -1,10 +1,16 @@
 import type { AnimationConfiguration } from "@/lib/phaser/models/configuration/shared/AnimationConfiguration";
+import { usePhaserStore } from "@/lib/phaser/store/phaser";
 
-export const useAnimations = (configuration: AnimationConfiguration["animations"]) => {
-  const animations = ref(configuration);
+export const useAnimations = (configurations: AnimationConfiguration["animations"]) => {
+  const phaserStore = usePhaserStore();
+  const { scene } = storeToRefs(phaserStore);
+  const animations = ref(configurations);
 
   onUnmounted(() => {
-    animations.value = [];
+    for (const { key } of configurations) {
+      if (!key) continue;
+      scene.value.anims.remove(key);
+    }
   });
 
   return animations;
--- lib/phaser/util/setterMap/shared/AnimationSetterMap.ts
+++ lib/phaser/util/setterMap/shared/AnimationSetterMap.ts
@@ -5,22 +5,19 @@ import type { GameObjects } from "phaser";
 import { Animations } from "phaser";
 
 export const AnimationSetterMap = {
-  animations: (gameObject, emit) =>
-    createAnimations<Animations.Animation, AnimationConfiguration["animations"]>((configurations) => {
-      const animations: Animations.Animation[] = [];
+  animations: (gameObject, emit) => (configurations) => {
+    if (!(configurations && configurations.length > 0)) return;
 
-      for (const configuration of configurations) {
-        const animation = gameObject.scene.anims.create(configuration);
-        if (!animation) throw new Error(`Invalid animation key: ${configuration.key}`);
-        const event = `${Animations.Events.ANIMATION_COMPLETE_KEY}${configuration.key}`;
-        gameObject.once(event, () => {
-          emit(event);
-        });
-        animations.push(animation);
-      }
-
-      return animations;
-    }),
+    for (const configuration of configurations) {
+      if (!configuration.key || gameObject.scene.anims.exists(configuration.key)) continue;
+      else if (!gameObject.scene.anims.create(configuration))
+        throw new Error(`Invalid animation key: ${configuration.key}`);
+      const event = `${Animations.Events.ANIMATION_COMPLETE_KEY}${configuration.key}`;
+      gameObject.once(event, () => {
+        emit(event);
+      });
+    }
+  },
   playAnimationKey: (gameObject) => (value) => {
     if (value === undefined) {
       gameObject.stop();
@@ -30,13 +27,3 @@ export const AnimationSetterMap = {
     gameObject.play(value);
   },
 } as const satisfies SetterMap<AnimationConfiguration, GameObjects.Sprite, SpriteEventEmitsOptions>;
-
-const createAnimations = <TAnimation extends { destroy: () => void }, TConfigurations extends object[]>(
-  callback: (configurations: TConfigurations) => TAnimation[],
-) => {
-  let previousAnimations: TAnimation[] = [];
-  return (configurations?: TConfigurations) => {
-    for (const { destroy } of previousAnimations) destroy();
-    previousAnimations = configurations ? callback(configurations) : [];
-  };
-};
