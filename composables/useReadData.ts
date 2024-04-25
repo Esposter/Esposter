bb2d41ee0d7da72a2eeb0df247b10cbd08f04454
Q16solver <jimmychen12138@gmail.com>
2024-04-25 13:29:50 +1000

    fix: remove unnecessary boolean

--- composables/useReadData.ts
+++ composables/useReadData.ts
@@ -1,8 +1,4 @@
-export const useReadData = async (
-  unauthedReader: () => void,
-  authedReader: () => Promise<void>,
-  isClientSideRendered?: true,
-) => {
+export const useReadData = async (unauthedReader: () => void, authedReader: () => Promise<void>) => {
   const { status } = useAuth();
   watch(status, async (newStatus) => {
     if (newStatus === "authenticated") await authedReader();
@@ -11,6 +7,5 @@ export const useReadData = async (
 
   if (status.value === "authenticated") await authedReader();
   // We'll assume that not being authenticated means that we will read from local storage for data
-  // If it's already client-side rendered then we do not need to add onMounted
-  else if (status.value === "unauthenticated") isClientSideRendered ? unauthedReader() : onMounted(unauthedReader);
+  else if (status.value === "unauthenticated") onMounted(unauthedReader);
 };
