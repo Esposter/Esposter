76bd46eedb3885cc6e07ccddf463122215e14dc1
Q16solver <jimmychen12138@gmail.com>
2024-04-25 17:39:39 +1000

    fix: add emoji pkg

--- configuration/security.ts
+++ configuration/security.ts
@@ -3,7 +3,14 @@ import type { NuxtConfig } from "nuxt/schema";
 export const security: NuxtConfig["security"] = {
   headers: {
     contentSecurityPolicy: {
-      "img-src": ["'self'", "data:", "blob:", "*.googleusercontent.com", process.env.AZURE_BLOB_URL],
+      "img-src": [
+        "'self'",
+        "data:",
+        "blob:",
+        "*.googleusercontent.com",
+        "https://unpkg.com/emoji-datasource-apple@15.0.1/img/apple/sheets-256/64.png",
+        process.env.AZURE_BLOB_URL,
+      ],
       "script-src-attr": ["'unsafe-inline'"],
     },
     crossOriginEmbedderPolicy: "unsafe-none",
