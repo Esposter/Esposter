a1f661bb224ab124785869100ed897956f06eb8e
Q16solver <jimmychen12138@gmail.com>
2024-04-25 10:44:52 +1000

    fix: allow user content images

--- configuration/security.ts
+++ configuration/security.ts
@@ -4,7 +4,7 @@ export const security: NuxtConfig["security"] = {
   csrf: true,
   headers: {
     contentSecurityPolicy: {
-      "img-src": ["'self'", "data:", "blob:", process.env.AZURE_BLOB_URL],
+      "img-src": ["'self'", "data:", "blob:", "*.googleusercontent.com", process.env.AZURE_BLOB_URL],
       "script-src-attr": ["'unsafe-inline'"],
     },
     crossOriginEmbedderPolicy: "unsafe-none",
