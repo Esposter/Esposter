81ddc300c6224805b5da3d97f3e28c986d05b3b5
Q16solver <jimmychen12138@gmail.com>
2024-05-05 13:07:27 +1000

    chore: fix up comments to be correct

--- lib/tmxParser/models/tmx/node/TMXNode.ts
+++ lib/tmxParser/models/tmx/node/TMXNode.ts
@@ -3,13 +3,12 @@ import type { TMXNodeType } from "@/lib/tmxParser/models/tmx/node/TMXNodeType";
 // due to typescript shenanigans with private properties >:C
 interface BaseTMXNode<T> extends Record<string, unknown> {
   $: T;
-  // @ts-expect-error Property derived from standard tmx convention
+  // @ts-expect-error Property derived from xml parsing
   #name: TMXNodeType;
 }
 // For convenience's sake, we will assume that parsing xml will always have
 // explicitChildren && preserveChildrenOrder set to true and we will type that instead
-// so it's easier on the typescript side rather than having to deal with dynamic xml data
-// We will ignore the duplicated parsed data in this way, which is fine
+// We deal with dynamic xml data by typing it explicitly in the specific types that extend this interface
 export type TMXNode<T, TChildNode extends BaseTMXNode<unknown> | undefined = undefined> = BaseTMXNode<T> & {
   $$: TChildNode extends undefined ? undefined : TChildNode[];
 };
