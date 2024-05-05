6b386fadc4bd08f1317fd1745ca5ba41ac97ef8d
Q16solver <jimmychen12138@gmail.com>
2024-05-05 13:33:16 +1000

    refactor: move validation to util

--- lib/phaser/hooks/onShutdown.ts
+++ lib/phaser/hooks/onShutdown.ts
@@ -1,23 +1,15 @@
 import { useInjectScene } from "@/lib/phaser/composables/useInjectScene";
 import { usePhaserStore } from "@/lib/phaser/store/phaser";
 import { useSceneStore } from "@/lib/phaser/store/phaser/scene";
-import { SceneKey } from "@/models/dungeons/keys/SceneKey";
+import { validateHook } from "@/lib/phaser/util/validateHook";
+import type { SceneKey } from "@/models/dungeons/keys/SceneKey";
 import type { SceneWithPlugins } from "@/models/dungeons/scene/SceneWithPlugins";
-import { NotInitializedError } from "@/models/error/NotInitializedError";
-import { GameObjectType } from "@/models/error/dungeons/GameObjectType";
 
 export const onShutdown = (listener: (scene: SceneWithPlugins) => void, sceneKey?: string) => {
   const phaserStore = usePhaserStore();
   const { game } = storeToRefs(phaserStore);
 
-  if (!game.value) throw new NotInitializedError(GameObjectType.Game);
-  else if (game.value.scene.scenes.length !== Object.keys(SceneKey).length)
-    throw new Error(
-      `
-      onShutdown has been incorrectly called when all the scenes in the game have not been initialized yet.
-      If you have called onShutdown in a root scene component that renders <Scene /> from the vue-phaser library,
-      please call onShutdown in the event emitters provided by <Scene /> instead and pass in the scene key.`,
-    );
+  validateHook(game.value, onShutdown.name);
 
   const sceneStore = useSceneStore();
   const { shutdownListenersMap } = storeToRefs(sceneStore);
