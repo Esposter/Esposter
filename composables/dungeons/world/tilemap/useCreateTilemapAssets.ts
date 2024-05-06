e41604005655dd4c21996bb89986e7440961b599
Jimmy Chen <jimmyc@resonatesolutions.com.au>
2024-05-06 12:34:50 +1000

    fix: better way to just ignore if we can't add unnecessary tileset images

--- composables/dungeons/world/tilemap/useCreateTilemapAssets.ts
+++ composables/dungeons/world/tilemap/useCreateTilemapAssets.ts
@@ -3,13 +3,19 @@ import { ObjectgroupName } from "@/generated/tiled/layers/ObjectgroupName";
 import { TilesetKey } from "@/models/dungeons/keys/TilesetKey";
 import { DEBUG_TILE_LAYER_ALPHA } from "@/services/dungeons/tilemap/constants";
 import { useWorldSceneStore } from "@/store/dungeons/world/scene";
+import type { Tilemaps } from "phaser";
 
 export const useCreateTilemapAssets = <TLayerName extends object>(layerNameEnum: TLayerName) => {
   const worldSceneStore = useWorldSceneStore();
   const { encounterLayer, objectLayerMap } = storeToRefs(worldSceneStore);
 
   for (const layerName of Object.values(layerNameEnum)) {
-    const tilesets = Object.values(TilesetKey).map((k) => useCreateTileset(k));
+    const tilesets: Tilemaps.Tileset[] = [];
+    for (const tilesetKey of Object.values(TilesetKey)) {
+      const tileset = useCreateTileset(tilesetKey);
+      if (!tileset) continue;
+      tilesets.push(tileset);
+    }
     const layer = useCreateLayer(layerName, tilesets);
     if (layerName === LayerName.Encounter) encounterLayer.value = layer.setAlpha(DEBUG_TILE_LAYER_ALPHA);
     else if (layerName === LayerName.Collision) layer.setAlpha(DEBUG_TILE_LAYER_ALPHA);
--- composables/dungeons/world/tilemap/useCreateTileset.ts
+++ composables/dungeons/world/tilemap/useCreateTileset.ts
@@ -1,6 +1,4 @@
 import type { TilesetKey } from "@/models/dungeons/keys/TilesetKey";
-import { InvalidOperationError } from "@/models/error/InvalidOperationError";
-import { Operation } from "@/models/shared/Operation";
 import { useWorldSceneStore } from "@/store/dungeons/world/scene";
 import type { Tilemaps } from "phaser";
 
@@ -14,7 +12,10 @@ export const useCreateTileset = (tilesetKey: TilesetKey) => {
   const { tilemap } = storeToRefs(worldSceneStore);
   // We will enforce that the tileset name should exactly match the tileset key
   const newTileset = tilemap.value.addTilesetImage(tilesetKey);
-  if (!newTileset) throw new InvalidOperationError(Operation.Create, useCreateTileset.name, `key: ${tilesetKey}`);
+  // This will happen if we try specifying tileset keys that are not required by
+  // the tilemap, which is ok because we always try create all the tilesets
+  // and store them in the cache for convenience in the development workflow
+  if (!newTileset) return null;
   cache.set(tilesetKey, newTileset);
   return newTileset;
 };
