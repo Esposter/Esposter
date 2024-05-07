439ca739d26d2491f4504bbedee036e4affc6bb3
Q16solver <jimmychen12138@gmail.com>
2024-05-07 15:02:48 +1000

    fix: don't use on shutdown for bgm

--- composables/dungeons/sound/useDungeonsBackgroundMusic.ts
+++ composables/dungeons/sound/useDungeonsBackgroundMusic.ts
@@ -1,10 +1,11 @@
-import { onShutdown } from "@/lib/phaser/hooks/onShutdown";
 import type { BackgroundMusicKey } from "@/models/dungeons/keys/sound/BackgroundMusicKey";
 import type { SceneWithPlugins } from "@/models/dungeons/scene/SceneWithPlugins";
 
 let backgroundMusicKey: BackgroundMusicKey | null = null;
 
 export const useDungeonsBackgroundMusic = (scene: SceneWithPlugins, key: BackgroundMusicKey) => {
+  if (key === backgroundMusicKey) return;
+
   const allPlayingSounds = scene.sound.getAllPlaying();
   if (backgroundMusicKey) scene.sound.stopByKey(backgroundMusicKey);
   if (!key || allPlayingSounds.some((s) => s.key === key)) return;
@@ -12,9 +13,4 @@ export const useDungeonsBackgroundMusic = (scene: SceneWithPlugins, key: Backgro
   const { play } = useDungeonsSound(scene, key, { loop: true });
   play();
   backgroundMusicKey = key;
-
-  onShutdown(() => {
-    if (backgroundMusicKey) scene.sound.stopByKey(backgroundMusicKey);
-    backgroundMusicKey = null;
-  }, scene.scene.key);
 };
