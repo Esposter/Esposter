9cc686a79bb030aac580f9cc01da814a3975b4f9
Q16solver <jimmychen12138@gmail.com>
2024-05-07 16:27:00 +1000

    fix: animations

--- lib/phaser/composables/useAnimations.ts
+++ lib/phaser/composables/useAnimations.ts
@@ -1,3 +1,4 @@
+import { onNextTick } from "@/lib/phaser/hooks/onNextTick";
 import { onShutdown } from "@/lib/phaser/hooks/onShutdown";
 import type { AnimationConfiguration } from "@/lib/phaser/models/configuration/shared/AnimationConfiguration";
 import type { SceneWithPlugins } from "@/models/dungeons/scene/SceneWithPlugins";
@@ -5,12 +6,18 @@ import type { SceneWithPlugins } from "@/models/dungeons/scene/SceneWithPlugins"
 export const useAnimations = (
   createConfigurations: (scene: SceneWithPlugins) => AnimationConfiguration["animations"],
 ) => {
-  const animations = ref<AnimationConfiguration["animations"]>();
+  const animations = ref<AnimationConfiguration["animations"]>([]);
+
+  onNextTick((scene) => {
+    animations.value = createConfigurations(scene);
+  });
+
   onShutdown((scene) => {
-    for (const { key } of createConfigurations(scene)) {
+    for (const { key } of animations.value) {
       if (!key) continue;
       scene.anims.remove(key);
     }
   });
+
   return animations;
 };
