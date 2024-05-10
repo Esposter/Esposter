1ca36834de8a9a5ec6e6c425f3bdfe7f15faf543
Jimmy Chen <jimmyc@resonatesolutions.com.au>
2024-05-10 10:42:01 +1000

    feat: migrate drizzle kit

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -2,10 +2,10 @@ import { MIGRATIONS_FOLDER_PATH } from "@/db/constants";
 import { defineConfig } from "drizzle-kit";
 
 export default defineConfig({
+  dialect: "postgresql",
+  dbCredentials: {
+    url: process.env.DATABASE_URL,
+  },
   schema: "db/schema",
   out: MIGRATIONS_FOLDER_PATH,
-  driver: "pg",
-  dbCredentials: {
-    connectionString: process.env.DATABASE_URL,
-  },
 });
