ee18cc4cd03ac7a1edaf8908bc278060df221bc4
Q16solver <jimmychen12138@gmail.com>
2024-05-11 13:46:28 +1000

    chore: some comment stuff

--- scripts/tiled/generate.ts
+++ scripts/tiled/generate.ts
@@ -8,9 +8,8 @@ await remove();
 // e.g. TilemapKey which must exist before we generate other tilemap specific metadata
 await generatePropertyTypes();
 // This causes a small issue when you run the generate scripts below for the first time
-// as the typechecking will complain about the not-yet existent
-// generated typescript enum files from generatePropertyTypes
-// If that's the case, please comment out the below generation scripts and run generatePropertyTypes first
-// to bootstrap the required metadata typescript enum files
+// as the typechecking will complain about the not-yet existent generated typescript enum files from generatePropertyTypes
+// If that's the case, please comment out the below generation scripts
+// and run generatePropertyTypes first to bootstrap the required metadata typescript enum files
 // We don't comment this out by default for a smoother continuous development workflow
 await generateTmxProperties();
