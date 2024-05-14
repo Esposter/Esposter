7b273fef90437c17189a9d70ac75a7104062d510
Jimmy Chen <jimmyc@resonatesolutions.com.au>
2024-05-14 11:13:01 +1000

    chore: syntax

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -3,9 +3,7 @@ import { defineConfig } from "drizzle-kit";
 
 export default defineConfig({
   dialect: "postgresql",
-  dbCredentials: {
-    url: process.env.DATABASE_URL,
-  },
+  dbCredentials: { url: process.env.DATABASE_URL },
   schema: "db/schema",
   out: MIGRATIONS_FOLDER_PATH,
 });
