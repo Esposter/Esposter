bd01ff67a0aee78b2e1fd263a52e1a1bc50cd339
Q16solver <jimmychen12138@gmail.com>
2024-05-17 23:59:36 +1000

    fix: types

--- configuration/typescript.ts
+++ configuration/typescript.ts
@@ -1,5 +1,9 @@
+import typescriptIgnores from "@esposter/eslint/typescriptIgnores";
 import type { NuxtConfig } from "nuxt/schema";
 
 export const typescript: NuxtConfig["typescript"] = {
   shim: false,
+  tsConfig: {
+    exclude: typescriptIgnores.map((i) => (i.startsWith("public") || i.startsWith("packages") ? `../${i}` : i)),
+  },
 };
