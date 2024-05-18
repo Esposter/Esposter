11aae139b259a55ff7e2d5349c0b6b53e3c67b48
Q16solver <jimmychen12138@gmail.com>
2024-05-18 16:17:36 +1000

    fix: remove deprecated stuff

--- packages/app/composables/tableEditor/useConfirmBeforeNavigation.ts
+++ packages/app/composables/tableEditor/useConfirmBeforeNavigation.ts
@@ -8,11 +8,8 @@ export const useConfirmBeforeNavigation = () => {
     if (isDirty.value && !window.confirm("Changes that you made may not be saved.")) return false;
   });
 
-  useEventListener("beforeunload", (e) => {
+  useEventListener("beforeunload", ({ preventDefault }) => {
     if (!isDirty.value) return;
-    e.preventDefault();
-    // Even though the docs tell us that it's deprecated,
-    // this actually makes the window prompt for reloading pop up
-    e.returnValue = false;
+    preventDefault();
   });
 };
