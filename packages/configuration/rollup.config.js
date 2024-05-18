b33061fdb55edda900bb8ebfb4f37e8177ec8874
Q16solver <jimmychen12138@gmail.com>
2024-05-18 23:34:25 +1000

    fix: add back typescript plugin

--- packages/configuration/rollup.config.js
+++ packages/configuration/rollup.config.js
@@ -1,6 +1,8 @@
+import typescript from "@rollup/plugin-typescript";
 import { defineConfig } from "rollup";
 
 export default defineConfig({
   input: "src/index.ts",
   output: { file: "dist/index.js", format: "esm" },
+  plugins: [typescript()],
 });
