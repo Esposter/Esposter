2082212701c7d881ef0ccebbe0539ef29e9ce793
Jimmy Chen <jimmyc@resonatesolutions.com.au>
2024-05-27 15:27:04 +1000

    fix: reference the function

--- packages/app/services/dashboard/chart/GetVisualTypeChartConfigurationMap.ts
+++ packages/app/services/dashboard/chart/GetVisualTypeChartConfigurationMap.ts
@@ -1,10 +1,12 @@
 import { VisualType } from "@/models/dashboard/VisualType";
 import type { ChartConfiguration } from "@/models/dashboard/chart/ChartConfiguration";
 import { AreaChartConfiguration } from "@/models/dashboard/chart/area/AreaChartConfiguration";
+import { ColumnChartType } from "@/models/dashboard/chart/column/ColumnChartType";
+import { GetColumnChartTypeConfigurationMap } from "@/models/dashboard/chart/column/GetColumnChartTypeConfigurationMap";
 import { LineChartConfiguration } from "@/models/dashboard/chart/line/LineChartConfiguration";
 
 export const GetVisualTypeChartConfigurationMap = {
   [VisualType.Area]: () => new AreaChartConfiguration(),
-  [VisualType.Column]: () => new ColumnChartConfiguration(),
+  [VisualType.Column]: GetColumnChartTypeConfigurationMap[ColumnChartType.Basic],
   [VisualType.Line]: () => new LineChartConfiguration(),
 } as const satisfies Record<VisualType, () => ChartConfiguration>;
