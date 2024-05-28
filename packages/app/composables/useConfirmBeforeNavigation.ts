bc0019ce82a34a99e19a59fdbd1a44023d109f5c
Jimmy Chen <jimmyc@resonatesolutions.com.au>
2024-05-28 12:35:30 +1000

    fix: add back return value

--- packages/app/composables/useConfirmBeforeNavigation.ts
+++ packages/app/composables/useConfirmBeforeNavigation.ts
@@ -1,10 +1,14 @@
 export const useConfirmBeforeNavigation = (isDirty: Ref<boolean>) => {
   onBeforeRouteLeave(() => {
+    console.log(isDirty.value);
     if (isDirty.value && !window.confirm("Changes that you made may not be saved.")) return false;
   });
 
-  useEventListener("beforeunload", ({ preventDefault }) => {
+  useEventListener("beforeunload", (e) => {
     if (!isDirty.value) return;
-    preventDefault();
+    e.preventDefault();
+    // Even though the docs tell us that it's deprecated,
+    // this actually makes the window prompt for reloading pop up
+    e.returnValue = false;
   });
 };
