dfc909f1f3f372c10a5135a0dee456c769f37023
Q16solver <jimmychen12138@gmail.com>
2024-05-28 17:48:26 +1000

    fix: use mouse events to determine drag/click

--- packages/app/components/Dashboard/Visual/Preview/Index.vue
+++ packages/app/components/Dashboard/Visual/Preview/Index.vue
@@ -8,10 +8,29 @@ interface VisualPreviewProps {
 
 const { type } = defineProps<VisualPreviewProps>();
 const emit = defineEmits<{ click: [] }>();
+const isDrag = ref(false);
+const isMouseDown = ref(false);
 </script>
 
 <template>
   <Transition name="fade">
-    <component :is="VisualTypeDemoComponentMap[type]" size="90%" @click="emit('click')" />
+    <component
+      :is="VisualTypeDemoComponentMap[type]"
+      size="90%"
+      @mousedown="isMouseDown = true"
+      @mouseup="
+        () => {
+          isMouseDown = false;
+          if (isDrag) isDrag = false;
+          else emit('click');
+        }
+      "
+      @mousemove="
+        () => {
+          if (!isMouseDown) return;
+          isDrag = true;
+        }
+      "
+    />
   </Transition>
 </template>
