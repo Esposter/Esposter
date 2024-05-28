e359053829964c80bebb47c688d3455432fb6134
Q16solver <jimmychen12138@gmail.com>
2024-05-28 19:16:10 +1000

    refactor: convert data to map for complete type-safety

--- packages/app/services/tableEditor/tableEditorTypeItemCategoryDefinitions.ts
+++ packages/app/services/tableEditor/tableEditorTypeItemCategoryDefinitions.ts
@@ -1,14 +1,17 @@
 import { TableEditorType } from "@/models/tableEditor/TableEditorType";
 import type { SelectItemCategoryDefinition } from "@/models/vuetify/SelectItemCategoryDefinition";
 import { getTableEditorTitle } from "@/services/tableEditor/getTableEditorTitle";
+import type { Except } from "type-fest";
+import { parseDictionaryToArray } from "~/util/parseDictionaryToArray";
 
-export const tableEditorTypeItemCategoryDefinitions: SelectItemCategoryDefinition<TableEditorType>[] = [
-  {
-    value: TableEditorType.TodoList,
+const TableEditorTypeItemCategoryDefinitionMap = {
+  [TableEditorType.TodoList]: {
     title: getTableEditorTitle(TableEditorType.TodoList),
   },
-  {
-    value: TableEditorType.VuetifyComponent,
+  [TableEditorType.VuetifyComponent]: {
     title: getTableEditorTitle(TableEditorType.VuetifyComponent),
   },
-];
+} as const satisfies Record<TableEditorType, Except<SelectItemCategoryDefinition<TableEditorType>, "value">>;
+
+export const tableEditorTypeItemCategoryDefinitions: SelectItemCategoryDefinition<TableEditorType>[] =
+  parseDictionaryToArray(TableEditorTypeItemCategoryDefinitionMap, "value");
--- packages/app/services/tableEditor/vuetifyComponent/vuetifyComponentItemTypeItemCategoryDefinitions.ts
+++ packages/app/services/tableEditor/vuetifyComponent/vuetifyComponentItemTypeItemCategoryDefinitions.ts
@@ -1,14 +1,18 @@
 import type { ItemCategoryDefinition } from "@/models/tableEditor/ItemCategoryDefinition";
 import { VuetifyComponentItem } from "@/models/tableEditor/vuetifyComponent/VuetifyComponentItem";
 import { VuetifyComponentItemType } from "@/models/tableEditor/vuetifyComponent/VuetifyComponentItemType";
+import { parseDictionaryToArray } from "@/util/parseDictionaryToArray";
 import { prettifyName } from "@/util/text/prettifyName";
+import type { Except } from "type-fest";
 
-export const vuetifyComponentItemTypeItemCategoryDefinitions: ItemCategoryDefinition<VuetifyComponentItem>[] = [
-  {
-    value: VuetifyComponentItemType.VuetifyComponent,
+const VuetifyComponentItemTypeItemCategoryDefinitionMap = {
+  [VuetifyComponentItemType.VuetifyComponent]: {
     title: prettifyName(VuetifyComponentItemType.VuetifyComponent),
     icon: "mdi-vuetify",
     targetTypeKey: "type",
     create: () => new VuetifyComponentItem(),
   },
-];
+} as const satisfies Record<VuetifyComponentItemType, Except<ItemCategoryDefinition<VuetifyComponentItem>, "value">>;
+
+export const vuetifyComponentItemTypeItemCategoryDefinitions: ItemCategoryDefinition<VuetifyComponentItem>[] =
+  parseDictionaryToArray(VuetifyComponentItemTypeItemCategoryDefinitionMap, "value");
