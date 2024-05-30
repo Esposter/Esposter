10736fe4207396ab2f1a437ced5b54d7c8ad7296
Jimmy Chen <jimmyc@resonatesolutions.com.au>
2024-05-30 15:47:40 +1000

    refactor: remove _

--- packages/app/models/resolvers/dashboard/AChartConfigurationResolver.ts
+++ packages/app/models/resolvers/dashboard/AChartConfigurationResolver.ts
@@ -2,7 +2,7 @@ import type { Chart } from "@/models/dashboard/chart/Chart";
 import type { ApexOptions } from "apexcharts";
 
 export abstract class AChartConfigurationResolver<T extends Chart["configuration"]> {
-  handleConfiguration(_apexOptions: ApexOptions, _configuration: T): ApexOptions {
-    return _apexOptions;
+  handleConfiguration(apexOptions: ApexOptions, _configuration: T): ApexOptions {
+    return apexOptions;
   }
 }
