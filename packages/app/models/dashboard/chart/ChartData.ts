61d7d079f619c6c6ff34ddad3e9865a4241200c8
Jimmy Chen <jimmyc@resonatesolutions.com.au>
2024-05-30 16:25:14 +1000

    fix: improve types

--- packages/app/models/dashboard/chart/ChartData.ts
+++ packages/app/models/dashboard/chart/ChartData.ts
@@ -1,9 +1,9 @@
 import type { Chart } from "@/models/dashboard/chart/Chart";
+import type { JSONSchema } from "@/models/jsonSchema/JSONSchema";
 import type { AChartConfigurationResolver } from "@/models/resolvers/dashboard/AChartConfigurationResolver";
-import type { JsonSchema7Type } from "zod-to-json-schema";
 
 export interface ChartData<T extends Chart["configuration"]> {
   getInitialConfiguration: () => T;
-  schema: JsonSchema7Type;
+  schema: JSONSchema<T>;
   resolver: AChartConfigurationResolver<T>;
 }
