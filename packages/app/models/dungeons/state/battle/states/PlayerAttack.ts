997a557fe4449ecb501d58a7414d18ed811dfd0a
Jimmy Chen <jimmyc@resonatesolutions.com.au>
2024-06-20 11:00:28 +1000

    refactor: move monster stat-baesd calcs to monster folder

--- packages/app/models/dungeons/state/battle/states/PlayerAttack.ts
+++ packages/app/models/dungeons/state/battle/states/PlayerAttack.ts
@@ -1,8 +1,8 @@
 import type { State } from "@/models/dungeons/state/State";
 import { StateName } from "@/models/dungeons/state/battle/StateName";
 import { dayjs } from "@/services/dayjs";
+import { calculateDamage } from "@/services/dungeons/monster/calculateDamage";
 import { battleStateMachine } from "@/services/dungeons/scene/battle/battleStateMachine";
-import { calculateDamage } from "@/services/dungeons/scene/battle/calculateDamage";
 import { useBattleDialogStore } from "@/store/dungeons/battle/dialog";
 import { useEnemyStore } from "@/store/dungeons/battle/enemy";
 import { useBattlePlayerStore } from "@/store/dungeons/battle/player";
