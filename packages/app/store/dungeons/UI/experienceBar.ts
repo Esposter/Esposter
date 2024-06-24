b25846ed1f768013488db14f25f7180e5619c649
Q16solver <jimmychen12138@gmail.com>
2024-06-24 20:19:31 +1000

    chore: remove unnecessary isAnimating

--- packages/app/store/dungeons/UI/experienceBar.ts
+++ packages/app/store/dungeons/UI/experienceBar.ts
@@ -2,6 +2,5 @@ import type { Tweens } from "phaser";
 
 export const useExperienceBarStore = defineStore("dungeons/UI/experienceBar", () => {
   const tween = ref<Tweens.Tween>();
-  const isAnimating = ref(false);
-  return { tween, isAnimating };
+  return { tween };
 });
