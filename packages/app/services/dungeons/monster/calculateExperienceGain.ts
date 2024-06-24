51dcb7de465c4c7db7c2f08f21fca4596b2d4b59
Q16solver <jimmychen12138@gmail.com>
2024-06-24 22:54:57 +1000

    fix: revert back exp

--- packages/app/services/dungeons/monster/calculateExperienceGain.ts
+++ packages/app/services/dungeons/monster/calculateExperienceGain.ts
@@ -1,1 +1,1 @@
-export const calculateExperienceGain = (baseExp: number, enemyLevel: number) => Math.round(baseExp * enemyLevel * 100);
+export const calculateExperienceGain = (baseExp: number, enemyLevel: number) => Math.round((baseExp * enemyLevel) / 7);
