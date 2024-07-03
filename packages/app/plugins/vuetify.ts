05c703f95797e2f48eebab59d9aa4a902f1626a8
Jimmy Chen <jimmyc@resonatesolutions.com.au>
2024-07-03 16:59:00 +1000

    fix: prop type

--- packages/app/plugins/vuetify.ts
+++ packages/app/plugins/vuetify.ts
@@ -1,12 +1,11 @@
 import { IconComponentMap } from "@/services/dungeons/IconComponentMap";
-import type { IconProps } from "vuetify";
 
 export default defineNuxtPlugin((nuxtApp) => {
   nuxtApp.hook("vuetify:configuration", ({ vuetifyOptions }) => {
     vuetifyOptions.icons = {
       sets: {
         custom: {
-          component: (props: IconProps) => h(props.tag, [h(IconComponentMap[props.icon], { class: "v-icon__svg" })]),
+          component: (props) => h(props.tag, [h(IconComponentMap[props.icon as string], { class: "v-icon__svg" })]),
         },
       },
     };
