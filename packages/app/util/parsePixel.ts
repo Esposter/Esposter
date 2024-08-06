55fbf7e8c670217aaa3ac73443c50966fdf4ce54
Jimmy Chen <jimmyc@resonatesolutions.com.au>
2024-08-06 13:06:36 +1000

    chore: Add TODO

--- packages/app/util/parsePixel.ts
+++ packages/app/util/parsePixel.ts
@@ -1,2 +1,4 @@
+// @TODO: Remove this in next phaser release
+// https://github.com/phaserjs/phaser/issues/6863
 export const parsePixel = (pixel: number | string) =>
   typeof pixel === "number" ? pixel : Number(pixel.replace(/px$/, ""));
