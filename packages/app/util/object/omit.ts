9cad3b36cfbbb22a090f4e27fbddc969b9a709ad
Jimmy Chen <jimmyc@resonatesolutions.com.au>
2024-08-27 14:55:37 +1000

    fix: types

--- packages/app/util/object/omit.ts
+++ packages/app/util/object/omit.ts
@@ -1,7 +1,7 @@
 import type { Except, TupleToUnion } from "type-fest";
 
-export const omit = <T extends object>(object: T, keys: (keyof T)[]) =>
-  Object.fromEntries(Object.entries(object).filter(([k]) => !keys.includes(k as keyof T))) as Except<
-    T,
-    TupleToUnion<typeof keys>
-  >;
+export const omit = <T extends object, TKeys extends (keyof T)[]>(
+  object: T,
+  keys: TKeys,
+): Except<T, TupleToUnion<TKeys>> =>
+  Object.fromEntries(Object.entries(object).filter(([k]) => !keys.includes(k as keyof T)));
