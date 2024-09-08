76a4d87d492049fff99c3c4c7bbd46a7c0911e11
Q16solver <jimmychen12138@gmail.com>
2024-09-08 12:13:20 +1000

    fix: remove now unnecessary rule

--- packages/app/uno.config.ts
+++ packages/app/uno.config.ts
@@ -1,7 +1,6 @@
 import { defineConfig } from "unocss";
 
 export default defineConfig({
-  rules: [["break-word", { "word-break": "break-word" }]],
   theme: {
     fontFamily: {
       inter: ["Inter"],
