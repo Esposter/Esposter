de5c87bc33d7b321dedfa8f9537d5ad95d4a942d
Q16solver <jimmychen12138@gmail.com>
2024-09-14 16:05:28 +1000

    fix: use cross-platform-compatible scripts

--- scripts/build.ps1
+++
@@ -1,3 +0,0 @@
-Invoke-Expression "pnpm lerna run build --ignore @esposter/app"
-Invoke-Expression "pnpm run docs"
-Invoke-Expression "pnpm lerna run build --scope=@esposter/app"
--- scripts/refreshLockfile.ps1
+++
@@ -1,3 +0,0 @@
-Remove-Item "pnpm-lock.yaml"
-Get-ChildItem -Filter "node_modules" -Recurse -Force | Remove-Item -Recurse -Force
-Invoke-Expression "pnpm i"
