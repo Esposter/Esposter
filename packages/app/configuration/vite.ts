76e5feb417f7638674ad829ce499fafccde76dc4
Jimmy Chen <jimmyc@resonatesolutions.com.au>
2024-09-24 09:12:52 +1000

    chore: Update dependencies && update some vjsf deps

--- packages/app/configuration/vite.ts
+++ packages/app/configuration/vite.ts
@@ -1,6 +1,5 @@
 import type { NuxtConfig } from "nuxt/schema";
 
-// @ts-expect-error We can ignore these configuration files as they will error at build time if they're wrong
 import { commonjsDeps } from "@koumoul/vjsf/utils/build.js";
 import { nodePolyfills } from "vite-plugin-node-polyfills";
 
@@ -9,6 +8,9 @@ export const vite: NuxtConfig["vite"] = {
     // Do not inline images and assets to avoid the phaser error
     // "Local data URIs are not supported"
     assetsInlineLimit: 0,
+    commonjsOptions: {
+      transformMixedEsModules: true,
+    },
   },
   css: {
     preprocessorOptions: {
