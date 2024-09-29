3f478d8cb948c7853f152d6b017694f314695f7b
Jimmy Chen <jimmyc@resonatesolutions.com.au>
2024-09-30 09:53:57 +1000

    fix: resetting viewport

--- packages/app/components/FlowchartEditor/Controls.vue
+++ packages/app/components/FlowchartEditor/Controls.vue
@@ -1,15 +1,18 @@
 <script setup lang="ts">
+import { DEFAULT_VIEWPORT_TRANSFORM } from "@/services/flowchartEditor/constants";
 import { ControlButton, Controls } from "@vue-flow/controls";
 import { useVueFlow } from "@vue-flow/core";
+import deepEqual from "fast-deep-equal";
 
-const { setViewport } = useVueFlow();
+const { setViewport, viewport } = useVueFlow();
 const { surface, surfaceOpacity80, text } = useColors();
+const disabled = computed(() => deepEqual(viewport.value, DEFAULT_VIEWPORT_TRANSFORM));
 </script>
 
 <template>
   <Controls position="top-right">
-    <ControlButton title="Reset Transform" @click="setViewport({ x: 0, y: 0, zoom: 1 })">
-      <v-icon icon="mdi-cached" size="small" />
+    <ControlButton title="Default Viewport" :disabled @click="setViewport(DEFAULT_VIEWPORT_TRANSFORM)">
+      <v-icon :op="disabled ? 40 : undefined" icon="mdi-home" size="x-small" />
     </ControlButton>
   </Controls>
 </template>
--- packages/app/services/flowchartEditor/constants.ts
+++ packages/app/services/flowchartEditor/constants.ts
@@ -1,4 +1,7 @@
+import type { ViewportTransform } from "@vue-flow/core";
+
 export const SAVE_FILENAME = "save.json";
 export const FLOWCHART_EDITOR_LOCAL_STORAGE_KEY = "flowchart-editor-store";
 
 export const DEFAULT_NODE_TYPE = "base";
+export const DEFAULT_VIEWPORT_TRANSFORM: ViewportTransform = { x: 0, y: 0, zoom: 1 };
