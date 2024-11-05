891bca5e2485a07432d0a2036c6fd6b4c5175d2f
Q16solver <jimmychen12138@gmail.com>
2024-11-05 14:47:30 +1100

    chore: Add comment

--- packages/app/server/plugins/migrate.ts
+++ packages/app/server/plugins/migrate.ts
@@ -1,6 +1,7 @@
 import { MIGRATIONS_FOLDER_PATH } from "@/server/db/constants";
 import { migrate } from "drizzle-orm/postgres-js/migrator";
 // @TODO: Make this async in nitro v3
+// https://github.com/nitrojs/nitro/issues/915
 export default defineNitroPlugin(() => {
   const db = useDb();
   void migrate(db, { migrationsFolder: MIGRATIONS_FOLDER_PATH });
