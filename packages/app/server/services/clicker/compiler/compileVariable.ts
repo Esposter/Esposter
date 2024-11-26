2ef3eae85f21609e027b7010fb008f133ea90c60
Jimmy Chen <jimmyc@resonatesolutions.com.au>
2024-11-26 16:18:02 +1100

    refactor: move clicker compiler

--- packages/app/server/services/clicker/compiler/compileVariable.ts
+++ packages/app/server/services/clicker/compiler/compileVariable.ts
@@ -1,6 +1,6 @@
 import type { ClickerItemProperties } from "@/models/clicker/ClickerItemProperties";
 
-import { VARIABLE_IDENTIFIER } from "@/services/clicker/constants";
+import { VARIABLE_IDENTIFIER } from "@/shared/services/clicker/constants";
 
 export const compileVariable = (property: keyof ClickerItemProperties) =>
-  `${VARIABLE_IDENTIFIER}${property as string}${VARIABLE_IDENTIFIER}`;
+  `${VARIABLE_IDENTIFIER}${property}${VARIABLE_IDENTIFIER}`;
