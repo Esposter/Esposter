84aa70845af402d6702da7bcba2273c594bbf334
Jimmy Chen <jimmyc@resonatesolutions.com.au>
2025-01-07 10:25:43 +1100

    fix: remove devalue

--- packages/app/shared/services/trpc/transformer.ts
+++ packages/app/shared/services/trpc/transformer.ts
@@ -1,11 +1,3 @@
 import { SuperJSON } from "#shared/services/superjson";
-import { uneval } from "devalue";
 
-export const transformer = {
-  input: SuperJSON,
-  output: {
-    // This `eval` only ever happens on the **client**
-    deserialize: (object: unknown) => eval(`(${object})`),
-    serialize: (object: unknown) => uneval(object),
-  },
-};
+export const transformer = SuperJSON;
