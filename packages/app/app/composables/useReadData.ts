bd31d3360098cfdde298182090dd040a633643c6
Jimmy Chen <jimmyc@resonatesolutions.com.au>
2025-01-08 12:01:39 +1100

    fix: watch for new session data instead

--- packages/app/app/composables/useReadData.ts
+++ packages/app/app/composables/useReadData.ts
@@ -3,10 +3,13 @@ import { authClient } from "@/services/auth/authClient";
 export const useReadData = async (unauthedReader: () => void, authedReader: () => Promise<void>) => {
   const session = authClient.useSession();
 
-  watch(session, async (newSession) => {
-    if (newSession.data) await authedReader();
-    else unauthedReader();
-  });
+  watch(
+    () => session.value.data,
+    async (newSessionData) => {
+      if (newSessionData) await authedReader();
+      else unauthedReader();
+    },
+  );
 
   if (session.value.data) await authedReader();
   // We'll assume that not being authenticated means that we will read from local storage for data
