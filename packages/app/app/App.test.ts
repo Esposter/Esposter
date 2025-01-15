0d7d5c69626221b9574b22f5eb625a7b38faa44b
Jimmy Chen <jimmyc@resonatesolutions.com.au>
2025-01-15 21:53:10 +1100

    feat + refactor: move to more generic grabbing vue files and snapshotting

---
+++ packages/app/app/App.test.ts
@@ -0,0 +1,24 @@
+import { mountSuspended } from "@nuxt/test-utils/runtime";
+import { describe, expect, test } from "vitest";
+import { createVuetify } from "vuetify";
+
+describe("app", () => {
+  test("snapshots", async () => {
+    expect.hasAssertions();
+
+    const componentFilepathEntries = Object.entries<Record<string, Component>>(
+      import.meta.glob("@/components/About/*.vue", { eager: true, import: "default" }),
+    );
+
+    for (const [filepath, component] of componentFilepathEntries) {
+      const mountedComponent = await mountSuspended(component, {
+        global: {
+          plugins: [createVuetify()],
+        },
+      });
+      const filename = filepath.replace(/\.[^/.]+$/, "");
+
+      await expect(mountedComponent.html()).toMatchFileSnapshot(`__snapshots__/${filename}.html`);
+    }
+  });
+});
