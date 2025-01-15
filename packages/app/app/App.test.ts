a1421f69f7430d5c1f6e6bbd9c437b9339e780d6
Q16solver <jimmychen12138@gmail.com>
2025-01-15 23:08:17 +1100

    feat: Add more snapshots

--- packages/app/app/App.test.ts
+++ packages/app/app/App.test.ts
@@ -3,18 +3,31 @@ import { mountSuspended } from "@nuxt/test-utils/runtime";
 import { describe, expect, test } from "vitest";
 import { createVuetify } from "vuetify";
 
+const vuetify = createVuetify();
+
 describe("app", () => {
   test("snapshots", async () => {
     expect.hasAssertions();
 
     const componentFilepathEntries = Object.entries(
-      import.meta.glob<Component>("@/components/About/*.vue", { eager: true, import: "default" }),
+      import.meta.glob<Component>(
+        [
+          "@/components/About/**/*.vue",
+          "@/components/Anime/**/*.vue",
+          "@/components/Nuxt/**/*.vue",
+          "@/components/Transition/**/*.vue",
+        ],
+        {
+          eager: true,
+          import: "default",
+        },
+      ),
     );
 
     for (const [filepath, component] of componentFilepathEntries) {
       const mountedComponent = await mountSuspended(component, {
         global: {
-          plugins: [createVuetify()],
+          plugins: [vuetify],
         },
       });
       const filename = trimFileExtension(filepath);
