95a08366c6f7857bd4982f5a3fe98f46908ade81
Q16solver <jimmychen12138@gmail.com>
2025-01-17 23:07:43 +1100

    refactor: remove unnecessary const

--- packages/app/server/trpc/routers/user.ts
+++ packages/app/server/trpc/routers/user.ts
@@ -1,5 +1,4 @@
 import { AzureContainer } from "#shared/models/azure/blob/AzureContainer";
-import { PROFILE_FILENAME } from "#shared/services/user/constants";
 import { router } from "@@/server/trpc";
 import { authedProcedure } from "@@/server/trpc/procedure/authedProcedure";
 import { useContainerClient } from "@@/server/util/azure/useContainerClient";
@@ -8,7 +7,7 @@ import { z } from "zod";
 export const userRouter = router({
   uploadProfileImage: authedProcedure.input(z.instanceof(ArrayBuffer)).mutation(async ({ ctx, input }) => {
     const containerClient = await useContainerClient(AzureContainer.UserAssets);
-    const blobName = `${ctx.session.user.id}/${PROFILE_FILENAME}`;
+    const blobName = `${ctx.session.user.id}/profile`;
     const blockBlobClient = containerClient.getBlockBlobClient(blobName);
     await blockBlobClient.uploadData(input);
     return blockBlobClient.url;
--- packages/app/shared/services/user/constants.ts
+++ packages/app/shared/services/user/constants.ts
@@ -1,3 +1,1 @@
 export const USER_NAME_MAX_LENGTH = 100;
-
-export const PROFILE_FILENAME = "profile";
