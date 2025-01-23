29c2611c4fe5a34036cc5d36411076462acacde8
Jimmy Chen <jimmyc@resonatesolutions.com.au>
2025-01-23 11:49:16 +1100

    chore: Update dependencies

--- packages/app/configuration/compatibilityDate.ts
+++ packages/app/configuration/compatibilityDate.ts
@@ -1,3 +1,3 @@
 import type { NuxtConfig } from "nuxt/schema";
 
-export const compatibilityDate: NuxtConfig["compatibilityDate"] = "2025-01-06";
+export const compatibilityDate: NuxtConfig["compatibilityDate"] = "2025-01-23";
