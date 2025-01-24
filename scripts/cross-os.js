aa16467386aeb5e4ed301b9fa4ec34d88b0e66b7
Jimmy Chen <jimmyc@resonatesolutions.com.au>
2025-01-24 14:17:54 +1100

    fix: directly use process cwd

--- scripts/cross-os.js
+++ scripts/cross-os.js
@@ -1,4 +1,4 @@
-import { exec, spawn } from "node:child_process";
+import { spawn } from "node:child_process";
 import { createRequire } from "node:module";
 import path from "node:path";
 
@@ -12,11 +12,7 @@ const script = process.argv[2];
 const args = process.argv.slice(3, process.argv.length);
 const { platform } = process;
 const require = createRequire(import.meta.url);
-const packageJson = await new Promise((resolve) =>
-  exec("pnpm prefix").stdout.on("data", (buffer) => {
-    resolve(require(path.resolve(buffer.toString("utf8").trim(), "package.json")));
-  }),
-);
+const packageJson = require(path.resolve(process.cwd(), "package.json"));
 const command = packageJson[property][script][platform];
 if (!command) throw new Error(`script: "${script}" not found for the current platform: ${platform}`);
 
