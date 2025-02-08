594c651536c0396f764f6e9b6f9e477629aabc2a
Q16solver <jimmychen12138@gmail.com>
2025-02-08 11:41:08 +1100

    feat: Add countries

--- packages/app/app/components/Visual/Globe.vue
+++ packages/app/app/components/Visual/Globe.vue
@@ -4,8 +4,9 @@ import type { ArrayElement } from "type-fest/source/internal";
 
 import { dayjs } from "#shared/services/dayjs";
 import { generateRandomInteger } from "#shared/util/math/random/generateRandomInteger";
+import countries from "@/assets/about/countries.json";
 import data from "@/assets/about/data.json";
-import countries from "@/assets/about/globe.json";
+import { features } from "@/assets/about/globe.json";
 import { ARC_STROKES, COLORS } from "@/services/visual/constants";
 import { AmbientLight, Color, DirectionalLight, Fog, PerspectiveCamera, PointLight, Scene, WebGLRenderer } from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
@@ -62,7 +63,7 @@ onMounted(async () => {
 
   const ThreeGlobe = (await import("three-globe")).default;
   const globe = new ThreeGlobe({ animateIn: true, waitForGlobeReady: true })
-    .hexPolygonsData(countries.features)
+    .hexPolygonsData(features)
     .hexPolygonResolution(3)
     .hexPolygonMargin(0.7)
     .showAtmosphere(true)
@@ -82,9 +83,17 @@ onMounted(async () => {
     .arcStroke(() => ARC_STROKES[generateRandomInteger(ARC_STROKES.length - 1)])
     .arcDashLength(0.9)
     .arcDashInitialGap((e) => (e as Data).order)
-    .arcDashGap(15)
+    .arcDashGap(4)
     .arcDashAnimateTime(dayjs.duration(2, "second").asMilliseconds())
     .arcsTransitionDuration(dayjs.duration(2, "second").asMilliseconds())
+    .labelsData(countries)
+    .labelColor(() => "#fff")
+    .labelDotOrientation(() => "right")
+    .labelDotRadius(0.3)
+    .labelSize(() => 1)
+    .labelText("name")
+    .labelResolution(6)
+    .labelAltitude(0.01)
     .pointsData(data)
     .pointColor(() => COLORS[generateRandomInteger(COLORS.length - 1)])
     .pointsMerge(true)
