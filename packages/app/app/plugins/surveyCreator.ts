597b00b197b93e815ab1617511373139dd1fb9d9
Q16solver <jimmychen12138@gmail.com>
2025-03-08 14:52:41 +1100

    chore: migrate to pnpm catalogs

--- packages/app/app/plugins/surveyCreator.ts
+++ packages/app/app/plugins/surveyCreator.ts
@@ -2,6 +2,6 @@
 // import { surveyCreatorPlugin } from "survey-creator-vue";
 // import { surveyPlugin } from "survey-vue3-ui";
 
-// export default defineNuxtPlugin((nuxtApp) => {
-//   nuxtApp.vueApp.use(surveyPlugin).use(surveyCreatorPlugin);
-// });
+export default defineNuxtPlugin(() => {
+  // nuxtApp.vueApp.use(surveyPlugin).use(surveyCreatorPlugin);
+});
