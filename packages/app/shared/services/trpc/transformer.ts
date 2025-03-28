4c59c7a1b669735f0a64e824cf6cf26ee83c78c2
Jimmy Chen <jimmyc@resonatesolutions.com.au>
2025-03-28 13:59:44 +1100

    chore: just use string overload

--- packages/app/shared/services/trpc/transformer.ts
+++ packages/app/shared/services/trpc/transformer.ts
@@ -1,6 +1,6 @@
 import { JSONClasses } from "#shared/services/superjson/JSONClasses";
 import { SuperJSON } from "superjson";
 
-for (const { cls, name } of JSONClasses) SuperJSON.registerClass(cls, { identifier: name });
+for (const { cls, name } of JSONClasses) SuperJSON.registerClass(cls, name);
 
 export const transformer = SuperJSON;
