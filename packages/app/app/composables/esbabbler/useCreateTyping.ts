70ec1eb56dea238f00727ed6a6f1d4469f55f49d
Jimmy Chen <jimmyc@resonatesolutions.com.au>
2025-04-08 12:36:59 +1000

    refactor: throttle the ref value change instead

--- packages/app/app/composables/esbabbler/useCreateTyping.ts
+++ packages/app/app/composables/esbabbler/useCreateTyping.ts
@@ -6,20 +6,18 @@ import { useRoomStore } from "@/store/esbabbler/room";
 export const useCreateTyping = () => {
   const session = authClient.useSession();
   const { $trpc } = useNuxtApp();
-  const messageInputStore = useMessageInputStore();
-  const { messageInput } = storeToRefs(messageInputStore);
   const roomStore = useRoomStore();
   const { currentRoomId } = storeToRefs(roomStore);
-  const throttledCreateTyping = useThrottleFn(async () => {
+  const messageInputStore = useMessageInputStore();
+  const { messageInput } = storeToRefs(messageInputStore);
+  const throttledMessageInput = useThrottle(messageInput, dayjs.duration(1, "second").asMilliseconds());
+
+  watch(throttledMessageInput, async () => {
     if (!(currentRoomId.value && session.value.data)) return;
     await $trpc.message.createTyping.query({
       roomId: currentRoomId.value,
       userId: session.value.data.user.id,
       username: session.value.data.user.name,
     });
-  }, dayjs.duration(1, "second").asMilliseconds());
-
-  watch(messageInput, async () => {
-    await throttledCreateTyping();
   });
 };
