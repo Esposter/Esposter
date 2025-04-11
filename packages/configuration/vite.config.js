e2511276c6320eff5afc61b2266b3398f4844301
Jimmy Chen <50786287+Q16solver@users.noreply.github.com>
2025-04-11 21:51:25 +1000

    Update vite.config.js

--- packages/configuration/vite.config.js
+++ packages/configuration/vite.config.js
@@ -5,7 +5,6 @@ import dts from "vite-plugin-dts";
 import mkcert from "vite-plugin-mkcert";
 import { nodePolyfills } from "vite-plugin-node-polyfills";
 import tsconfigPaths from "vite-tsconfig-paths";
-
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
