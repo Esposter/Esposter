d46189710b2f807684de21081c0c0a3dcff10804
Q16solver <jimmychen12138@gmail.com>
2025-04-13 21:24:21 +1000

    fix: no need for undefined

--- packages/parse-tmx/src/util/parseProperties.ts
+++ packages/parse-tmx/src/util/parseProperties.ts
@@ -1,11 +1,9 @@
 import type { TMXPropertyNode } from "@/models/tmx/node/TMXPropertyNode";
 import type { TMXPropertiesParsed } from "@/models/tmx/parsed/TMXPropertiesParsed";
 
-export const parseProperties = (properties?: { property: TMXPropertyNode[] }[]): TMXPropertiesParsed | undefined =>
-  properties
-    ? properties.map(({ property }) => {
-        const { $, _ } = property[0];
-        const { name, value } = $;
-        return { name, value: value ?? _ };
-      })
-    : undefined;
+export const parseProperties = (properties: { property: TMXPropertyNode[] }[]): TMXPropertiesParsed =>
+  properties.map(({ property }) => {
+    const { $, _ } = property[0];
+    const { name, value } = $;
+    return { name, value: value ?? _ };
+  });
