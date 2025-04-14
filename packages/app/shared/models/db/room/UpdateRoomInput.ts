24f04f9b91ecee1ea1f6ef191ad52cb073a133c0
Jimmy Chen <jimmyc@resonatesolutions.com.au>
2025-04-14 12:09:04 +1000

    refactor: use partial near the end

--- packages/app/shared/models/db/room/UpdateRoomInput.ts
+++ packages/app/shared/models/db/room/UpdateRoomInput.ts
@@ -4,5 +4,5 @@ import { selectRoomSchema } from "#shared/db/schema/rooms";
 
 export const updateRoomInputSchema = selectRoomSchema
   .pick({ id: true })
-  .merge(selectRoomSchema.partial().pick({ name: true }));
+  .merge(selectRoomSchema.pick({ name: true }).partial());
 export type UpdateRoomInput = z.infer<typeof updateRoomInputSchema>;
