abb3095fd6b7b9f38b8e20182b4d8f43c9b000b3
Q16solver <jimmychen12138@gmail.com>
2025-04-15 21:11:41 +1000

    fix: update check to be stricter

--- packages/app/app/composables/esbabbler/useRefreshMentions.ts
+++ packages/app/app/composables/esbabbler/useRefreshMentions.ts
@@ -11,7 +11,7 @@ export const useRefreshMentions = (message: MaybeRefOrGetter<string>) => {
     for (const mention of mentions) {
       const memberId = mention.getAttribute("data-id");
       const member = members.value.find(({ id }) => id === memberId);
-      if (!member?.name) continue;
+      if (!member?.name || member.name === mention.textContent.substring(1)) continue;
 
       mention.textContent = `@${member.name}`;
       mention.setAttribute("data-label", member.name);
