d9c66cc4c19222c6985f3c23b44ecea47b404298
Q16solver <jimmychen12138@gmail.com>
2025-04-17 08:59:40 +1000

    chore: update some typedoc config

--- typedoc.config.js
+++ typedoc.config.js
@@ -4,7 +4,7 @@ config({ path: "packages/app/.env" });
 /** @type {Partial<import('typedoc').TypeDocOptions>} */
 const typedocConfiguration = {
   entryPoints: ["packages/*"],
-  name: "Documentation",
+  name: "Esposter",
   entryPointStrategy: "packages",
   packageOptions: {
     entryPoints: ["src/index.ts"],
@@ -12,7 +12,7 @@ const typedocConfiguration = {
   },
   exclude: ["packages/app", "packages/configuration"],
   out: "packages/app/public/docs",
-}
+};
 
 if (process.env.BASE_URL) {
   typedocConfiguration.hostedBaseUrl = `${process.env.BASE_URL}/docs`;
