6b563358f29aaf5e1b3440dfe70b54a8972e6102
Q16solver <jimmychen12138@gmail.com>
2025-04-18 12:49:17 +1000

    fix: don't use absolute path

--- typedoc.config.js
+++ typedoc.config.js
@@ -1,6 +1,3 @@
-import { config } from "dotenv";
-
-config({ path: "packages/app/.env" });
 /** @satisfies {import('typedoc').TypeDocOptions} */
 const typedocConfiguration = {
   entryPoints: ["packages/*"],
@@ -13,10 +10,5 @@ const typedocConfiguration = {
   exclude: ["packages/app", "packages/configuration"],
   out: "packages/app/public/docs",
 };
-
-if (process.env.BASE_URL) {
-  typedocConfiguration.hostedBaseUrl = `${process.env.BASE_URL}/docs`;
-  typedocConfiguration.useHostedBaseUrlForAbsoluteLinks = true;
-}
 
 export default typedocConfiguration;
