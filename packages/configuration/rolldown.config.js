f2e60baa41aacdf1d0f49ecfe0876312c7ad8f12
Q16solver <jimmychen12138@gmail.com>
2025-04-19 10:38:18 +1000

    feat: Add polyfills

--- packages/configuration/rolldown.config.js
+++ packages/configuration/rolldown.config.js
@@ -1,3 +1,4 @@
+import nodePolyfills from "@rolldown/plugin-node-polyfills";
 import { defineConfig } from "rolldown";
 import { dts } from "rolldown-plugin-dts";
 
@@ -5,7 +6,7 @@ export default defineConfig([
   {
     input: "src/index.ts",
     output: [{ dir: "dist", format: "es" }],
-    plugins: [dts({ tsconfig: "tsconfig.build.json" })],
+    plugins: [nodePolyfills(), dts({ tsconfig: "tsconfig.build.json" })],
     resolve: {
       tsconfigFilename: "tsconfig.build.json",
     },
