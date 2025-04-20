dfb299e975a25f46b38136bcf8b04b6fe38c1a8d
Q16solver <jimmychen12138@gmail.com>
2025-04-20 14:03:19 +1000

    perf: remove unnecessary vite plugin node polyfills

--- packages/configuration/vite.config.js
+++ packages/configuration/vite.config.js
@@ -3,7 +3,6 @@ import AutoImport from "unplugin-auto-import/vite";
 import { defineConfig } from "vite";
 import dts from "vite-plugin-dts";
 import mkcert from "vite-plugin-mkcert";
-import { nodePolyfills } from "vite-plugin-node-polyfills";
 import tsconfigPaths from "vite-tsconfig-paths";
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -20,7 +19,6 @@ export default defineConfig({
   plugins: [
     AutoImport({ dts: true, imports: ["vue", "pinia"] }),
     tsconfigPaths(),
-    nodePolyfills({ include: ["zlib"] }),
     vue(),
     // @TODO: https://github.com/qmhc/vite-plugin-dts/issues/302
     dts({ tsconfigPath: "tsconfig.build.json" }),
