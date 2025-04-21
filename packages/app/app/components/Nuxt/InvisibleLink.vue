f4257b416d4ef9a42c34cc05769a2d80a6535fdc
Q16solver <jimmychen12138@gmail.com>
2025-04-21 20:07:05 +1000

    fix: docs links

--- packages/app/app/components/Nuxt/InvisibleLink.vue
+++ packages/app/app/components/Nuxt/InvisibleLink.vue
@@ -1,5 +1,6 @@
 <script lang="ts">
-export default defineNuxtLink({ componentName: "NuxtInvisibleLink" });
+// @TODO: https://github.com/nuxt/nuxt/issues/31819
+export default defineNuxtLink({ componentName: "NuxtInvisibleLink", trailingSlash: "append" });
 </script>
 
 <style scoped lang="scss">
