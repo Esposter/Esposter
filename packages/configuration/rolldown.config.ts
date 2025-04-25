5a792c7c846f22aa1f1d67d9c379a8a202e17acc
Q16solver <jimmychen12138@gmail.com>
2025-04-25 22:27:16 +1000

    fix: remove rolldown plugin node polyfills for now

--- packages/configuration/rolldown.config.ts
+++ packages/configuration/rolldown.config.ts
@@ -1,6 +1,5 @@
 import type { ConfigExport } from "rolldown";
 
-import nodePolyfills from "@rolldown/plugin-node-polyfills";
 import { defineConfig } from "rolldown";
 import { dts } from "rolldown-plugin-dts";
 
@@ -8,7 +7,7 @@ const rolldownConfiguration: ConfigExport = defineConfig([
   {
     input: "src/index.ts",
     output: [{ dir: "dist", format: "es" }],
-    plugins: [nodePolyfills(), dts({ resolve: ["type-fest"], tsconfig: "tsconfig.build.json" })],
+    plugins: [dts({ resolve: ["type-fest"], tsconfig: "tsconfig.build.json" })],
     resolve: {
       tsconfigFilename: "tsconfig.build.json",
     },
