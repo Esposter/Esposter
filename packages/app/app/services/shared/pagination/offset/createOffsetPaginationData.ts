b0e6bd3907ff36b674b19ad8936239387a684e42
Q16solver <jimmychen12138@gmail.com>
2025-04-29 19:18:29 +1000

    fix: just re-implement same api to achieve best perf and elegance in API

--- packages/app/app/services/shared/pagination/offset/createOffsetPaginationData.ts
+++ packages/app/app/services/shared/pagination/offset/createOffsetPaginationData.ts
@@ -1,15 +1,33 @@
 import { OffsetPaginationData } from "#shared/models/pagination/offset/OffsetPaginationData";
 
 export const createOffsetPaginationData = <TItem>(baseItems?: Ref<TItem[]>) => {
+  if (baseItems) {
+    const hasMore = ref(false);
+
+    const initializeOffsetPaginationData = (data: OffsetPaginationData<TItem>) => {
+      baseItems.value = data.items;
+      hasMore.value = data.hasMore;
+    };
+    const resetOffsetPaginationData = () => {
+      baseItems.value = [];
+      hasMore.value = false;
+    };
+
+    return {
+      hasMore,
+      initializeOffsetPaginationData,
+      items: baseItems,
+      resetOffsetPaginationData,
+    };
+  }
+
   const offsetPaginationData = ref(new OffsetPaginationData()) as Ref<OffsetPaginationData<TItem>>;
-  const items =
-    baseItems ??
-    computed({
-      get: () => offsetPaginationData.value.items,
-      set: (items) => {
-        offsetPaginationData.value.items = items;
-      },
-    });
+  const items = computed({
+    get: () => offsetPaginationData.value.items,
+    set: (items) => {
+      offsetPaginationData.value.items = items;
+    },
+  });
   const hasMore = computed({
     get: () => offsetPaginationData.value.hasMore,
     set: (hasMore) => {
@@ -19,11 +37,9 @@ export const createOffsetPaginationData = <TItem>(baseItems?: Ref<TItem[]>) => {
 
   const initializeOffsetPaginationData = (data: OffsetPaginationData<TItem>) => {
     offsetPaginationData.value = data;
-    if (baseItems) baseItems.value = offsetPaginationData.value.items;
   };
   const resetOffsetPaginationData = () => {
     offsetPaginationData.value = new OffsetPaginationData<TItem>();
-    if (baseItems) baseItems.value = [];
   };
 
   return {
