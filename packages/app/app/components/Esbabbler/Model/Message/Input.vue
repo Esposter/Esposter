14e845c724620ecda636cf1d9a5dd9653670840b
Q16solver <jimmychen12138@gmail.com>
2025-05-10 15:35:45 +1000

    fix: make placeholder simple

--- packages/app/app/components/Esbabbler/Model/Message/Input.vue
+++ packages/app/app/components/Esbabbler/Model/Message/Input.vue
@@ -2,21 +2,13 @@
 import { MESSAGE_MAX_LENGTH } from "#shared/services/esbabbler/constants";
 import { getSynchronizedFunction } from "#shared/util/getSynchronizedFunction";
 import { getTypingMessage } from "@/services/esbabbler/getTypingMessage";
-import { useEsbabblerStore } from "@/store/esbabbler";
 import { useMessageStore } from "@/store/esbabbler/message";
 import { useMessageInputStore } from "@/store/esbabbler/messageInput";
 import { useRoomStore } from "@/store/esbabbler/room";
 import { Extension } from "@tiptap/vue-3";
 
-const esbabblerStore = useEsbabblerStore();
-const { userMap } = storeToRefs(esbabblerStore);
 const roomStore = useRoomStore();
-const { currentRoom } = storeToRefs(roomStore);
-const placeholder = computed(() => {
-  if (!currentRoom.value) return "";
-  const user = userMap.value.get(currentRoom.value.userId);
-  return user ? `Message ${user.name}'s Room` : "";
-});
+const { currentRoomName } = storeToRefs(roomStore);
 const messageStore = useMessageStore();
 const { sendMessage } = messageStore;
 const { messages, typings } = storeToRefs(messageStore);
@@ -46,7 +38,7 @@ const reply = computed(() =>
     <EsbabblerModelMessageReplyHeader v-if="reply" :user-id="reply.userId" @close="replyRowKey = undefined" />
     <RichTextEditor
       v-model="messageInput"
-      :placeholder
+      :placeholder="`Message ${currentRoomName}`"
       :limit="MESSAGE_MAX_LENGTH"
       :extensions="[keyboardExtension, mentionExtension]"
       :card-props="reply ? { class: 'rd-t-none' } : undefined"
