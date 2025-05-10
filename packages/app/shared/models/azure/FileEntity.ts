ce3edc7e7f305c82e009556bf9a2376b774f086f
Q16solver <jimmychen12138@gmail.com>
2025-05-10 23:14:55 +1000

    fix: remove unnecessary description

--- packages/app/shared/models/azure/FileEntity.ts
+++ packages/app/shared/models/azure/FileEntity.ts
@@ -1,18 +1,14 @@
-import type { SetOptional } from "type-fest";
-
 import { FILENAME_MAX_LENGTH } from "#shared/services/azure/container/constants";
 import { z } from "zod";
 
 export class FileEntity {
-  description!: string;
   filename!: string;
   id!: string;
   mimetype!: string;
 }
 
 export const fileEntitySchema = z.object({
-  description: z.string().default(""),
   filename: z.string().min(1).max(FILENAME_MAX_LENGTH),
   id: z.string().uuid(),
   mimetype: z.string(),
-}) satisfies z.ZodType<SetOptional<FileEntity, "description">>;
+}) satisfies z.ZodType<FileEntity>;
