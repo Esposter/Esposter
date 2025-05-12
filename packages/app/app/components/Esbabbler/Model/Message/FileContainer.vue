9ff9639d230f9d5149fc08f3d394cade603892e0
Jimmy Chen <jimmyc@resonatesolutions.com.au>
2025-05-12 11:03:05 +1000

    feat: Add crazy dynamic column layout

--- packages/app/app/components/Esbabbler/Model/Message/FileContainer.vue
+++ packages/app/app/components/Esbabbler/Model/Message/FileContainer.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import type { FileEntity } from "#shared/models/azure/FileEntity";
 
+import { getColumnLayout } from "@/services/esbabbler/file/getColumnLayout";
 import { useMessageStore } from "@/store/esbabbler/message";
 
 interface FileContainerProps {
@@ -10,14 +11,15 @@ interface FileContainerProps {
 const { files } = defineProps<FileContainerProps>();
 const messageStore = useMessageStore();
 const { downloadFileUrlMap } = storeToRefs(messageStore);
+const columnLayout = computed(() => getColumnLayout(files.length));
 const getUrl = (fileId: string) => downloadFileUrlMap.value.get(fileId)?.url ?? "";
 </script>
 
 <template>
   <v-container v-if="files.length > 0" fluid p-0>
-    <v-row>
-      <v-col v-for="file in files" :key="file.id" xl="2" lg="3" md="4" sm="6">
-        <StyledCard rd-4>
+    <v-row m--0.25 max-w-140>
+      <v-col v-for="(file, index) in files" :key="file.id" p-0.25 :cols="columnLayout[index]">
+        <StyledCard>
           <EsbabblerFileRenderer :file :url="getUrl(file.id)" />
         </StyledCard>
       </v-col>
