2f2153e7fc60fdeec4e7b8ea003460f2dd41c9df
Jimmy Chen <50786287+Q16solver@users.noreply.github.com>
2025-05-13 00:16:15 +1000

    Update OptionMenuItem.ts

--- packages/app/app/models/esbabbler/message/OptionMenuItem.ts
+++ packages/app/app/models/esbabbler/message/OptionMenuItem.ts
@@ -1,7 +1,7 @@
 export interface OptionMenuItem {
   color?: string;
   icon: string;
-  onClick: (event: (KeyboardEvent | MouseEvent) => void;
+  onClick: (event: KeyboardEvent | MouseEvent) => void;
   shortTitle?: string;
   title: string;
 }
