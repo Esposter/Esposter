c47885fdc11dbea92fac9e5e81f25e26f39f0e94
Jimmy Chen <jimmyc@resonatesolutions.com.au>
2025-05-16 15:51:26 +1000

    feat: Add custom toolbar buttons

--- packages/app/app/pages/surveyer/[id].vue
+++ packages/app/app/pages/surveyer/[id].vue
@@ -1,16 +1,45 @@
 <script setup lang="ts">
+import { getSynchronizedFunction } from "#shared/util/getSynchronizedFunction";
 import { validate } from "@/services/router/validate";
 import { useSurveyStore } from "@/store/surveyer/survey";
+import { Action, ComputedUpdater } from "survey-core";
 import { SurveyCreatorModel } from "survey-creator-core";
 import { DefaultDark, SC2020 } from "survey-creator-core/themes";
 
 defineRouteRules({ ssr: false });
 definePageMeta({ middleware: "auth", validate });
 
+const { $trpc } = useNuxtApp();
 const survey = reactive(await useReadSurveyFromRoute());
 const surveyerStore = useSurveyStore();
 const { updateSurvey } = surveyerStore;
 const creator = new SurveyCreatorModel({ autoSaveEnabled: true, showTranslationTab: true });
+const actions = [
+  new Action({
+    action: getSynchronizedFunction(async () => {
+      await $trpc.survey.publishSurvey.mutate({ id: survey.id, publishVersion: survey.publishVersion });
+    }),
+    iconName: "icon-publish-24x24",
+    id: "publish-survey",
+    tooltip: "Publish Survey",
+    visible: new ComputedUpdater(() => creator.activeTab === "designer"),
+  }),
+  new Action({
+    action: () => {
+      creator.JSON = {};
+    },
+    iconName: "icon-clear-24x24",
+    id: "clear-survey",
+    tooltip: "Clear Survey",
+    visible: new ComputedUpdater(() => creator.activeTab === "designer"),
+  }),
+];
+
+for (const action of actions) {
+  creator.toolbar.actions.push(action);
+  creator.footerToolbar.actions.push(action);
+}
+
 creator.text = survey.model;
 creator.saveSurveyFunc = async (saveNo: number, callback: Function) => {
   try {
@@ -41,7 +70,9 @@ watch(
     <v-toolbar class="border-b-sm" color="surface">
       <v-toolbar-title px-4 font-bold>
         {{ survey.name }}
-        <span class="text-lg">(Version: {{ survey.modelVersion }}, Publish Version: {{ survey.publishVersion }})</span>
+        <span class="text-lg text-gray">
+          (Version: {{ survey.modelVersion }}, Published Version: {{ survey.publishVersion }})
+        </span>
       </v-toolbar-title>
     </v-toolbar>
     <SurveyCreatorComponent :model="creator" />
