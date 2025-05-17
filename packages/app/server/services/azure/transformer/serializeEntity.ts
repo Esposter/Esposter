c6613dc7696d196b607c7870b171ab575074e717
Q16solver <jimmychen12138@gmail.com>
2025-05-17 17:56:02 +1000

    fix: type

--- packages/app/server/services/azure/transformer/serializeEntity.ts
+++ packages/app/server/services/azure/transformer/serializeEntity.ts
@@ -1,8 +1,9 @@
+import type { AzureEntity } from "#shared/models/azure/AzureEntity";
 import type { TableEntity } from "@azure/data-tables";
 
 import { getIsSerializable } from "@@/server/services/azure/transformer/getIsSerializable";
 
-export const serializeEntity = (entity: TableEntity) =>
+export const serializeEntity = (entity: AzureEntity) =>
   Object.fromEntries(
     Object.entries(entity).map(([property, value]) => {
       if (getIsSerializable(value)) return [property, JSON.stringify(value)];
