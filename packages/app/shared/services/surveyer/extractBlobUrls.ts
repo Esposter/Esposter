452f6999a37879c00e38bf27c7d462471d18a0e0
Jimmy Chen <jimmyc@resonatesolutions.com.au>
2025-05-20 13:12:17 +1000

    fix: make blob urls unique

--- packages/app/shared/services/surveyer/extractBlobUrls.ts
+++ packages/app/shared/services/surveyer/extractBlobUrls.ts
@@ -2,5 +2,7 @@ import { AzureContainer } from "#shared/models/azure/blob/AzureContainer";
 
 export const extractBlobUrls = (model: string): string[] => {
   const blobUrlToMatch = `${process.env.AZURE_BLOB_URL}/${AzureContainer.SurveyerAssets}`;
-  return Array.from(model.matchAll(new RegExp(`${blobUrlToMatch}[^\\]*`, "g"))).map(([blobUrl]) => blobUrl);
+  return [
+    ...new Set(Array.from(model.matchAll(new RegExp(`${blobUrlToMatch}[^\\]*`, "g"))).map(([blobUrl]) => blobUrl)),
+  ];
 };
