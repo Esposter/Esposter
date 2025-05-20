46e78b7d2b7738bfd19357ee323d07a3de1e6569
Jimmy Chen <jimmyc@resonatesolutions.com.au>
2025-05-20 13:12:38 +1000

    refactor: var names

--- packages/app/shared/services/surveyer/extractBlobUrls.ts
+++ packages/app/shared/services/surveyer/extractBlobUrls.ts
@@ -1,8 +1,8 @@
 import { AzureContainer } from "#shared/models/azure/blob/AzureContainer";
 
 export const extractBlobUrls = (model: string): string[] => {
-  const blobUrlToMatch = `${process.env.AZURE_BLOB_URL}/${AzureContainer.SurveyerAssets}`;
+  const blobUrlPrefix = `${process.env.AZURE_BLOB_URL}/${AzureContainer.SurveyerAssets}`;
   return [
-    ...new Set(Array.from(model.matchAll(new RegExp(`${blobUrlToMatch}[^\\]*`, "g"))).map(([blobUrl]) => blobUrl)),
+    ...new Set(Array.from(model.matchAll(new RegExp(`${blobUrlPrefix}[^\\]*`, "g"))).map(([blobUrl]) => blobUrl)),
   ];
 };
