4a0ce562a2e02b2f4f0d426a2f61a90e84082e59
Q16solver <jimmychen12138@gmail.com>
2025-05-20 23:45:32 +1000

    chore: remove unnecessary blank line

--- packages/app/shared/models/db/message/UpdateMessageInput.ts
+++ packages/app/shared/models/db/message/UpdateMessageInput.ts
@@ -6,5 +6,4 @@ export const updateMessageInputSchema = messageEntitySchema
   .pick({ partitionKey: true, rowKey: true })
   // @TODO: oneOf([files, message])
   .merge(messageEntitySchema.pick({ files: true, message: true }).partial());
-
 export type UpdateMessageInput = z.infer<typeof updateMessageInputSchema>;
