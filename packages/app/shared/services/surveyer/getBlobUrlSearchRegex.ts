afa3c653e4a5930a08120fb6414087f14f8a181c
Jimmy Chen <jimmyc@resonatesolutions.com.au>
2025-05-21 12:39:11 +1000

    fix: escape regexp

--- packages/app/shared/services/surveyer/getBlobUrlSearchRegex.ts
+++ packages/app/shared/services/surveyer/getBlobUrlSearchRegex.ts
@@ -1,9 +1,10 @@
 import { AzureContainer } from "#shared/models/azure/blob/AzureContainer";
+import { escapeRegExp } from "@/util/regex/escapeRegExp";
 
 export const getBlobUrlSearchRegex = (blobUrl?: string) =>
   new RegExp(
     // We want {protocol}{hostname}{pathname without query params} and
     // match until we reach a character that ends with "
-    `${blobUrl ?? `${process.env.AZURE_BLOB_URL}/${AzureContainer.SurveyerAssets}`}[^"]*`,
+    `${blobUrl ? escapeRegExp(blobUrl) : `${process.env.AZURE_BLOB_URL}/${AzureContainer.SurveyerAssets}`}[^"]*`,
     "g",
   );
