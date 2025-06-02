c2f25846a2e657da73e8633fa526a8119a1c36fb
Q16solver <jimmychen12138@gmail.com>
2025-06-02 18:23:27 +1000

    fix: vue-phaserjs inject types

--- packages/vue-phaserjs/src/composables/useInjectSceneKey.ts
+++ packages/vue-phaserjs/src/composables/useInjectSceneKey.ts
@@ -4,7 +4,7 @@ import { InjectionKeyMap } from "@/util/InjectionKeyMap";
 import { NotInitializedError } from "@esposter/shared";
 // We need to define the return type manually so it doesn't get lost after the inject
 export const useInjectSceneKey = (): SceneWithPlugins["scene"]["key"] => {
-  const sceneKey = inject<SceneWithPlugins["scene"]["key"]>(InjectionKeyMap.SceneKey);
+  const sceneKey = inject(InjectionKeyMap.SceneKey);
   if (!sceneKey) throw new NotInitializedError(InjectionKeyMap.SceneKey.description ?? "");
   return sceneKey;
 };
