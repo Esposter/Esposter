b264f97b837a76626e4abd06d8f2bca2a2198033
Q16solver <jimmychen12138@gmail.com>
2025-06-03 17:00:52 +1000

    fix: font size types

--- packages/app/app/pages/privacy-policy.vue
+++ packages/app/app/pages/privacy-policy.vue
@@ -14,17 +14,17 @@ const privacyPolicyHtml = await $fetch<string>("/privacyPolicy.html");
 // https://chromium.googlesource.com/chromium/blink/+/master/Source/core/css/html.css
 :deep(p) {
   display: block;
-  -webkit-margin-before: 1__qem;
-  -webkit-margin-after: 1__qem;
+  -webkit-margin-before: 1rem;
+  -webkit-margin-after: 1rem;
   -webkit-margin-start: 0;
   -webkit-margin-end: 0;
 }
 
 :deep(h1) {
   display: block;
-  font-size: 2em;
-  -webkit-margin-before: 0.67__qem;
-  -webkit-margin-after: 0.67em;
+  font-size: 2rem;
+  -webkit-margin-before: 0.67rem;
+  -webkit-margin-after: 0.67rem;
   -webkit-margin-start: 0;
   -webkit-margin-end: 0;
   font-weight: bold;
@@ -32,9 +32,9 @@ const privacyPolicyHtml = await $fetch<string>("/privacyPolicy.html");
 
 :deep(h2) {
   display: block;
-  font-size: 1.5em;
-  -webkit-margin-before: 0.83__qem;
-  -webkit-margin-after: 0.83em;
+  font-size: 1.5rem;
+  -webkit-margin-before: 0.83rem;
+  -webkit-margin-after: 0.83rem;
   -webkit-margin-start: 0;
   -webkit-margin-end: 0;
   font-weight: bold;
@@ -42,9 +42,18 @@ const privacyPolicyHtml = await $fetch<string>("/privacyPolicy.html");
 
 :deep(h3) {
   display: block;
-  font-size: 1.17em;
-  -webkit-margin-before: 1__qem;
-  -webkit-margin-after: 1em;
+  font-size: 1.17rem;
+  -webkit-margin-before: 1rem;
+  -webkit-margin-after: 1rem;
+  -webkit-margin-start: 0;
+  -webkit-margin-end: 0;
+  font-weight: bold;
+}
+
+:deep(h4) {
+  display: block;
+  -webkit-margin-before: 1.33rem;
+  -webkit-margin-after: 1.33rem;
   -webkit-margin-start: 0;
   -webkit-margin-end: 0;
   font-weight: bold;
@@ -53,10 +62,10 @@ const privacyPolicyHtml = await $fetch<string>("/privacyPolicy.html");
 :deep(ul, menu, dir) {
   display: block;
   list-style-type: disc;
-  -webkit-margin-before: 1__qem;
-  -webkit-margin-after: 1em;
+  -webkit-margin-before: 1rem;
+  -webkit-margin-after: 1rem;
   -webkit-margin-start: 0;
   -webkit-margin-end: 0;
-  -webkit-padding-start: 40px;
+  -webkit-padding-start: 2.5rem;
 }
 </style>
