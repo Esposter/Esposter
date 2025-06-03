c6da15f1a302cc92c743047943051eba748c7f4c
Q16solver <jimmychen12138@gmail.com>
2025-06-03 12:12:07 +1000

    feat: Add some small tests to shared pkg

---
+++ packages/shared/src/util/object/isPlainObject.test.ts
@@ -0,0 +1,18 @@
+import { isPlainObject } from "@/util/object/isPlainObject";
+import { describe, expect, test } from "vitest";
+
+describe(isPlainObject, () => {
+  test(isPlainObject, () => {
+    expect.hasAssertions();
+
+    expect(isPlainObject({})).toBe(true);
+    expect(isPlainObject(Object.create(null))).toBe(true);
+    expect(isPlainObject([])).toBe(false);
+    expect(isPlainObject(new Date())).toBe(false);
+    expect(isPlainObject(null)).toBe(false);
+    expect(isPlainObject(undefined)).toBe(false);
+    expect(isPlainObject("string")).toBe(false);
+    expect(isPlainObject(123)).toBe(false);
+    expect(isPlainObject(Symbol())).toBe(false);
+  });
+});
