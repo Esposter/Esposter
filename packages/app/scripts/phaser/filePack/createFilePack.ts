64d70a55a777f56d2bb7136c9f43bd10aa12d663
Q16solver <jimmychen12138@gmail.com>
2025-06-03 11:38:12 +1000

    Fix: align create filepack with main

--- packages/app/scripts/phaser/filePack/createFilePack.ts
+++ packages/app/scripts/phaser/filePack/createFilePack.ts
@@ -15,7 +15,7 @@ export const createFilePack = async () => {
   config();
   const blobServiceClient = BlobServiceClient.fromConnectionString(process.env.AZURE_STORAGE_ACCOUNT_CONNECTION_STRING);
   const containerClient = blobServiceClient.getContainerClient(AzureContainer.DungeonsAssets);
-  const files: Types.Loader.FileConfig[] = [];
+  const filePack: Types.Loader.FileTypes.PackFileSection = { baseURL: process.env.AZURE_BLOB_URL, files: [] };
   const fileKeys = new Set<string>();
   const enumName = "FileKey";
 
@@ -29,7 +29,7 @@ export const createFilePack = async () => {
         if (fileKeys.has(key)) throw new InvalidOperationError(Operation.Push, enumName, `Duplicate key: ${key}`);
 
         fileKeys.add(key);
-        files.push({
+        filePack.files.push({
           key,
           type,
           url: blob.name,
@@ -47,8 +47,8 @@ export const createFilePack = async () => {
       if (!options)
         throw new InvalidOperationError(Operation.Read, "Prettier Configuration", "Missing Prettier Configuration");
 
-      const formatted = await format(JSON.stringify(files), { ...options, parser: "json" });
-      await outputFile("files.json", formatted);
+      const formatted = await format(JSON.stringify(filePack), { ...options, parser: "json" });
+      await outputFile("filepack.json", formatted);
     })(),
   ]);
 };
