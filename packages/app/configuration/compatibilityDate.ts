4e393e863330bbf1021961055ca830bab4e1d242
Q16solver <jimmychen12138@gmail.com>
2025-06-05 21:05:09 +1000

    chore: update compat date

--- packages/app/configuration/compatibilityDate.ts
+++ packages/app/configuration/compatibilityDate.ts
@@ -1,3 +1,3 @@
 import type { NuxtConfig } from "nuxt/schema";
 
-export const compatibilityDate: NuxtConfig["compatibilityDate"] = "2025-05-14";
+export const compatibilityDate: NuxtConfig["compatibilityDate"] = "2025-06-05";
