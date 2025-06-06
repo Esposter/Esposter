0ee0d2811c4cb15fae9ede479ae22ce8ae80f600
Q16solver <jimmychen12138@gmail.com>
2025-06-06 18:16:05 +1000

    fix: types

--- packages/app/shared/services/esbabbler/refineMessageSchema.ts
+++ packages/app/shared/services/esbabbler/refineMessageSchema.ts
@@ -1,5 +1,6 @@
 import type { MessageEntity } from "#shared/models/db/message/MessageEntity";
 import type { z } from "zod/v4";
 
-export const refineMessageSchema = <T extends z.ZodType<Pick<MessageEntity, "files" | "message">>>(schema: T) =>
-  schema.refine(({ files, message }) => Boolean(files) || Boolean(message));
+export const refineMessageSchema = <T extends z.ZodType<Partial<Pick<MessageEntity, "files" | "message">>>>(
+  schema: T,
+) => schema.refine(({ files, message }) => Boolean(files) || Boolean(message));
