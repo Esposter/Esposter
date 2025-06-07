642b58baaeff1620a3f0020b5993af7d291b405f
Q16solver <jimmychen12138@gmail.com>
2025-06-07 18:22:30 +1000

    refactor: mobile value

--- packages/app/app/store/layout.ts
+++ packages/app/app/store/layout.ts
@@ -1,14 +1,15 @@
 export const useLayoutStore = defineStore("layout", () => {
   const { mobile } = useDisplay();
-  const leftDrawerOpen = ref(!mobile.value);
-  const rightDrawerOpen = ref(!mobile.value);
+  const isDesktop = !mobile.value;
+  const leftDrawerOpen = ref(isDesktop);
+  const rightDrawerOpen = ref(isDesktop);
   // These variables are only affected by screen resizing
   // They will be read-only to outer components deeper in the layout
   // because we want the decision of showing the outer components (i.e. buttons that open the drawers)
   // to only be dependent on screen-resizing for better UI/UX and not disappear and shift the UI
   // suddenly whenever we open and close the drawer
-  const leftDrawerOpenAuto = ref(!mobile.value);
-  const rightDrawerOpenAuto = ref(!mobile.value);
+  const leftDrawerOpenAuto = ref(isDesktop);
+  const rightDrawerOpenAuto = ref(isDesktop);
   return {
     leftDrawerOpen,
     leftDrawerOpenAuto,
