98db8045c37634b8902db4cf8879b1d6ae27c08c
Q16solver <jimmychen12138@gmail.com>
2025-06-08 10:51:48 +1000

    fix: remove unnecessary v-card-actions

--- packages/app/app/components/Esbabbler/Model/Message/LinkPreview/URL.vue
+++ packages/app/app/components/Esbabbler/Model/Message/LinkPreview/URL.vue
@@ -14,8 +14,6 @@ const { description, images, siteName, title, url } = defineProps<URLLinkPreview
     <v-card-text v-if="description">
       {{ description }}
     </v-card-text>
-    <v-card-actions>
-      <v-img width="100%" :src="images[0]" :alt="siteName ?? title" cover />
-    </v-card-actions>
+    <v-img width="100%" :src="images[0]" :alt="siteName ?? title" cover />
   </StyledCard>
 </template>
