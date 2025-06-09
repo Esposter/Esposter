72f4fe102be875fcf1ec4c8ce27165c7abed8bc8
Q16solver <jimmychen12138@gmail.com>
2025-06-09 15:39:57 +1000

    fix: accept multiple files

--- packages/app/app/components/RichTextEditor/Custom/UploadFileButton.vue
+++ packages/app/app/components/RichTextEditor/Custom/UploadFileButton.vue
@@ -3,7 +3,7 @@ import { showOpenFilePicker } from "show-open-file-picker";
 
 const emit = defineEmits<{ "upload-file": [files: File[]] }>();
 const onClick = async () => {
-  const fileSystemFileHandles = await showOpenFilePicker();
+  const fileSystemFileHandles = await showOpenFilePicker({ multiple: true });
   if (fileSystemFileHandles.length === 0) return;
   const files = await Promise.all(fileSystemFileHandles.map((fileSystemFileHandle) => fileSystemFileHandle.getFile()));
   emit("upload-file", files);
