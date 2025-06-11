b126707aa502036b9efa208537b748c541829ed7
Q16solver <jimmychen12138@gmail.com>
2025-06-11 19:11:33 +1000

    fix: types

--- packages/app/app/services/dashboard/jsonSchema/recusiveAssignTitle.ts
+++ packages/app/app/services/dashboard/jsonSchema/recusiveAssignTitle.ts
@@ -3,9 +3,10 @@ import type { z } from "zod/v4";
 import { prettify } from "@/util/text/prettify";
 import { toTitleCase } from "@/util/text/toTitleCase";
 
-export const recursiveAssignTitle = (properties: z.core.JSONSchema.ObjectSchema["properties"]) => {
+export const recursiveAssignTitle = (properties: z.core.JSONSchema.JSONSchema["properties"]) => {
   if (!properties) return;
   for (const [key, prop] of Object.entries(properties))
-    if (prop.type === "object") recursiveAssignTitle(prop.properties as z.core.JSONSchema.ObjectSchema["properties"]);
+    if (typeof prop === "boolean") continue;
+    else if (prop.type === "object") recursiveAssignTitle(prop.properties);
     else prop.title = toTitleCase(prettify(key.toString()));
 };
