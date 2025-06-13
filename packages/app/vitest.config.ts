3d2bd1b75ebb323d7998d2f833fb3c112d2cea4c
Jimmy Chen <jimmyc@resonatesolutions.com.au>
2025-06-13 11:55:58 +1000

    feat(test): Add testing scripts as well

--- packages/app/vitest.config.ts
+++ packages/app/vitest.config.ts
@@ -1,7 +1,9 @@
+import { dayjs } from "#shared/services/dayjs";
 import { defineVitestConfig } from "@nuxt/test-utils/config";
 
 export default defineVitestConfig({
   test: {
     environment: "nuxt",
+    hookTimeout: dayjs.duration(60, "seconds").asMilliseconds(),
   },
 });
