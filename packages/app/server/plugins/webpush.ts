3e2aa4c213c0e2fe52ef654593ef05ee83daa2d2
Q16solver <jimmychen12138@gmail.com>
2025-06-15 18:26:01 +1000

    fix: use app base url

--- packages/app/server/plugins/webpush.ts
+++ packages/app/server/plugins/webpush.ts
@@ -2,7 +2,7 @@ import webpush from "web-push";
 
 export default defineNitroPlugin(() => {
   const runtimeConfig = useRuntimeConfig();
-  const hostname = new URL(runtimeConfig.app.baseURL).hostname;
+  const hostname = new URL(runtimeConfig.public.baseUrl).hostname;
   webpush.setVapidDetails(
     `mailto:no-reply@${hostname}`,
     runtimeConfig.public.vapid.publicKey,
