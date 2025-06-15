cc559f9d6dc9af3877bbbcae65ec97634ce246f7
Q16solver <jimmychen12138@gmail.com>
2025-06-15 22:36:35 +1000

    fix: revert back to watch for web notif

--- packages/app/app/composables/usePushSubscription.ts
+++ packages/app/app/composables/usePushSubscription.ts
@@ -5,30 +5,21 @@ export const usePushSubscription = () => {
   const pushSubscriptionStore = usePushSubscriptionStore();
   const { pushSubscription } = storeToRefs(pushSubscriptionStore);
   const { permissionGranted } = useWebNotification();
+  watch(
+    permissionGranted,
+    async (newPermissionGranted) => {
+      if (!newPermissionGranted) {
+        await pushSubscription.value?.unsubscribe();
+        pushSubscription.value = undefined;
+        return;
+      }
 
-  const unsubscribe = async () => {
-    await pushSubscription.value?.unsubscribe();
-    pushSubscription.value = undefined;
-  };
-
-  const { trigger } = watchTriggerable(permissionGranted, async (newPermissionGranted) => {
-    if (!newPermissionGranted) {
-      await unsubscribe();
-      return;
-    }
-
-    const registration = await navigator.serviceWorker.ready;
-    pushSubscription.value = await registration.pushManager.subscribe({
-      applicationServerKey: runtimeConfig.public.vapid.publicKey,
-      userVisibleOnly: true,
-    });
-  });
-
-  onMounted(async () => {
-    await trigger();
-  });
-
-  onUnmounted(async () => {
-    await unsubscribe();
-  });
+      const registration = await navigator.serviceWorker.ready;
+      pushSubscription.value = await registration.pushManager.subscribe({
+        applicationServerKey: runtimeConfig.public.vapid.publicKey,
+        userVisibleOnly: true,
+      });
+    },
+    { immediate: true },
+  );
 };
