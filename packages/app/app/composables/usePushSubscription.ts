bbdaa512b225ba2f7acc55ad3a1459b102ed6308
Q16solver <jimmychen12138@gmail.com>
2025-06-15 22:57:57 +1000

    fix: try show notif on post message

--- packages/app/app/composables/usePushSubscription.ts
+++ packages/app/app/composables/usePushSubscription.ts
@@ -1,25 +1,48 @@
+import type { WebNotificationOptions } from "@vueuse/core";
+import type { SetRequired } from "type-fest";
+
+import { getSynchronizedFunction } from "#shared/util/getSynchronizedFunction";
 import { usePushSubscriptionStore } from "@/store/pushSubscription";
 
 export const usePushSubscription = () => {
   const runtimeConfig = useRuntimeConfig();
   const pushSubscriptionStore = usePushSubscriptionStore();
   const { pushSubscription } = storeToRefs(pushSubscriptionStore);
+  const messageListener = ref<(this: ServiceWorkerContainer, ev: ServiceWorkerContainerEventMap["message"]) => void>();
   const { permissionGranted } = useWebNotification();
-  watch(
-    permissionGranted,
-    async (newPermissionGranted) => {
-      if (!newPermissionGranted) {
-        await pushSubscription.value?.unsubscribe();
-        pushSubscription.value = undefined;
-        return;
-      }
 
-      const registration = await navigator.serviceWorker.ready;
-      pushSubscription.value = await registration.pushManager.subscribe({
-        applicationServerKey: runtimeConfig.public.vapid.publicKey,
-        userVisibleOnly: true,
-      });
-    },
-    { immediate: true },
-  );
+  const unsubscribe = async () => {
+    await pushSubscription.value?.unsubscribe();
+    pushSubscription.value = undefined;
+    if (messageListener.value) {
+      window.navigator.serviceWorker.removeEventListener("message", messageListener.value);
+      messageListener.value = undefined;
+    }
+  };
+
+  const { trigger } = watchTriggerable(permissionGranted, async (newPermissionGranted) => {
+    if (!newPermissionGranted) {
+      await unsubscribe();
+      return;
+    }
+
+    const registration = await window.navigator.serviceWorker.ready;
+    pushSubscription.value = await registration.pushManager.subscribe({
+      applicationServerKey: runtimeConfig.public.vapid.publicKey,
+      userVisibleOnly: true,
+    });
+    messageListener.value = getSynchronizedFunction(
+      ({ data: { title, ...rest } }: MessageEvent<SetRequired<WebNotificationOptions, "title">>) =>
+        registration.showNotification(title, rest),
+    );
+    window.navigator.serviceWorker.addEventListener("message", messageListener.value);
+  });
+
+  onMounted(async () => {
+    await trigger();
+  });
+
+  onUnmounted(async () => {
+    await unsubscribe();
+  });
 };
