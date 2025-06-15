59434f9dca25485bd18dee3dee028b9bf4de41aa
Q16solver <jimmychen12138@gmail.com>
2025-06-15 23:35:27 +1000

    fix: use back js

--- packages/app/public/serviceWorker/push.js
+++ packages/app/public/serviceWorker/push.js
@@ -1,13 +1,8 @@
-import type { WebNotificationOptions } from "@vueuse/core";
-import type { SetRequired } from "type-fest";
 // https://github.com/vite-pwa/docs/issues/132
-declare const self: ServiceWorkerGlobalScope;
-declare const clients: Clients;
-
 self.addEventListener("push", async ({ data, waitUntil }) => {
   if (!data) return;
 
-  const jsonData = data.json() as SetRequired<WebNotificationOptions, "title">;
+  const jsonData = data.json();
   const { title, ...rest } = jsonData;
   const clients = await self.clients.matchAll();
   for (const client of clients) {
