4b4465a50ad648d9e6a0204e46451fa25fd70877
Jimmy Chen <jimmyc@resonatesolutions.com.au>
2025-06-16 14:53:18 +1000

    feat: nuxt use rolldown

--- packages/app/server/plugins/webpush.ts
+++ packages/app/server/plugins/webpush.ts
@@ -1,6 +1,8 @@
+import { IS_PRODUCTION } from "#shared/util/environment/constants";
 import webpush from "web-push";
 
 export default defineNitroPlugin(() => {
+  if (!IS_PRODUCTION) return;
   const runtimeConfig = useRuntimeConfig();
   webpush.setVapidDetails(
     runtimeConfig.public.baseUrl,
