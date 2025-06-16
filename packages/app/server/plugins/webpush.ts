fd618b47ae0bfb6d8836af2b861c4128e9413c41
Jimmy Chen <jimmyc@resonatesolutions.com.au>
2025-06-16 15:23:03 +1000

    fix: don't set vapid on dev

--- packages/app/server/plugins/webpush.ts
+++ packages/app/server/plugins/webpush.ts
@@ -1,7 +1,9 @@
+import { IS_PRODUCTION } from "#shared/util/environment/constants";
 import webpush from "web-push";
 
 export default defineNitroPlugin(() => {
   const runtimeConfig = useRuntimeConfig();
+  if (!IS_PRODUCTION || new URL(runtimeConfig.public.baseUrl).protocol === "http:") return;
   webpush.setVapidDetails(
     runtimeConfig.public.baseUrl,
     runtimeConfig.public.vapid.publicKey,
