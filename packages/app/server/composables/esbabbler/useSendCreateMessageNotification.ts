f0163157542d31eaaa787ffce74c4ce913ccac28
Jimmy Chen <jimmyc@resonatesolutions.com.au>
2025-06-16 10:51:21 +1000

    fix: just use name

--- packages/app/server/composables/esbabbler/useSendCreateMessageNotification.ts
+++ packages/app/server/composables/esbabbler/useSendCreateMessageNotification.ts
@@ -2,7 +2,6 @@ import type { Session } from "@@/server/models/auth/Session";
 import type { PushSubscription } from "web-push";
 
 import { RoutePath } from "#shared/models/router/RoutePath";
-import { SITE_NAME } from "#shared/services/esposter/constants";
 import { truncate } from "#shared/util/text/truncate";
 import { PUSH_NOTIFICATION_MAX_LENGTH } from "@@/server/services/esposter/constants";
 import { parse } from "node-html-parser";
@@ -10,7 +9,7 @@ import webpush from "web-push";
 
 export const useSendCreateMessageNotification = (pushSubscription: PushSubscription | undefined, roomId: string) => {
   const runtimeConfig = useRuntimeConfig();
-  return async (message: string, name: Session["user"]["name"], icon: Session["user"]["image"]) => {
+  return async (message: string, title: Session["user"]["name"], icon: Session["user"]["image"]) => {
     if (!pushSubscription) return;
 
     const messageHtml = parse(message);
@@ -20,12 +19,12 @@ export const useSendCreateMessageNotification = (pushSubscription: PushSubscript
     await webpush.sendNotification(
       pushSubscription,
       JSON.stringify({
-        body: truncate(`${name}: ${textContent}`, PUSH_NOTIFICATION_MAX_LENGTH),
+        body: truncate(textContent, PUSH_NOTIFICATION_MAX_LENGTH),
         data: {
           url: `${runtimeConfig.public.baseUrl}${RoutePath.Messages(roomId)}`,
         },
         icon,
-        title: SITE_NAME,
+        title,
       }),
     );
   };
