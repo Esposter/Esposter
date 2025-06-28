daf5d625c86241082b04dc9c463568dc0f88b7f1
Q16solver <jimmychen12138@gmail.com>
2025-06-28 21:40:18 +1000

    refactor: align error msg with vuetify

--- packages/app/app/services/vuetify/formRules.ts
+++ packages/app/app/services/vuetify/formRules.ts
@@ -4,6 +4,7 @@ import type { ValidationRule } from "vuetify";
 import { MAX_REQUEST_SIZE, MEGABYTE } from "#shared/services/esposter/constants";
 import { profanityMatcher } from "#shared/services/obscenity/profanityMatcher";
 import deepEqual from "fast-deep-equal";
+import en from "vuetify/lib/locale/en.js";
 
 type FileFieldValue = File | undefined;
 type TextFieldValue = null | string;
@@ -16,12 +17,12 @@ export const formRules: {
   required: ValidationRule;
 } = {
   isNotEqual: (oldValue) => (value: TextFieldValue) =>
-    !deepEqual(value, oldValue) || `new value cannot be the same as the existing value`,
-  isNotProfanity: (value: TextFieldValue) => !value || !profanityMatcher.hasMatch(value) || `cannot contain profanity`,
+    !deepEqual(value, oldValue) || `This field's value cannot be the same as the existing value`,
+  isNotProfanity: (value: TextFieldValue) => !value || !profanityMatcher.hasMatch(value) || `This field cannot contain profanity`,
   requireAtMostMaxFileSize: (value: FileFieldValue) =>
     // @TODO: Right now trpc uses application/octet-stream for uploading files which uses the same namespace as normal requests
     // compared to using multipart/form-data, so we'll apply the same frontend validation for now for simplicity
-    !value || value.size < MAX_REQUEST_SIZE || `file size should be less than ${MAX_REQUEST_SIZE / MEGABYTE} MB`,
-  requireAtMostNCharacters: (n) => (value: TextFieldValue) => (value && value.length <= n) || `max ${n} characters`,
-  required: (value: TextFieldValue) => (value && value.length > 0) || "required",
+    !value || value.size < MAX_REQUEST_SIZE || `This field's file size should be less than ${MAX_REQUEST_SIZE / MEGABYTE} MB`,
+  requireAtMostNCharacters: (n) => (value: TextFieldValue) => (value && value.length <= n) || `You must enter a maximum of ${n} characters`,
+  required: (value: TextFieldValue) => (value && value.length > 0) || en.rules.required,
 };
