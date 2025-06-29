e3c8cbdf97b710c399024b3c8560da80dc3e04e9
Q16solver <jimmychen12138@gmail.com>
2025-06-29 19:14:17 +1000

    chore: lint

--- packages/app/app/services/vuetify/formRules.ts
+++ packages/app/app/services/vuetify/formRules.ts
@@ -3,7 +3,7 @@ import type { ValidationRule } from "vuetify";
 
 import { MAX_REQUEST_SIZE, MEGABYTE } from "#shared/services/esposter/constants";
 import { profanityMatcher } from "#shared/services/obscenity/profanityMatcher";
-import deepEqual from "fast-deep-equal"
+import deepEqual from "fast-deep-equal";
 
 type FileFieldValue = File | undefined;
 type TextFieldValue = null | string;
@@ -17,11 +17,15 @@ export const formRules: {
 } = {
   isNotEqual: (oldValue) => (value: TextFieldValue) =>
     !deepEqual(value, oldValue) || `This field's value cannot be the same as the existing value`,
-  isNotProfanity: (value: TextFieldValue) => !value || !profanityMatcher.hasMatch(value) || `This field cannot contain profanity`,
+  isNotProfanity: (value: TextFieldValue) =>
+    !value || !profanityMatcher.hasMatch(value) || `This field cannot contain profanity`,
   requireAtMostMaxFileSize: (value: FileFieldValue) =>
     // @TODO: Right now trpc uses application/octet-stream for uploading files which uses the same namespace as normal requests
     // compared to using multipart/form-data, so we'll apply the same frontend validation for now for simplicity
-    !value || value.size < MAX_REQUEST_SIZE || `This field's file size should be less than ${MAX_REQUEST_SIZE / MEGABYTE} MB`,
-  requireAtMostNCharacters: (n) => (value: TextFieldValue) => (value && value.length <= n) || `You must enter a maximum of ${n} characters`,
+    !value ||
+    value.size < MAX_REQUEST_SIZE ||
+    `This field's file size should be less than ${MAX_REQUEST_SIZE / MEGABYTE} MB`,
+  requireAtMostNCharacters: (n) => (value: TextFieldValue) =>
+    (value && value.length <= n) || `You must enter a maximum of ${n} characters`,
   required: (value: TextFieldValue) => (value && value.length > 0) || "This field is required",
 };
