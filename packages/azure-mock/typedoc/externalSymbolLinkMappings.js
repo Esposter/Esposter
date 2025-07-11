c911a17e2cfcecca0d029c85643be8c02a2a9b41
Q16solver <jimmychen12138@gmail.com>
2025-07-11 13:30:59 +1000

    chore: Add external symbolic link mappings to suppress warnings

---
+++ packages/azure-mock/typedoc/externalSymbolLinkMappings.js
@@ -0,0 +1,19 @@
+/** @satisfies {import('typedoc').TypeDocOptions["externalSymbolLinkMappings"]} */
+export default {
+  "@azure/storage-blob": {
+    AppendBlobClient: "#",
+    BlobClient: "#",
+    BlobLeaseClient: "#",
+    BlobServiceClient: "#",
+    BlockBlobClient: "#",
+    "BlockBlobClient.commitBlockList": "#",
+    "BlockBlobClient.stageBlock": "#",
+    "BlockBlobClient.uploadBrowserData": "#",
+    "BlockBlobClient.uploadFile": "#",
+    "BlockBlobClient.uploadStream": "#",
+    "BlockBlobParallelUploadOptions.blobHTTPHeaders": "#",
+    "BlockBlobParallelUploadOptions.maxSingleShotSize": "#",
+    ContainerClient: "#",
+    PageBlobClient: "#",
+  },
+};
