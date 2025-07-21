3736fab44d0d1113595caf4235d5330df6ebec50
Q16solver <jimmychen12138@gmail.com>
2025-07-21 19:45:08 +1000

    chore: update nuxt

--- packages/app/configuration/typescript.ts
+++ packages/app/configuration/typescript.ts
@@ -3,15 +3,6 @@ import type { NuxtConfig } from "nuxt/schema";
 import typescriptIgnores from "@esposter/configuration/eslint/typescriptIgnores.js";
 
 export const typescript: NuxtConfig["typescript"] = {
-  sharedTsConfig: {
-    compilerOptions: {
-      // @TODO: https://github.com/nuxt/nuxt/issues/32556
-      paths: {
-        "#shared": ["../shared"],
-        "#shared/*": ["../shared/*"],
-      },
-    },
-  },
   tsConfig: {
     compilerOptions: {
       noUncheckedIndexedAccess: false,
