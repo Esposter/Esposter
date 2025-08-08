73ee124972dc5293d1bc915bbb440ea6b221cee5
Jimmy Chen <50786287+Q16solver@users.noreply.github.com>
2025-08-08 22:30:03 +1000

    Update [rowKey].vue

--- packages/app/app/pages/messages/[id]/[rowKey].vue
+++ packages/app/app/pages/messages/[id]/[rowKey].vue
@@ -1,23 +1,14 @@
 <script setup lang="ts">
-import { useMessageStore } from "@/store/esbabbler/message";
 import { useReplyStore } from "@/store/esbabbler/reply";
 
 const route = useRoute();
-const messageStore = useMessageStore();
-const { messages } = storeToRefs(messageStore);
 const replyStore = useReplyStore();
 const { onIndicatorClick } = replyStore;
 
 onMounted(() => {
   const rowKey = route.params.rowKey as string;
   if (!rowKey) return;
-  watchOnce(
-    messages,
-    () => {
-      onIndicatorClick(rowKey);
-    },
-    { immediate: true },
-  );
+  onIndicatorClick(rowKey);
 });
 </script>
 
