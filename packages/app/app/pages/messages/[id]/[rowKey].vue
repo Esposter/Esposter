ac83bc45b3f127a64b5bbd6be2a75dad4009c2de
Q16solver <jimmychen12138@gmail.com>
2025-08-08 21:37:06 +1000

    refactor: move layouts

--- packages/app/app/pages/messages/[id]/[rowKey].vue
+++ packages/app/app/pages/messages/[id]/[rowKey].vue
@@ -1,22 +1,15 @@
 <script setup lang="ts">
 import { useMessageStore } from "@/store/esbabbler/message";
 import { useReplyStore } from "@/store/esbabbler/reply";
-import { useRoomStore } from "@/store/esbabbler/room";
 
-definePageMeta({ middleware: "auth" });
-
-useHead({ titleTemplate: (title) => (title ? `Esbabbler | ${title}` : "Esbabbler") });
-useSubscribables();
-const roomStore = useRoomStore();
-const { currentRoomName } = storeToRefs(roomStore);
 const route = useRoute();
 const messageStore = useMessageStore();
 const { messages } = storeToRefs(messageStore);
 const replyStore = useReplyStore();
 const { onIndicatorClick } = replyStore;
-const rowKey = route.params.rowKey as string;
 
 onMounted(() => {
+  const rowKey = route.params.rowKey as string;
   if (!rowKey) return;
   watchOnce(
     messages,
@@ -29,21 +22,5 @@ onMounted(() => {
 </script>
 
 <template>
-  <!-- Set max height here so we can hide global window scrollbar
-    and show scrollbar within the chat content only for chat routes -->
-  <NuxtLayout :main-style="{ maxHeight: '100dvh' }" :footer-style="{ paddingBottom: 0 }">
-    <Head>
-      <Title>{{ currentRoomName }}</Title>
-    </Head>
-    <EsbabblerContent />
-    <template #left>
-      <EsbabblerLeftSideBar />
-    </template>
-    <template #right>
-      <EsbabblerRightSideBar />
-    </template>
-    <template #footer>
-      <EsbabblerModelMessageInput />
-    </template>
-  </NuxtLayout>
+  <NuxtLayout name="messages" />
 </template>
