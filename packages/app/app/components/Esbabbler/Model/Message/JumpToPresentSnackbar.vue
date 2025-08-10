680c54b4dd41ff77761ae5815ba4b4b9bd5963b3
Q16solver <jimmychen12138@gmail.com>
2025-08-10 13:58:06 +1000

    fix: jump to present

--- packages/app/app/components/Esbabbler/Model/Message/JumpToPresentSnackbar.vue
+++ packages/app/app/components/Esbabbler/Model/Message/JumpToPresentSnackbar.vue
@@ -1,17 +1,17 @@
 <script setup lang="ts">
 import StyledButton from "@/components/Styled/Button.vue";
-import { useMessageStore } from "@/store/esbabbler/message";
+import { useMessageScrollStore } from "@/store/esbabbler/messageScroll";
 
-const messageStore = useMessageStore();
-const { isViewingOlderMessages } = storeToRefs(messageStore);
-const { scrollToBottom } = messageStore;
+const messageScrollStore = useMessageScrollStore();
+const { isViewingOlderMessages } = storeToRefs(messageScrollStore);
+const { jumpToPresent } = messageScrollStore;
 </script>
 
 <template>
   <TransitionFade>
     <v-snackbar v-model="isViewingOlderMessages" color="background">
       <div flex justify-center items-center flex-wrap>
-        You're Viewing Older Messages <StyledButton mx-2 @click="scrollToBottom">Jump to Present</StyledButton>
+        You're Viewing Older Messages <StyledButton mx-2 @click="jumpToPresent">Jump to Present</StyledButton>
       </div>
     </v-snackbar>
   </TransitionFade>
--- packages/app/app/store/esbabbler/messageScroll.ts
+++ packages/app/app/store/esbabbler/messageScroll.ts
@@ -1,15 +1,23 @@
 import type { VList } from "vuetify/components";
 
+import { RoutePath } from "#shared/models/router/RoutePath";
+import { useRoomStore } from "@/store/esbabbler/room";
+
 export const useMessageScrollStore = defineStore("esbabbler/messageScroll", () => {
   const messageContainer = ref<InstanceType<typeof VList> | null>(null);
   const messageContainerElement = computed(() => messageContainer.value?.$el as HTMLDivElement | null);
   const { y } = useScroll(messageContainerElement);
   const isViewingOlderMessages = computed(() => y.value < -2000);
-  const scrollToBottom = () => messageContainerElement.value?.scrollTo({ behavior: "smooth", top: 0 });
+  const roomStore = useRoomStore();
+  // We're gonna cheat here and just send them to the messages page C:
+  const jumpToPresent = async () => {
+    if (!roomStore.currentRoomId) return;
+    await navigateTo(RoutePath.Messages(roomStore.currentRoomId));
+  };
   return {
     isViewingOlderMessages,
+    jumpToPresent,
     messageContainer,
     messageContainerElement,
-    scrollToBottom,
   };
 });
