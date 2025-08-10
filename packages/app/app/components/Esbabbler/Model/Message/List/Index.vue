ab33c06b746f4caa1d25fc3fa00b269062af5e1e
Q16solver <jimmychen12138@gmail.com>
2025-08-10 11:38:35 +1000

    feat: Add skeletons

--- packages/app/app/components/Esbabbler/Model/Message/List/Index.vue
+++ packages/app/app/components/Esbabbler/Model/Message/List/Index.vue
@@ -1,20 +1,43 @@
 <script setup lang="ts">
+import { DEFAULT_READ_LIMIT } from "#shared/services/pagination/constants";
 import { useMessageStore } from "@/store/esbabbler/message";
 
 const { readMoreMessages, readMoreNewerMessages } = await useReadMessages();
 const messageStore = useMessageStore();
 const { hasMore, hasMoreNewer, messageContainer, messages } = storeToRefs(messageStore);
+const isLoadingOlder = ref(false);
+const isLoadingNewer = ref(false);
+const topWaypoint = ref<HTMLElement | null>(null);
+const bottomWaypoint = ref<HTMLElement | null>(null);
+
+useIntersectionObserver(topWaypoint, ([entry]) => {
+  if (!entry.isIntersecting || !hasMoreNewer.value || isLoadingNewer.value) return;
+  isLoadingNewer.value = true;
+  readMoreNewerMessages(() => (isLoadingNewer.value = false));
+});
+
+useIntersectionObserver(bottomWaypoint, ([entry]) => {
+  if (!entry.isIntersecting || !hasMore.value || isLoadingOlder.value) return;
+  isLoadingOlder.value = true;
+  readMoreMessages(() => (isLoadingOlder.value = false));
+});
 </script>
 
 <template>
   <v-list ref="messageContainer" flex-1 flex pb-0 basis-full flex-col-reverse overflow-y-auto="!" lines="two">
-    <StyledWaypoint :active="hasMoreNewer" @change="readMoreNewerMessages" />
+    <template v-if="isLoadingNewer">
+      <EsbabblerModelMessageListSkeletonItem v-for="i in DEFAULT_READ_LIMIT" :key="i" />
+    </template>
+    <div ref="topWaypoint" aria-hidden="true" h-1 />
     <EsbabblerModelMessageListItemContainer
       v-for="(message, index) of messages"
       :key="message.rowKey"
       :current-message="message"
       :next-message="messages[index + 1]"
     />
-    <StyledWaypoint :active="hasMore" @change="readMoreMessages" />
+    <div ref="bottomWaypoint" aria-hidden="true" h-1 />
+    <template v-if="isLoadingOlder">
+      <EsbabblerModelMessageListSkeletonItem v-for="i in DEFAULT_READ_LIMIT" :key="i" />
+    </template>
   </v-list>
 </template>
---
+++ packages/app/app/components/Esbabbler/Model/Message/List/SkeletonItem.vue
@@ -0,0 +1,23 @@
+<template>
+  <div flex>
+    <v-skeleton-loader self-start type="avatar" :loading="true" />
+    <div w-full>
+      <v-skeleton-loader type="heading" :loading="true" width="20%" />
+      <v-skeleton-loader type="paragraph" :loading="true" />
+    </div>
+  </div>
+</template>
+
+<style scoped lang="scss">
+:deep(.v-skeleton-loader__avatar) {
+  margin-right: 0;
+  min-width: $avatar-width;
+  min-height: $avatar-width;
+  width: $avatar-width;
+  height: $avatar-width;
+}
+
+:deep(.v-skeleton-loader__heading) {
+  margin-bottom: 0;
+}
+</style>
