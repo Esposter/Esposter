b4ffe3903119a760e196aae0e107773c04382f3b
Q16solver <jimmychen12138@gmail.com>
2025-08-13 12:06:21 +1000

    feat: Add linking to reply

--- packages/app/app/composables/esbabbler/useScrollToMessage.ts
+++ packages/app/app/composables/esbabbler/useScrollToMessage.ts
@@ -1,10 +1,23 @@
+import { RoutePath } from "#shared/models/router/RoutePath";
 import { dayjs } from "#shared/services/dayjs";
+import { useMessageStore } from "@/store/esbabbler/message";
 import { useReplyStore } from "@/store/esbabbler/reply";
+import { useRoomStore } from "@/store/esbabbler/room";
 
 export const useScrollToMessage = () => {
   const replyStore = useReplyStore();
   const { activeRowKey } = storeToRefs(replyStore);
-  return (rowKey: string) => {
+  const roomStore = useRoomStore();
+  const { currentRoomId } = storeToRefs(roomStore);
+  const messageStore = useMessageStore();
+  const { messages } = storeToRefs(messageStore);
+  return async (rowKey: string) => {
+    if (!currentRoomId.value) return;
+    else if (!messages.value.some((m) => m.rowKey === rowKey)) {
+      await navigateTo(RoutePath.MessagesMessage(currentRoomId.value, rowKey));
+      return;
+    }
+
     activeRowKey.value = rowKey;
     document.getElementById(rowKey)?.scrollIntoView();
     useTimeoutFn(() => {
