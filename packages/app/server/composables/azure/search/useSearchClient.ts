e8325ae4e97133d2ff848dca1c17f72602589631
Jimmy Chen <jimmyc@resonatesolutions.com.au>
2025-08-18 16:33:29 +1000

    fix: add type so I don't need that type assertion error

--- packages/app/server/composables/azure/search/useSearchClient.ts
+++ packages/app/server/composables/azure/search/useSearchClient.ts
@@ -4,9 +4,11 @@ import type { SearchIndexDocumentMap } from "@@/server/models/azure/search/Searc
 import { getSearchUrl } from "@@/server/services/azure/search/getSearchUrl";
 import { AzureKeyCredential, SearchClient } from "@azure/search-documents";
 
-export const useSearchClient = <TIndex extends SearchIndex>(index: TIndex) => {
+export const useSearchClient = <TIndex extends SearchIndex>(
+  index: TIndex,
+): SearchClient<SearchIndexDocumentMap[TIndex]> => {
   const runtimeConfig = useRuntimeConfig();
   const endpoint = getSearchUrl();
-  const apiKey = runtimeConfig.azure.search.apiKey as string;
+  const apiKey = runtimeConfig.azure.search.apiKey;
   return new SearchClient<SearchIndexDocumentMap[TIndex]>(endpoint, index, new AzureKeyCredential(apiKey));
 };
