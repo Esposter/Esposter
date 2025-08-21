d3f9292ec5a4d4b4f23ba70a4a8654debfdd8d96
Jimmy Chen <jimmychen12138@gmail.com>
2025-08-21 16:34:31 +0800

    fix: null condition

--- packages/shared/src/services/azure/isNull.ts
+++ packages/shared/src/services/azure/isNull.ts
@@ -2,4 +2,5 @@ import { BinaryOperator } from "@/models/azure/BinaryOperator";
 import { Literal } from "@/models/azure/Literal";
 import { UnaryOperator } from "@/models/azure/UnaryOperator";
 // https://stackoverflow.com/questions/4228460/querying-azure-table-storage-for-null-values
-export const isNull = (key: string): string => `${UnaryOperator.not}(${key} ${BinaryOperator.ne} ${Literal.NaN})`;
+export const isNull = (key: string): string =>
+  `(${UnaryOperator.not}(${key} ${BinaryOperator.ne} ${Literal.NaN}) ${UnaryOperator.or} ${key} ${BinaryOperator.eq} 'null')`;
