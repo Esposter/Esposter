7ea99fc35be58f5ace20b5189101522123272979
Jimmy Chen <jimmyc@resonatesolutions.com.au>
2025-09-08 16:03:00 +1000

    fix: conditions

--- packages/app/app/components/Message/Content/Search/Menu.vue
+++ packages/app/app/components/Message/Content/Search/Menu.vue
@@ -7,26 +7,27 @@ const { activeSelectedFilter, isSearched } = storeToRefs(searchMessageStore);
 </script>
 
 <template>
+  <!-- @TODO: Menu should be based on focus of the input -->
   <v-menu location="top" :close-on-content-click="false">
     <template #activator="{ props }">
       <MessageContentSearchInput :="props" />
     </template>
     <StyledCard p-2>
       <MessageContentSearchMessages v-if="isSearched" />
+      <component
+        :is="SearchFilterComponentMap[activeSelectedFilter.type]"
+        v-else-if="
+          activeSelectedFilter && !activeSelectedFilter.value && SearchFilterComponentMap[activeSelectedFilter.type]
+        "
+        @select="
+          (value: string) => {
+            if (!activeSelectedFilter) return;
+            activeSelectedFilter.value = value;
+          }
+        "
+      />
       <template v-else>
-        <component
-          :is="SearchFilterComponentMap[activeSelectedFilter.type]"
-          v-if="
-            activeSelectedFilter && !activeSelectedFilter.value && SearchFilterComponentMap[activeSelectedFilter.type]
-          "
-          @select="
-            (value: string) => {
-              if (!activeSelectedFilter) return;
-              activeSelectedFilter.value = value;
-            }
-          "
-        />
-        <MessageContentSearchOptions v-else />
+        <MessageContentSearchOptions />
         <v-divider mx-4 />
         <MessageContentSearchHistory />
       </template>
