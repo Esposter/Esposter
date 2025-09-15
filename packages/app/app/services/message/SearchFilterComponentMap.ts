52e01fc42646113d846dca85ce3a9c5f3d680e5d
Jimmy Chen <jimmyc@resonatesolutions.com.au>
2025-09-15 14:04:09 +1000

    fix: comment out unsupported filter types for now

--- packages/app/app/services/message/SearchFilterComponentMap.ts
+++ packages/app/app/services/message/SearchFilterComponentMap.ts
@@ -1,15 +1,15 @@
-/* eslint-disable perfectionist/sort-objects */
+// /* eslint-disable perfectionist/sort-objects */
 import { FilterType } from "#shared/models/message/FilterType";
 
 export const SearchFilterComponentMap = {
   [FilterType.From]: defineAsyncComponent(() => import("@/components/Message/Content/Search/Filter/UserPicker.vue")),
-  [FilterType.Mentions]: defineAsyncComponent(
-    () => import("@/components/Message/Content/Search/Filter/UserPicker.vue"),
-  ),
-  [FilterType.Has]: null,
-  [FilterType.Before]: null,
-  [FilterType.During]: null,
-  [FilterType.After]: null,
-  [FilterType.Pinned]: null,
-  [FilterType.AuthorType]: null,
+  // [FilterType.Mentions]: defineAsyncComponent(
+  //   () => import("@/components/Message/Content/Search/Filter/UserPicker.vue"),
+  // ),
+  // [FilterType.Has]: null,
+  // [FilterType.Before]: null,
+  // [FilterType.During]: null,
+  // [FilterType.After]: null,
+  // [FilterType.Pinned]: null,
+  // [FilterType.AuthorType]: null,
 } as const satisfies Record<FilterType, Component | null>;
