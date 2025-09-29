9fc40bbda3c7da8314cc69ffaedf24a118eb2263
Jimmy Chen <jimmyc@resonatesolutions.com.au>
2025-09-29 14:35:32 +1000

    refactor: adding message metadata to a fn

--- packages/app/server/services/message/addMessageMetadata.ts
+++ packages/app/server/services/message/addMessageMetadata.ts
@@ -1,13 +1,26 @@
+import type { AzureUpdateEntity } from "#shared/models/azure/AzureUpdateEntity";
 import type { MessageEntity } from "#shared/models/db/message/MessageEntity";
 
 import { MessageType } from "#shared/models/db/message/MessageType";
 import { MENTION_ID } from "#shared/services/message/constants";
 import { getMentions } from "#shared/services/message/getMentions";
 import { getLinkPreviewResponse } from "@@/server/services/message/getLinkPreviewResponse";
+import { Operation } from "@esposter/shared";
 
-export const addMessageMetadata = async (messageEntity: MessageEntity) => {
+export const addMessageMetadata = async (
+  messageEntity: AzureUpdateEntity<MessageEntity>,
+  operation: Operation.Create | Operation.Update = Operation.Create,
+) => {
   if (messageEntity.type === MessageType.Message && messageEntity.message) {
-    messageEntity.linkPreviewResponse = await getLinkPreviewResponse(messageEntity.message);
+    switch (operation) {
+      case Operation.Create:
+        messageEntity.linkPreviewResponse = await getLinkPreviewResponse(messageEntity.message);
+        break;
+      case Operation.Update:
+        messageEntity.isEdited = true;
+        break;
+    }
+
     messageEntity.mentions = getMentions(messageEntity.message)
       .map((mention) => mention.getAttribute(MENTION_ID))
       .filter((id) => id !== undefined);
