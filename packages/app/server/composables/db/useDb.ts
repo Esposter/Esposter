7816dc477d6334ef6d65c19dc65f44320edd9e4d
Q16solver <jimmychen12138@gmail.com>
2025-10-05 17:11:53 +1100

    fix: don't use runtimeconfig for db

--- packages/app/server/composables/db/useDb.ts
+++ packages/app/server/composables/db/useDb.ts
@@ -2,13 +2,10 @@ import { IS_PRODUCTION } from "#shared/util/environment/constants";
 import { DrizzleLogger } from "@@/server/db/logger";
 import { schema } from "@@/server/db/schema";
 import { drizzle } from "drizzle-orm/postgres-js";
-// @TODO: Remove this import when nuxt fixes its types
-import { useRuntimeConfig } from "nuxt/app";
 import postgres from "postgres";
 
 export const useDb = () => {
-  const runtimeConfig = useRuntimeConfig();
-  const client = postgres(runtimeConfig.database.url);
+  const client = postgres(process.env.DATABASE_URL);
   return drizzle(client, {
     logger: IS_PRODUCTION ? undefined : new DrizzleLogger(),
     schema,
