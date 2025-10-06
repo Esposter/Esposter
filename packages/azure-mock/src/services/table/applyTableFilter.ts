36a0faab76fc19bf70becfaf3a730f3cf0a35d1e
Q16solver <jimmychen12138@gmail.com>
2025-10-06 19:47:52 +1100

    fix: types

--- packages/azure-mock/src/services/table/applyTableFilter.ts
+++ packages/azure-mock/src/services/table/applyTableFilter.ts
@@ -4,7 +4,10 @@ import type { Clause } from "@esposter/shared";
 import { createTableFilterPredicate } from "@/services/table/createTableFilterPredicate";
 import { serializeClauses } from "@esposter/shared";
 
-export const applyTableFilter = <T extends object>(entities: TableEntity<T>[], clauses: Clause[]): TableEntity<T>[] => {
+export const applyTableFilter = <T extends Record<string, unknown>>(
+  entities: TableEntity<T>[],
+  clauses: Clause[],
+): TableEntity<T>[] => {
   const predicate = createTableFilterPredicate<T>(serializeClauses(clauses));
   return entities.filter((e) => predicate(e));
 };
