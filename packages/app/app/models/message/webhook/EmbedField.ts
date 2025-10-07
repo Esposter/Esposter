b4029efe1a5bfed430dabaef49ffffcd0b218ad6
Jimmy Chen <jimmyc@resonatesolutions.com.au>
2025-10-07 14:36:46 +1100

    feat: Add webhook types

---
+++ packages/app/app/models/message/webhook/EmbedField.ts
@@ -0,0 +1,13 @@
+import { z } from "zod";
+
+export interface EmbedField {
+  inline?: boolean;
+  name: string;
+  value: string;
+}
+
+export const embedFieldSchema = z.object({
+  inline: z.boolean().optional(),
+  name: z.string().min(1).max(256),
+  value: z.string().min(1).max(1024),
+}) satisfies z.ZodType<EmbedField>;
---
+++ packages/app/app/models/message/webhook/WebhookPayload.ts
@@ -0,0 +1,25 @@
+import type { Embed } from "@/models/message/webhook/Embed";
+
+import { USER_NAME_MAX_LENGTH } from "#shared/services/user/constants";
+import { embedSchema } from "@/models/message/webhook/Embed";
+import { z } from "zod";
+
+export interface WebhookPayload {
+  avatarUrl?: string;
+  content?: string;
+  embeds?: Embed[];
+  username?: string;
+}
+
+export const webhookPayloadSchema = z
+  .object({
+    avatarUrl: z.url().optional(),
+    content: z.string().max(2000).optional(),
+    embeds: embedSchema.array().max(10).optional(),
+    username: z.string().max(USER_NAME_MAX_LENGTH).optional(),
+  })
+  // Ensure at least one of content or embeds exists
+  // eslint-disable-next-line @typescript-eslint/prefer-nullish-coalescing
+  .refine((data) => data.content || (data.embeds && data.embeds.length > 0), {
+    message: 'Webhook must have either "content" or "embeds".',
+  }) satisfies z.ZodType<WebhookPayload>;
