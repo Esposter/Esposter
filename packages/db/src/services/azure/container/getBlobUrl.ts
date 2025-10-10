1d13edcd94d52da3555377b9d943e66febcc6bc4
Jimmy Chen <jimmyc@resonatesolutions.com.au>
2025-10-10 15:50:00 +1100

    fix: just re-implement in ts

--- packages/db/src/services/azure/container/getBlobUrl.ts
+++ packages/db/src/services/azure/container/getBlobUrl.ts
@@ -1,3 +1,4 @@
-import { getBlobUrl as getBaseBlobUrl } from "@/services/azure/container/getBlobUrl.js";
+import { IS_PRODUCTION } from "@esposter/shared";
 
-export const getBlobUrl: () => string = getBaseBlobUrl;
+export const getBlobUrl = (): string =>
+  IS_PRODUCTION ? "https://pshpstespauea001.blob.core.windows.net" : "https://dshpstespauea001.blob.core.windows.net";
