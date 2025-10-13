09966dc9bf3d18c17334e622e2d6d3aeff2cc6c3
Jimmy Chen <jimmyc@resonatesolutions.com.au>
2025-10-14 10:50:39 +1100

    chore: update todo

--- packages/app/app/components/Dashboard/Visual/Index.vue
+++ packages/app/app/components/Dashboard/Visual/Index.vue
@@ -34,7 +34,7 @@ const options = useApexOptions(
 <template>
   <StyledCard size-full>
     <div ref="container" h-full>
-      <!-- @vue-expect-error https://github.com/apexcharts/vue3-apexcharts/issues/145 -->
+      <!-- @vue-expect-error @TODO: https://github.com/apexcharts/vue3-apexcharts/issues/145 -->
       <VueApexCharts :="data" :options />
     </div>
   </StyledCard>
