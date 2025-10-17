2e53bdad694e214e7b998f332b78fb6450e7e446
Q16solver <jimmychen12138@gmail.com>
2025-10-18 10:55:29 +1100

    fix: actually use the composable

--- packages/app/server/composables/survey/useBlobUrlSearchRegex.ts
+++ packages/app/server/composables/survey/useBlobUrlSearchRegex.ts
@@ -1,11 +1,12 @@
+import { useContainerBaseUrl } from "@@/server/composables/azure/container/useContainerBaseUrl";
 import { AzureContainer } from "@esposter/db-schema";
 import { escapeRegExp } from "@esposter/shared";
 
 export const useBlobUrlSearchRegex = (blobUrl?: string) => {
-  const runtimeConfig = useRuntimeConfig();
+  const containerBaseUrl = useContainerBaseUrl();
   return new RegExp(
     // Match until we reach a character that ends with "
-    `${blobUrl ? escapeRegExp(blobUrl) : `${runtimeConfig.public.azure.container.baseUrl}/${AzureContainer.SurveyAssets}`}[^"]*`,
+    `${blobUrl ? escapeRegExp(blobUrl) : `${containerBaseUrl}/${AzureContainer.SurveyAssets}`}[^"]*`,
     "g",
   );
 };
