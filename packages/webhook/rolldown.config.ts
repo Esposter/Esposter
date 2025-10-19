c360f92cb378bf63eedb0c72af5eecf72829a407
Q16solver <jimmychen12138@gmail.com>
2025-10-19 12:13:12 +1100

    fix: bundle in node env

--- packages/webhook/rolldown.config.ts
+++ packages/webhook/rolldown.config.ts
@@ -3,6 +3,8 @@ import type { ConfigExport } from "rolldown";
 import rolldownConfigurationBase from "@esposter/configuration/rolldown.config.base.js";
 import { defineConfig } from "rolldown";
 
-const rolldownConfiguration: ConfigExport = defineConfig([{ ...rolldownConfigurationBase, external: [] }]);
+const rolldownConfiguration: ConfigExport = defineConfig([
+  { ...rolldownConfigurationBase, external: [], platform: "node" },
+]);
 
 export default rolldownConfiguration;
