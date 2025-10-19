b993dac2a4c1da8a88694f8b1e121cc3cc970cdb
Q16solver <jimmychen12138@gmail.com>
2025-10-19 23:26:48 +1100

    fix: service worker

--- packages/app/public/serviceWorker/push.js
+++ packages/app/public/serviceWorker/push.js
@@ -1,28 +1,22 @@
 // https://github.com/vite-pwa/docs/issues/132
-self.addEventListener("push", ({ data, waitUntil }) => {
-  if (!data) return;
+self.addEventListener("push", (event) => {
+  if (!event.data) return;
 
-  const jsonData = data.json();
+  const jsonData = event.data.json();
   const { title, ...rest } = jsonData;
-  waitUntil((async () => {
+  event.waitUntil((async () => {
     const clients = await self.clients.matchAll({ includeUncontrolled: true });
     for (const client of clients) client.postMessage(jsonData);
     await self.registration.showNotification(title, rest);
   })());
 });
 
-self.addEventListener("notificationclick", async ({ notification, waitUntil }) => {
-  notification.close();
-  const { url } = notification.data;
-  waitUntil(
-    (async () => {
-      const clients = await self.clients.matchAll({ includeUncontrolled: true });
-      for (const client of clients)
-        if (client.url === url) {
-          await client.focus();
-          return;
-        }
-      await self.clients.openWindow(url);
-    })(),
-  );
+self.addEventListener("notificationclick", async (event) => {
+  event.notification.close();
+  const { url } = event.notification.data;
+  event.waitUntil((async () => {
+    const clients = await self.clients.matchAll({ includeUncontrolled: true });
+    for (const client of clients) if (client.url === url) { await client.focus(); return; }
+    await self.clients.openWindow(url);
+  })());
 });
