bb7c129e30eb05f6921b73934e73b8844341e0db
Q16solver <jimmychen12138@gmail.com>
2025-10-19 23:27:45 +1100

    fix: not need for async

--- packages/app/public/serviceWorker/push.js
+++ packages/app/public/serviceWorker/push.js
@@ -4,19 +4,27 @@ self.addEventListener("push", (event) => {
 
   const jsonData = event.data.json();
   const { title, ...rest } = jsonData;
-  event.waitUntil((async () => {
-    const clients = await self.clients.matchAll({ includeUncontrolled: true });
-    for (const client of clients) client.postMessage(jsonData);
-    await self.registration.showNotification(title, rest);
-  })());
+  event.waitUntil(
+    (async () => {
+      const clients = await self.clients.matchAll({ includeUncontrolled: true });
+      for (const client of clients) client.postMessage(jsonData);
+      await self.registration.showNotification(title, rest);
+    })(),
+  );
 });
 
-self.addEventListener("notificationclick", async (event) => {
+self.addEventListener("notificationclick", (event) => {
   event.notification.close();
   const { url } = event.notification.data;
-  event.waitUntil((async () => {
-    const clients = await self.clients.matchAll({ includeUncontrolled: true });
-    for (const client of clients) if (client.url === url) { await client.focus(); return; }
-    await self.clients.openWindow(url);
-  })());
+  event.waitUntil(
+    (async () => {
+      const clients = await self.clients.matchAll({ includeUncontrolled: true });
+      for (const client of clients)
+        if (client.url === url) {
+          await client.focus();
+          return;
+        }
+      await self.clients.openWindow(url);
+    })(),
+  );
 });
