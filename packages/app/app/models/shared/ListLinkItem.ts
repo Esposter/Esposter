c3a32f6faf4eea2a13669181c28f40dedbfde86f
Q16solver <jimmychen12138@gmail.com>
2025-11-05 21:28:12 +1100

    fix: add back type

--- packages/app/app/models/shared/ListLinkItem.ts
+++ packages/app/app/models/shared/ListLinkItem.ts
@@ -1,7 +1,10 @@
 import type { NuxtLinkProps } from "#app";
+import type { AllowedComponentProps, AnchorHTMLAttributes, VNodeProps } from "vue";
 
-export interface ListLinkItem extends NuxtLinkProps {
+export interface ListLinkItem extends Props {
   icon: string;
   onClick?: () => Promise<void>;
   title: string;
 }
+// @TODO: new nuxt version should fix this
+type Props = AllowedComponentProps & AnchorHTMLAttributes & NuxtLinkProps & VNodeProps;
