68ec5cfae83ff662e4b31feebf9a71c3c74fe6a1
Q16solver <jimmychen12138@gmail.com>
2025-11-24 21:56:47 +1100

    fix: just call it on mounted

--- packages/app/app/composables/message/subscribables/useHuddleSubscribables.ts
+++ packages/app/app/composables/message/subscribables/useHuddleSubscribables.ts
@@ -1,3 +1,5 @@
+import type { WatchHandle } from "vue";
+
 import { useHuddle } from "@/composables/message/useHuddle";
 import { useRoomStore } from "@/store/message/room";
 
@@ -6,39 +8,48 @@ export const useHuddleSubscribables = () => {
   const roomStore = useRoomStore();
   const { currentRoomId } = storeToRefs(roomStore);
   const { callPeer, huddleUsers, isInHuddle, removePeerConnection } = useHuddle();
+  let readWatchHandle: undefined | WatchHandle;
+  let eventWatchHandle: undefined | WatchHandle;
 
-  watchImmediate(currentRoomId, async (roomId) => {
-    if (!roomId) return;
+  onMounted(() => {
+    readWatchHandle = watchImmediate(currentRoomId, async (roomId) => {
+      if (!roomId) return;
 
-    const readHuddleUsers = await $trpc.huddle.readHuddleUsers.query({ roomId });
-    huddleUsers.value = readHuddleUsers.map(({ user }) => user);
+      const readHuddleUsers = await $trpc.huddle.readHuddleUsers.query({ roomId });
+      huddleUsers.value = readHuddleUsers.map(({ user }) => user);
+    });
+
+    eventWatchHandle = watchImmediate(currentRoomId, (roomId) => {
+      if (!roomId) return;
+
+      const joinHuddleSubscription = $trpc.huddle.onJoinHuddle.subscribe(
+        { roomId },
+        {
+          onData: (user) => {
+            huddleUsers.value.push(user);
+            if (isInHuddle.value) callPeer(user.id, user);
+          },
+        },
+      );
+      const leaveHuddleSubscription = $trpc.huddle.onLeaveHuddle.subscribe(
+        { roomId },
+        {
+          onData: ({ userId }) => {
+            huddleUsers.value = huddleUsers.value.filter(({ id }) => id !== userId);
+            removePeerConnection(userId);
+          },
+        },
+      );
+
+      return () => {
+        joinHuddleSubscription.unsubscribe();
+        leaveHuddleSubscription.unsubscribe();
+      };
+    });
   });
 
-  watchImmediate(currentRoomId, (roomId) => {
-    if (!roomId) return;
-
-    const joinHuddleSubscription = $trpc.huddle.onJoinHuddle.subscribe(
-      { roomId },
-      {
-        onData: (user) => {
-          huddleUsers.value.push(user);
-          if (isInHuddle.value) callPeer(user.id, user);
-        },
-      },
-    );
-    const leaveHuddleSubscription = $trpc.huddle.onLeaveHuddle.subscribe(
-      { roomId },
-      {
-        onData: ({ userId }) => {
-          huddleUsers.value = huddleUsers.value.filter(({ id }) => id !== userId);
-          removePeerConnection(userId);
-        },
-      },
-    );
-
-    return () => {
-      joinHuddleSubscription.unsubscribe();
-      leaveHuddleSubscription.unsubscribe();
-    };
+  onUnmounted(() => {
+    readWatchHandle?.();
+    eventWatchHandle?.();
   });
 };
