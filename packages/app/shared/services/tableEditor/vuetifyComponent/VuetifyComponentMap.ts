d6f4e826b633acb5dab8f41f5e6d132a3ae80e4f
Jimmy Chen <jimmyc@resonatesolutions.com.au>
2025-11-25 11:51:49 +1100

    fix: finally fix up vuetify type issues

--- packages/app/shared/services/tableEditor/vuetifyComponent/VuetifyComponentMap.ts
+++ packages/app/shared/services/tableEditor/vuetifyComponent/VuetifyComponentMap.ts
@@ -1,14 +1,7 @@
-import type { KebabCasedPropertiesDeep } from "type-fest";
-import type { Component } from "vue";
-
-import { toKebabCase } from "@esposter/shared";
 import * as components from "vuetify/components";
 
 export type VuetifyComponentMap = {
-  [P in keyof KebabCasedPropertiesDeepVuetifyComponents]: KebabCasedPropertiesDeepVuetifyComponents[P];
+  [P in keyof typeof components]: (typeof components)[P];
 };
-type KebabCasedPropertiesDeepVuetifyComponents = KebabCasedPropertiesDeep<typeof components>;
-// TS2590: Expression produces a union type that is too complex to represent.
-export const VuetifyComponentMap = Object.fromEntries(
-  Object.entries(components as Record<string, Component>).map(([name, component]) => [toKebabCase(name), component]),
-) as VuetifyComponentMap;
+
+export const VuetifyComponentMap = components;
