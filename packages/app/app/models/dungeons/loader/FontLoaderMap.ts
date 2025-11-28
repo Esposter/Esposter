357ce4a8f4a47f127bd0595af7565d5a85c7cde1
Q16solver <jimmychen12138@gmail.com>
2025-11-28 22:13:29 +1100

    fix: imports

--- packages/app/app/models/dungeons/loader/FontLoaderMap.ts
+++ packages/app/app/models/dungeons/loader/FontLoaderMap.ts
@@ -1,8 +1,8 @@
 import type { Loader } from "phaser";
 import type { SceneWithPlugins } from "vue-phaserjs";
 
+import { FontKey } from "#shared/models/dungeons/keys/FontKey";
 import KenneyFutureNarrow from "@/assets/dungeons/thirdParty/kenneysAssets/fonts/KenneyFutureNarrow.woff2";
-import { FontKey } from "@/models/dungeons/keys/FontKey";
 
 export const FontLoaderMap: Record<FontKey, (scene: SceneWithPlugins) => Loader.LoaderPlugin> = {
   [FontKey.KenneyFutureNarrow]: (scene) => scene.load.font(FontKey.KenneyFutureNarrow, KenneyFutureNarrow),
