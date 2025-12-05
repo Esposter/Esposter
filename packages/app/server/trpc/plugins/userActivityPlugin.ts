8c8e82865f613f879788b2bf9de122f68413e016
Jimmy Chen <jimmyc@resonatesolutions.com.au>
2025-12-05 14:47:24 +1100

    chore: update more info

--- packages/app/server/trpc/plugins/userActivityPlugin.ts
+++ packages/app/server/trpc/plugins/userActivityPlugin.ts
@@ -12,15 +12,18 @@ export const userActivityPlugin = t.procedure.use(async ({ ctx, next, path }) =>
   const result = await next();
   if (!result.ok) return result;
 
-  const userAgent = ctx.req.headers["User-Agent"];
+  const forwardedFor = ctx.req.headers["x-forwarded-for"] as string | undefined;
   const userActivitiesTableClient = await useTableClient(AzureTable.UserActivities);
   await createEntity(
     userActivitiesTableClient,
     new UserActivityEntity({
+      acceptLanguage: ctx.req.headers["accept-language"],
+      ipAddress: forwardedFor ? forwardedFor.split(",")[0].trim() : ctx.req.socket.remoteAddress,
       partitionKey: ctx.session.user.id,
+      referer: ctx.req.headers.referer,
       rowKey: getReverseTickedTimestamp(),
       triggerPath: path,
-      userAgent: typeof userAgent === "string" ? userAgent : "unknown",
+      userAgent: ctx.req.headers["user-agent"],
     }),
   );
   return result;
--- packages/db-schema/src/models/userActivity/UserActivityEntity.ts
+++ packages/db-schema/src/models/userActivity/UserActivityEntity.ts
@@ -2,12 +2,14 @@ import type { CompositeKeyEntity } from "@/models/azure/table/CompositeKeyEntity
 import type { ToData } from "@esposter/shared";
 
 import { AzureEntity, createAzureEntitySchema } from "@/models/azure/table/AzureEntity";
-import { selectUserSchema } from "@/schema/users";
 import { z } from "zod";
 
 export class UserActivityEntity extends AzureEntity {
+  acceptLanguage?: string;
+  ipAddress?: string;
+  referer?: string;
   triggerPath!: string;
-  userAgent!: string;
+  userAgent?: string;
 
   constructor(init?: Partial<UserActivityEntity> & ToData<CompositeKeyEntity>) {
     super();
@@ -18,11 +20,14 @@ export class UserActivityEntity extends AzureEntity {
 export const userActivityEntitySchema = z.object({
   ...createAzureEntitySchema(
     z.object({
-      partitionKey: selectUserSchema.shape.id,
+      partitionKey: z.string().uuid(), // userId
       // reverse-ticked timestamp
       rowKey: z.string(),
     }),
   ).shape,
+  acceptLanguage: z.string().optional(),
+  ipAddress: z.ipv4().or(z.ipv6()).optional(),
+  referer: z.string().optional(),
   triggerPath: z.string(),
-  userAgent: z.string(),
+  userAgent: z.string().optional(),
 }) satisfies z.ZodType<ToData<UserActivityEntity>>;
