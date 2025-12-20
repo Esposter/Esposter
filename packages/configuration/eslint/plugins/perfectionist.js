83ac04f92b2c2e23d5fbbd1256658c431e0933ef
Jimmy Chen <jimmychen12138@gmail.com>
2025-12-20 17:06:29 +0800

    feat: update eslint plugin perfectionist

--- packages/configuration/eslint/plugins/perfectionist.js
+++ packages/configuration/eslint/plugins/perfectionist.js
@@ -1,15 +1,15 @@
-import perfectionist from "eslint-plugin-perfectionist";
+import { configs } from "eslint-plugin-perfectionist";
 import { defineConfig } from "eslint/config";
 
 export default defineConfig({
-  ...perfectionist.configs["recommended-natural"],
+  ...configs["recommended-natural"],
   ignores: ["**/*.json"],
   rules: {
-    ...perfectionist.configs["recommended-natural"].rules,
+    ...configs["recommended-natural"].rules,
     "perfectionist/sort-imports": [
       "error",
       {
-        ...perfectionist.configs["recommended-natural"].rules["perfectionist/sort-imports"][1],
+        ...configs["recommended-natural"].rules["perfectionist/sort-imports"][1],
         internalPattern: [],
       },
     ],
