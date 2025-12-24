8de2c976c7ead4bb24607b7f28a2d50006ab7202
Q16solver <jimmychen12138@gmail.com>
2025-12-24 21:25:32 +1100

    fix: lint

--- packages/db-schema/src/services/message/createMessageEntity.ts
+++ packages/db-schema/src/services/message/createMessageEntity.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unnecessary-type-assertion */
 import type { CreateMessageInput } from "@/models/message/CreateMessageInput";
 import type { MessageEntityMap } from "@/models/message/MessageEntityMap";
 import type { ServerCreateMessageInput } from "@/models/message/ServerCreateMessageInput";
