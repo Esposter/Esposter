d09d5356b98b7176a9fee9ddf6f4bc34a0d7dd3e
Jimmy Chen <jimmyc@resonatesolutions.com.au>
2026-01-13 10:07:05 +1100

    refactor: specify external types + refactor external symbolic links

--- packages/db/src/services/azure/table/createEntity.ts
+++ packages/db/src/services/azure/table/createEntity.ts
@@ -1,3 +1,4 @@
+import type { TableInsertEntityHeaders } from "@azure/data-tables";
 import type { CompositeKey, CustomTableClient } from "@esposter/db-schema";
 import type { TupleSlice } from "@esposter/shared";
 
@@ -7,7 +8,7 @@ export const createEntity = <TEntity extends CompositeKey>(
   tableClient: CustomTableClient<TEntity>,
   entity: TEntity,
   ...args: TupleSlice<Parameters<CustomTableClient<TEntity>["createEntity"]>, 1>
-) => {
+): Promise<TableInsertEntityHeaders> => {
   const serializedEntity = serializeEntity(entity);
   return tableClient.createEntity(serializedEntity, ...args);
 };
