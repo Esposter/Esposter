7758f4230bd29272fcf682c90041d779d15cf24c
Q16solver <jimmychen12138@gmail.com>
2026-01-19 18:53:40 +1100

    refactor: test values

--- packages/shared/src/util/object/isPlainObject.test.ts
+++ packages/shared/src/util/object/isPlainObject.test.ts
@@ -12,7 +12,7 @@ describe(isPlainObject, () => {
     expect(isPlainObject(null)).toBe(false);
     expect(isPlainObject(undefined)).toBe(false);
     expect(isPlainObject("string")).toBe(false);
-    expect(isPlainObject(123)).toBe(false);
+    expect(isPlainObject(0)).toBe(false);
     expect(isPlainObject(Symbol(""))).toBe(false);
   });
 });
