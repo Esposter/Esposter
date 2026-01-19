2ddedc8f3cba92521aa7d2fdee1c9673685655ba
Q16solver <jimmychen12138@gmail.com>
2026-01-19 18:53:15 +1100

    refactor: test values

--- packages/shared/src/util/object/mergeObjectsStrict.test.ts
+++ packages/shared/src/util/object/mergeObjectsStrict.test.ts
@@ -5,12 +5,12 @@ describe(mergeObjectsStrict, () => {
   test("merges objects", () => {
     expect.hasAssertions();
 
-    expect(mergeObjectsStrict({ a: 0 }, { b: 1 })).toStrictEqual({ a: 0, b: 1 });
+    expect(mergeObjectsStrict({ "": "" }, { " ": " " })).toStrictEqual({ "": "", " ": " " });
   });
 
   test("overwrites properties", () => {
     expect.hasAssertions();
 
-    expect(mergeObjectsStrict({ a: 0 }, { a: 1 })).toStrictEqual({ a: 1 });
+    expect(mergeObjectsStrict({ "": "" }, { "": " " })).toStrictEqual({ "": " " });
   });
 });
