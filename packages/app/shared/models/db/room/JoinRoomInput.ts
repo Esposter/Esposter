390675cf090bcf179dd9b7edba7bc68fc7a9b918
Q16solver <jimmychen12138@gmail.com>
2026-01-24 14:50:40 +1100

    fix: types

--- packages/app/shared/models/db/room/JoinRoomInput.ts
+++ packages/app/shared/models/db/room/JoinRoomInput.ts
@@ -1,4 +1,6 @@
 import type { z } from "zod";
 
+import { selectInviteInMessageSchema } from "@esposter/db-schema";
+
 export const joinRoomInputSchema = selectInviteInMessageSchema.shape.code;
 export type JoinRoomInput = z.infer<typeof joinRoomInputSchema>;
