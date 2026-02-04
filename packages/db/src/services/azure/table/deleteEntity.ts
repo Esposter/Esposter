5f602c56958184bffd305c1f45d4f64e3ede109e
Jimmy Chen <jimmyc@resonatesolutions.com.au>
2026-02-05 09:45:26 +1100

    fix: explicit types

--- packages/db/src/services/azure/table/deleteEntity.ts
+++ packages/db/src/services/azure/table/deleteEntity.ts
@@ -1,6 +1,7 @@
+import type { TableDeleteEntityHeaders } from "@azure/data-tables";
 import type { AzureEntity, CustomTableClient } from "@esposter/db-schema";
 
 export const deleteEntity = <TEntity extends AzureEntity>(
   tableClient: CustomTableClient<TEntity>,
   ...args: Parameters<CustomTableClient<TEntity>["deleteEntity"]>
-) => tableClient.deleteEntity(...args);
+): Promise<TableDeleteEntityHeaders> => tableClient.deleteEntity(...args);
