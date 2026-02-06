ad7fb52753cdeab755f1c54e1c3c6662c356d5d5
Q16solver <jimmychen12138@gmail.com>
2026-02-06 22:56:13 +1100

    fix: links

--- packages/app/app/plugins/trpc.ts
+++ packages/app/app/plugins/trpc.ts
@@ -24,9 +24,9 @@ export default defineNuxtPlugin(() => {
     true: httpLink({ transformer, url: TRPC_CLIENT_PATH }),
   });
 
-  if (getIsServer()) links.push(httpSplitLink);
+  if (getIsServer() || !isProduction) links.push(httpSplitLink);
   // @TODO: Disabling for local development since it currently gives an infinite loop of invalid frame headers when trying to connect via ws
-  else if (isProduction) {
+  else {
     const wsProtocol = window.location.protocol === "https:" ? "wss:" : "ws:";
     const wsClient = createWSClient({ url: `${wsProtocol}//${window.location.host}` });
     links.push(
