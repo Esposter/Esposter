4b7d5ee2b9695d862066b8627303e412f13425fd
Q16solver <jimmychen12138@gmail.com>
2026-02-26 12:32:35 +1100

    fix: try remove resolved tags

--- packages/app/vitest.config.ts
+++ packages/app/vitest.config.ts
@@ -5,16 +5,17 @@ import { dayjs } from "./shared/services/dayjs";
 export default defineVitestConfig({
   test: {
     environment: "nuxt",
-    hookTimeout: dayjs.duration(60, "seconds").asMilliseconds(),
-    projects: [
-      // V4 schema-org tags:afterResolve unhandled rejection fixed by using node environment for server/** tests
-      {
-        test: {
-          environment: "node",
-          include: ["server/**"],
-          name: "node",
+    environmentOptions: {
+      nuxt: {
+        overrides: {
+          unhead: {
+            renderSSRHeadOptions: {
+              resolvedTags: [],
+            },
+          },
         },
       },
-    ],
+    },
+    hookTimeout: dayjs.duration(60, "seconds").asMilliseconds(),
   },
 });
