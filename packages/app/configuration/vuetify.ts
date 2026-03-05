54ea65e2589448ecc8f91d47f7c0d07b7401193e
Jimmy Chen <jimmyc@resonatesolutions.com.au>
2026-03-05 13:54:44 +1100

    feat: update vuetify nuxt module

--- packages/app/configuration/vuetify.ts
+++ packages/app/configuration/vuetify.ts
@@ -2,10 +2,8 @@ import type { NuxtConfig } from "nuxt/schema";
 
 export const vuetify: NuxtConfig["vuetify"] = {
   moduleOptions: {
-    // https://www.youtube.com/watch?v=aamWg1TuC3o
-    disableVuetifyStyles: true,
     styles: {
-      configFile: "assets/css/components.scss",
+      configFile: "@/assets/css/components.scss",
     },
   },
 };
