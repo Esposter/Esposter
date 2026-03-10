488e3426c5b81469ac17de2afa1d06c02e2fd037
Jimmy Chen <jimmyc@resonatesolutions.com.au>
2026-03-10 11:31:16 +1100

    chore: cache lookup

--- packages/app/app/composables/file/useImportJsonFile.ts
+++ packages/app/app/composables/file/useImportJsonFile.ts
@@ -1,8 +1,10 @@
 import { lookup } from "mime-types";
 
+const jsonMimeType = lookup(".json") || "";
+
 export const useImportJsonFile = () => {
   const importFile = useImportFile();
   return async (onSelect: (file: File) => Promise<void>): Promise<void> => {
-    await importFile(lookup(".json") || "", ".json", onSelect);
+    await importFile(jsonMimeType, ".json", onSelect);
   };
 };
