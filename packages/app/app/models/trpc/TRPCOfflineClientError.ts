d01c71cf03b641a173cb3350dc7b5ea3909d6a28
Q16solver <jimmychen12138@gmail.com>
2026-03-25 17:35:36 +1100

    chore: deps

--- packages/app/app/models/trpc/TRPCOfflineClientError.ts
+++ packages/app/app/models/trpc/TRPCOfflineClientError.ts
@@ -1,6 +1,10 @@
+import type { InferrableClientTypes } from "@trpc/server/unstable-core-do-not-import";
+
 import { TRPCClientError } from "@trpc/client";
 
-export class TRPCOfflineClientError extends TRPCClientError<never> {
+export class TRPCOfflineClientError extends TRPCClientError<InferrableClientTypes> {
+  override readonly name = "TRPCOfflineClientError";
+
   constructor() {
     super("offline");
   }
