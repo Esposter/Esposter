ddf642afe11b83af8d46a39b0dbd5652a1414e8f
Jimmy Chen <50786287+Q16solver@users.noreply.github.com>
2026-03-26 19:42:14 +1100

    Update MessageType.ts to add StandardMessageType and update standardMessageTypeSchema

--- packages/db-schema/src/models/message/MessageType.ts
+++ packages/db-schema/src/models/message/MessageType.ts
@@ -7,6 +7,8 @@ export enum MessageType {
   Webhook = "Webhook",
 }
 
+export type StandardMessageType = Exclude<MessageType, MessageType.Webhook>;
+
 export const standardMessageTypeSchema = z.enum(
   Object.values(MessageType).filter((type) => type !== MessageType.Webhook),
-);
+) satisfies z.ZodType<StandardMessageType>;
