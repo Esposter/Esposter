8668ab1e174328c36226498699b4df4005abe500
Jimmy Chen <50786287+Q16solver@users.noreply.github.com>
2026-03-26 19:44:13 +1100

    Update BaseMessageEntity.ts to import StandardMessageType and update generic

--- packages/db-schema/src/models/message/BaseMessageEntity.ts
+++ packages/db-schema/src/models/message/BaseMessageEntity.ts
@@ -2,10 +2,9 @@ import type { FileEntity } from "@/models/azure/table/FileEntity";
 import type { LinkPreviewResponse } from "@/models/message/linkPreview/LinkPreviewResponse";
 import type { ItemEntityType, ToData } from "@esposter/shared";
 import type { Except } from "type-fest";
-
 import { AzureEntity, createAzureEntitySchema } from "@/models/azure/table/AzureEntity";
 import { fileEntitySchema } from "@/models/azure/table/FileEntity";
-import { MessageType, standardMessageTypeSchema } from "@/models/message/MessageType";
+import { MessageType, StandardMessageType, standardMessageTypeSchema } from "@/models/message/MessageType";
 import { selectRoomSchema } from "@/schema/rooms";
 import { selectUserSchema } from "@/schema/users";
 import { FILE_MAX_LENGTH } from "@/services/azure/container/constants";
@@ -14,38 +13,34 @@ import { z } from "zod";
 export const MENTION_MAX_LENGTH = 100;
 export const MESSAGE_MAX_LENGTH = 10000;
 
-export class BaseMessageEntity<TType extends MessageType = Exclude<MessageType, MessageType.Webhook>>
-  extends AzureEntity
-  implements ItemEntityType<TType>
-{
-  files: FileEntity[] = [];
-  isEdited?: true;
-  isForward?: true;
-  // Only used by the frontend for visual effects
-  isLoading?: true;
-  isPinned?: true;
-  linkPreviewResponse: LinkPreviewResponse | null = null;
-  mentions: string[] = [];
-  message!: string;
-  replyRowKey?: string;
-  type = MessageType.Message as TType;
+export class BaseMessageEntity<TType extends StandardMessageType = StandardMessageType> extends AzureEntity implements ItemEntityType<TType> {
+    files: FileEntity[] = [];
+    isEdited?: true;
+    isForward?: true;
+    // Only used by the frontend for visual effects
+    isLoading?: true;
+    isPinned?: true;
+    linkPreviewResponse: LinkPreviewResponse | null = null;
+    mentions: string[] = [];
+    message!: string;
+    replyRowKey?: string;
+    type = MessageType.Message as TType;
 }
 
 export const baseMessageEntitySchema = z.object({
-  ...createAzureEntitySchema(
-    z.object({
-      partitionKey: selectRoomSchema.shape.id,
-      // Reverse-ticked timestamp
-      rowKey: z.string(),
-    }),
-  ).shape,
-  files: fileEntitySchema.array().max(FILE_MAX_LENGTH).default([]),
-  isEdited: z.literal(true).optional(),
-  isForward: z.literal(true).optional(),
-  isPinned: z.literal(true).optional(),
-  mentions: selectUserSchema.shape.id.array().max(MENTION_MAX_LENGTH).default([]),
-  message: z.string().max(MESSAGE_MAX_LENGTH).default(""),
-  replyRowKey: z.string().optional(),
-  type: standardMessageTypeSchema.default(MessageType.Message),
-  userId: selectUserSchema.shape.id,
-}) satisfies z.ZodType<ToData<Except<BaseMessageEntity, "linkPreviewResponse">>>; // We only generate link preview responses via the backend, so we can safely exclude it from the schema
+    ...createAzureEntitySchema(
+        z.object({
+            partitionKey: selectRoomSchema.shape.id, // Reverse-ticked timestamp
+            rowKey: z.string(),
+        }),
+    ).shape,
+    files: fileEntitySchema.array().max(FILE_MAX_LENGTH).default([]),
+    isEdited: z.literal(true).optional(),
+    isForward: z.literal(true).optional(),
+    isPinned: z.literal(true).optional(),
+    mentions: selectUserSchema.shape.id.array().max(MENTION_MAX_LENGTH).default([]),
+    message: z.string().max(MESSAGE_MAX_LENGTH).default(""),
+    replyRowKey: z.string().optional(),
+    type: standardMessageTypeSchema.default(MessageType.Message),
+    userId: selectUserSchema.shape.id,
+}) satisfies z.ZodType<ToData<Except<BaseMessageEntity, "linkPreviewResponse">>>;
