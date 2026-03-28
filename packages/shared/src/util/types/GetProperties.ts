1fe066498dba0ed7705edfe58bd88e7e61373307
Q16solver <jimmychen12138@gmail.com>
2026-03-28 19:10:07 +1100

    perf: optimize

--- packages/shared/src/util/types/GetProperties.ts
+++ packages/shared/src/util/types/GetProperties.ts
@@ -3,45 +3,52 @@ export type GetProperties<
   Prefix extends string = "",
   Depth extends unknown[] = [unknown, unknown, unknown, unknown, unknown],
   IsRoot extends boolean = true,
-> = Depth extends []
+> = [T] extends [never]
   ? never
-  : NonNullable<T> extends infer O
-    ? O extends unknown[]
-      ? // Handle arrays and tuples
+  : Depth extends []
+    ? never
+    : NonNullable<T> extends infer O
+      ? O extends unknown[]
+        ? GetArrayProps<O, Prefix, Depth, IsRoot>
+        : O extends object
+          ? GetObjectProps<O, Prefix, Depth, IsRoot>
+          : GetPrimitiveProps<O, Prefix, Depth, IsRoot>
+      : never;
+
+type GetArrayProps<T extends unknown[], Prefix extends string, Depth extends unknown[], IsRoot extends boolean> =
+  | (Depth extends [unknown, ...infer Rest]
+      ? GetProperties<T[number], IsRoot extends true ? `${Prefix}[number]` : `${Prefix}.[number]`, Rest, false>
+      : never)
+  | {
+      path: IsRoot extends true ? "length" : `${Prefix}.length`;
+      value: T["length"];
+    };
+
+type GetObjectProps<T, Prefix extends string, Depth extends unknown[], IsRoot extends boolean> = {
+  [K in keyof KnownKeys<T> & (number | string)]: K extends `${number}`
+    ? never
+    : NonNullable<T[K]> extends Function
+      ? never
+      :
           | (Depth extends [unknown, ...infer Rest]
-              ? GetProperties<O[number], IsRoot extends true ? `${Prefix}[number]` : `${Prefix}.[number]`, Rest, false>
+              ? GetProperties<T[K], IsRoot extends true ? `${K}` : `${Prefix}.${K}`, Rest, false>
               : never)
-          | {
-              path: IsRoot extends true ? "length" : `${Prefix}.length`;
-              value: O extends { length: infer L } ? L : number;
-            }
-      : O extends object
-        ? {
-            [K in keyof KnownKeys<O> & (number | string)]: K extends `${number}`
-              ? never
-              : NonNullable<O[K]> extends Function
-                ? never
-                :
-                    | (Depth extends [unknown, ...infer Rest]
-                        ? GetProperties<O[K], IsRoot extends true ? `${K}` : `${Prefix}.${K}`, Rest, false>
-                        : never)
-                    | { path: IsRoot extends true ? `${K}` : `${Prefix}.${K}`; value: O[K] };
-          }[keyof KnownKeys<O> & (number | string)]
-        : // For primitive types
-          O extends string
-          ? { path: IsRoot extends true ? "length" : `${Prefix}.length`; value: number }
-          : O extends symbol
-            ?
-                | (Depth extends [unknown, ...infer Rest]
-                    ? GetProperties<
-                        string | undefined,
-                        IsRoot extends true ? "description" : `${Prefix}.description`,
-                        Rest,
-                        false
-                      >
-                    : never)
-                | { path: IsRoot extends true ? "description" : `${Prefix}.description`; value: string | undefined }
-            : never
+          | { path: IsRoot extends true ? `${K}` : `${Prefix}.${K}`; value: T[K] };
+}[keyof KnownKeys<T> & (number | string)];
+
+type GetPrimitiveProps<T, Prefix extends string, Depth extends unknown[], IsRoot extends boolean> = T extends string
+  ? { path: IsRoot extends true ? "length" : `${Prefix}.length`; value: number }
+  : T extends symbol
+    ?
+        | (Depth extends [unknown, ...infer Rest]
+            ? GetProperties<
+                string | undefined,
+                IsRoot extends true ? "description" : `${Prefix}.description`,
+                Rest,
+                false
+              >
+            : never)
+        | { path: IsRoot extends true ? "description" : `${Prefix}.description`; value: string | undefined }
     : never;
 
 type KnownKeys<T> = {
