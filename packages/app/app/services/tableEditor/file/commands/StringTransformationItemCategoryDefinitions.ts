74ac7ecba012970ca8dfe5e259ea99421b45f7c7
Q16solver <jimmychen12138@gmail.com>
2026-03-28 15:20:32 +1100

    fix: prettify names

--- packages/app/app/services/tableEditor/file/commands/StringTransformationItemCategoryDefinitions.ts
+++ packages/app/app/services/tableEditor/file/commands/StringTransformationItemCategoryDefinitions.ts
@@ -1,11 +1,12 @@
 import type { SelectItemCategoryDefinition } from "@/models/vuetify/SelectItemCategoryDefinition";
 
 import { BasicStringTransformationType } from "#shared/models/tableEditor/file/column/transformation/string/BasicStringTransformationType";
+import { prettify } from "@/util/text/prettify";
 
 export const StringTransformationItemCategoryDefinitions: SelectItemCategoryDefinition<BasicStringTransformationType>[] =
   [
-    { title: "Lowercase", value: BasicStringTransformationType.Lowercase },
-    { title: "Title Case", value: BasicStringTransformationType.TitleCase },
-    { title: "Trim", value: BasicStringTransformationType.Trim },
-    { title: "Uppercase", value: BasicStringTransformationType.Uppercase },
+    { title: prettify(BasicStringTransformationType.Lowercase), value: BasicStringTransformationType.Lowercase },
+    { title: prettify(BasicStringTransformationType.TitleCase), value: BasicStringTransformationType.TitleCase },
+    { title: prettify(BasicStringTransformationType.Trim), value: BasicStringTransformationType.Trim },
+    { title: prettify(BasicStringTransformationType.Uppercase), value: BasicStringTransformationType.Uppercase },
   ];
