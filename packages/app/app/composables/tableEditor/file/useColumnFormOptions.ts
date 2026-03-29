7a1cc146f894417d0c269a4f88045d15f34f971c
Q16solver <jimmychen12138@gmail.com>
2026-03-29 17:05:09 +1100

    refactor: column items

--- packages/app/app/composables/tableEditor/file/useColumnFormOptions.ts
+++ packages/app/app/composables/tableEditor/file/useColumnFormOptions.ts
@@ -1,9 +1,16 @@
+import type { Column } from "#shared/models/tableEditor/file/column/Column";
 import type { DataSource } from "#shared/models/tableEditor/file/datasource/DataSource";
 import type { ColumnFormVjsfContext } from "@/models/tableEditor/file/column/ColumnFormVjsfContext";
 import type { VjsfOptions } from "@/models/vjsf/VjsfOptions";
+import type { SelectItemCategoryDefinition } from "@/models/vuetify/SelectItemCategoryDefinition";
 import type { MaybeRefOrGetter } from "vue";
 
 import { ColumnType } from "#shared/models/tableEditor/file/column/ColumnType";
+
+const mapColumnToSelectItemCategoryDefinition = ({ id, name }: Column): SelectItemCategoryDefinition<Column["id"]> => ({
+  title: name,
+  value: id,
+});
 
 export const useColumnFormOptions = (dataSource: MaybeRefOrGetter<DataSource>, currentName: MaybeRefOrGetter<string>) =>
   computed<VjsfOptions<ColumnFormVjsfContext>>(() => {
@@ -11,18 +18,24 @@ export const useColumnFormOptions = (dataSource: MaybeRefOrGetter<DataSource>, c
     const currentNameValue = toValue(currentName);
     return {
       context: {
-        columnItems: dataSourceValue.columns.map(({ id, name }) => ({ title: name, value: id })),
+        booleanColumnItems: dataSourceValue.columns
+          .filter(({ type }) => type === ColumnType.Boolean)
+          .map((column) => mapColumnToSelectItemCategoryDefinition(column)),
+        columnItems: dataSourceValue.columns.map(mapColumnToSelectItemCategoryDefinition),
         columnNames: dataSourceValue.columns.map(({ name }) => name),
+        computedColumnItems: dataSourceValue.columns
+          .filter(({ type }) => type === ColumnType.Computed)
+          .map((column) => mapColumnToSelectItemCategoryDefinition(column)),
         currentName: currentNameValue,
         dateColumnItems: dataSourceValue.columns
-          .filter(({ type }) => type === ColumnType.Date || type === ColumnType.Computed)
-          .map(({ id, name }) => ({ title: name, value: id })),
+          .filter(({ type }) => type === ColumnType.Date)
+          .map((column) => mapColumnToSelectItemCategoryDefinition(column)),
         numberColumnItems: dataSourceValue.columns
-          .filter(({ type }) => type === ColumnType.Number || type === ColumnType.Computed)
-          .map(({ id, name }) => ({ title: name, value: id })),
+          .filter(({ type }) => type === ColumnType.Number)
+          .map((column) => mapColumnToSelectItemCategoryDefinition(column)),
         stringColumnItems: dataSourceValue.columns
-          .filter(({ type }) => type === ColumnType.String || type === ColumnType.Computed)
-          .map(({ id, name }) => ({ title: name, value: id })),
+          .filter(({ type }) => type === ColumnType.String)
+          .map((column) => mapColumnToSelectItemCategoryDefinition(column)),
       },
     };
   });
