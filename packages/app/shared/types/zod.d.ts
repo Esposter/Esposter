60df9af06586bd5b2493e920d61af3238fdac317
Jimmy Chen <jimmyc@resonatesolutions.com.au>
2026-03-30 15:33:55 +1100

    chore: fix use record

--- packages/app/shared/types/zod.d.ts
+++ packages/app/shared/types/zod.d.ts
@@ -1,9 +1,7 @@
 import type { uniqueColumnNameKeywordDefinition } from "@/services/ajv/keywords/uniqueColumnNameKeywordDefinition";
 import type { PartialCompObject } from "@json-layout/vocabulary";
 
-type AjvKeywords = {
-  [K in (typeof uniqueColumnNameKeywordDefinition)["keyword"]]?: boolean;
-};
+type AjvKeywords = Partial<Record<(typeof uniqueColumnNameKeywordDefinition)["keyword"], boolean>>;
 
 declare module "zod" {
   interface GlobalMeta extends AjvKeywords {
