817747328cd2343ae5bec6e5e65c5fdedc236f09
Jimmy Chen <jimmyc@resonatesolutions.com.au>
2026-04-01 13:25:28 +1100

    fix: interfaces

---
+++ packages/app/shared/models/entity/Description.ts
@@ -0,0 +1,10 @@
+import { DESCRIPTION_MAX_LENGTH } from "#shared/services/constants";
+import { z } from "zod";
+
+export interface Description {
+  description: string;
+}
+
+export const descriptionSchema = z.object({
+  description: z.string().max(DESCRIPTION_MAX_LENGTH),
+});
