b36de2f5b8852c1e61a56bd16c4b5c0dfde1dd02
Jimmy Chen <jimmyc@resonatesolutions.com.au>
2026-04-02 09:48:55 +1100

    fix: builds

--- packages/app/app/services/tableEditor/file/column/FieldInputComponentMap.ts
+++ packages/app/app/services/tableEditor/file/column/FieldInputComponentMap.ts
@@ -1,7 +1,7 @@
 import { ColumnType } from "#shared/models/tableEditor/file/column/ColumnType";
-import FieldInputBoolean from "@/components/TableEditor/File/Row/FieldInput/Boolean.vue";
-import FieldInputDate from "@/components/TableEditor/File/Row/FieldInput/Date.vue";
-import FieldInputText from "@/components/TableEditor/File/Row/FieldInput/Text.vue";
+import FieldInputBoolean from "@/components/TableEditor/File/Row/Field/Input/Boolean.vue";
+import FieldInputDate from "@/components/TableEditor/File/Row/Field/Input/Date.vue";
+import FieldInputText from "@/components/TableEditor/File/Row/Field/Input/Text.vue";
 
 export const FieldInputComponentMap = {
   [ColumnType.Boolean]: FieldInputBoolean,
