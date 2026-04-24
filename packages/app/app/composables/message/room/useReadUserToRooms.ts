77601a6297dd12c5e87ddd114450e71c9db9135c
Q16solver <jimmychen12138@gmail.com>
2026-04-24 21:47:32 +1000

    fix: types

--- packages/app/app/composables/message/room/useReadUserToRooms.ts
+++ packages/app/app/composables/message/room/useReadUserToRooms.ts
@@ -5,11 +5,11 @@ import { useUserToRoomStore } from "@/store/message/room/userToRoom";
 export const useReadUserToRooms = () => {
   const { $trpc } = useNuxtApp();
   const userToRoomStore = useUserToRoomStore();
-  const { setNotificationTypeMap } = userToRoomStore;
+  const { setNotificationType } = userToRoomStore;
   return async (roomIds: Room["id"][]) => {
     if (roomIds.length === 0) return;
 
     const readUserToRooms = await $trpc.userToRoom.readUserToRooms.query({ roomIds });
-    for (const { notificationType, roomId } of readUserToRooms) setNotificationTypeMap(roomId, notificationType);
+    for (const { notificationType, roomId } of readUserToRooms) setNotificationType(roomId, notificationType);
   };
 };
