fc6161ea1ff568dcd53678b9eabf70caa0364797
Jimmy Chen <jimmyc@resonatesolutions.com.au>
2026-05-05 19:49:32 +1000

    fix: remove unnecessary import

--- packages/app/shared/error/withFinalizer.ts
+++ packages/app/shared/error/withFinalizer.ts
@@ -1,5 +1,3 @@
-import { toAppError } from "@esposter/shared";
-
 export const withFinalizer = async <T>(fn: () => PromiseLike<T>, finalizer: () => PromiseLike<unknown>): Promise<T> => {
   const runFinalizer = () => getResultAsync(Promise.resolve(finalizer())).match(() => undefined, console.error);
   const result = await getResultAsync(Promise.resolve(fn()));
