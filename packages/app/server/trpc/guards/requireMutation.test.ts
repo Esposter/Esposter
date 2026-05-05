e11d2e3c8bdca7213c617dc65fa731d913c652e2
Jimmy Chen <jimmyc@resonatesolutions.com.au>
2026-05-05 15:36:43 +1000

    fix: lint

--- packages/app/server/trpc/guards/requireMutation.test.ts
+++ packages/app/server/trpc/guards/requireMutation.test.ts
@@ -16,30 +16,30 @@ describe(requireMutation, () => {
   test("throws TRPCError with code BAD_REQUEST when result is undefined (default)", () => {
     expect.hasAssertions();
 
-    let error: unknown;
+    let err: unknown;
     try {
       requireMutation(undefined, Operation.Create, "Entity", "1");
-    } catch (err) {
-      error = err;
+    } catch (error) {
+      err = error;
     }
 
-    expect(error).toBeInstanceOf(TRPCError);
-    expect((error as TRPCError).code).toBe("BAD_REQUEST");
-    expect(error).toMatchInlineSnapshot(`[TRPCError: Invalid operation: Create, name: Entity, 1]`);
+    expect(err).toBeInstanceOf(TRPCError);
+    expect((err as TRPCError).code).toBe("BAD_REQUEST");
+    expect(err).toMatchInlineSnapshot(`[TRPCError: Invalid operation: Create, name: Entity, 1]`);
   });
 
   test("throws TRPCError with code NOT_FOUND when result is undefined and code param is NOT_FOUND", () => {
     expect.hasAssertions();
 
-    let error: unknown;
+    let err: unknown;
     try {
       requireMutation(undefined, Operation.Create, "Entity", "1", "NOT_FOUND");
-    } catch (err) {
-      error = err;
+    } catch (error) {
+      err = error;
     }
 
-    expect(error).toBeInstanceOf(TRPCError);
-    expect((error as TRPCError).code).toBe("NOT_FOUND");
-    expect(error).toMatchInlineSnapshot(`[TRPCError: Invalid operation: Create, name: Entity, 1]`);
+    expect(err).toBeInstanceOf(TRPCError);
+    expect((err as TRPCError).code).toBe("NOT_FOUND");
+    expect(err).toMatchInlineSnapshot(`[TRPCError: Invalid operation: Create, name: Entity, 1]`);
   });
 });
