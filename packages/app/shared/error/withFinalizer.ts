bc2ba370b86a1ddc241bd818d7ff30fc0bf066e3
Jimmy Chen <jimmyc@resonatesolutions.com.au>
2026-05-05 19:50:16 +1000

    fix: imports

--- packages/app/shared/error/withFinalizer.ts
+++ packages/app/shared/error/withFinalizer.ts
@@ -1,3 +1,5 @@
+import { getResultAsync } from "@esposter/shared";
+
 export const withFinalizer = async <T>(fn: () => PromiseLike<T>, finalizer: () => PromiseLike<unknown>): Promise<T> => {
   const runFinalizer = () => getResultAsync(Promise.resolve(finalizer())).match(() => undefined, console.error);
   const result = await getResultAsync(Promise.resolve(fn()));
