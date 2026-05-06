8530fc2d313f58aca3726b717fb266d5ac224384
Jimmy Chen <jimmyc@resonatesolutions.com.au>
2026-05-06 23:32:38 +1000

    fix: options + build error

--- packages/app/configuration/site.ts
+++ packages/app/configuration/site.ts
@@ -1,9 +1,9 @@
 import type { NuxtConfig } from "nuxt/schema";
 
-import { SITE_DESCRIPTION, SITE_NAME } from "../shared/services/app/constants";
+import { SITE_DESCRIPTION, SITE_NAME, SITE_URL } from "../shared/services/app/constants";
 
 export const site: NuxtConfig["site"] = {
   description: SITE_DESCRIPTION,
   name: SITE_NAME,
-  url: process.env.BASE_URL,
+  url: process.env.BASE_URL ?? SITE_URL,
 };
