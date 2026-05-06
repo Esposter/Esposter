77569ab9ea48b48bf880220f9861834fe735d4ec
Jimmy Chen <jimmychen12138@gmail.com>
2026-05-07 08:58:34 +1000

    fix: tests

--- packages/db-mock/src/createMockDb.ts
+++ packages/db-mock/src/createMockDb.ts
@@ -2,7 +2,7 @@ import type { PostgresJsDatabase } from "drizzle-orm/postgres-js";
 
 import "@/util/bigintPolyfill";
 import { PGlite } from "@electric-sql/pglite";
-import { messageSchema, relations } from "@esposter/db-schema";
+import { messageSchema, relations, schema } from "@esposter/db-schema";
 import { generateDrizzleJson, generateMigration } from "drizzle-kit/api-postgres";
 import { sql } from "drizzle-orm";
 import { drizzle } from "drizzle-orm/pglite";
@@ -12,7 +12,7 @@ export const createMockDb = async (): Promise<PostgresJsDatabase<typeof relation
   const db = drizzle({ client, relations });
   await db.execute(sql.raw(`CREATE SCHEMA "${messageSchema.schemaName}"`));
   const previousJson = await generateDrizzleJson({});
-  const currentJson = await generateDrizzleJson(relations, previousJson.id);
+  const currentJson = await generateDrizzleJson(schema, previousJson.id);
   const statements = await generateMigration(previousJson, currentJson);
   for (const statement of statements) await db.execute(statement);
   return db as unknown as PostgresJsDatabase<typeof relations>;
