97fa9674fdc681e4b2dcf1e1c72a4a569ebe2b76
Q16solver <jimmychen12138@gmail.com>
2026-05-10 17:28:54 +1000

    chore: migrate to inbuilt options

--- packages/app/configuration/vuetify.ts
+++ packages/app/configuration/vuetify.ts
@@ -2,10 +2,10 @@ import type { NuxtConfig } from "nuxt/schema";
 
 export const vuetify: NuxtConfig["vuetify"] = {
   moduleOptions: {
-    disableVuetifyStyles: true,
     prefixComposables: true,
     styles: {
-      configFile: "@/assets/css/settings.scss",
+      colors: false,
+      utilities: false,
     },
   },
 };
