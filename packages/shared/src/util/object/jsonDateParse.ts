326e1d481d10e83c876eabbaaeffe4538ecf5644
Jimmy Chen <jimmyc@resonatesolutions.com.au>
2026-05-12 10:55:21 +1000

    fix: code review comments

--- packages/shared/src/util/object/jsonDateParse.ts
+++ packages/shared/src/util/object/jsonDateParse.ts
@@ -2,7 +2,7 @@
 import { takeOne } from "@/util/array/takeOne";
 
 const ISO_DATE_REGEX = /^(\d{4})-(\d{2})-(\d{2})T(\d{2}):(\d{2}):(\d{2}(?:\.{0,1}\d*))(?:Z|(\+|-)([\d|:]*))?$/u;
-const MS_AJAX_DATE_REGEX = /^\/Date\((d|-|.*)\)[/|\\]$/u;
+const MS_AJAX_DATE_REGEX = /^\/Date\((-?\d+(?:[-+]\d+)?)\)[/\\]$/u;
 // oxlint-disable-next-line @typescript-eslint/no-unnecessary-type-parameters
 export const jsonDateParse = <T = any>(text: string): T =>
   JSON.parse(text, (_key, value) => {
