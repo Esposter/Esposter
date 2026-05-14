0ee20c90171e3f8bb79fd306fd8f47a924fe4d22
Jimmy Chen <jimmyc@resonatesolutions.com.au>
2026-05-14 13:30:07 +1000

    fix: styles

--- packages/app/app/components/Message/Content/Call/ParticipantTile.vue
+++ packages/app/app/components/Message/Content/Call/ParticipantTile.vue
@@ -34,16 +34,17 @@ const displayName = computed(() => (isSelf ? `${participant.name} (You)` : parti
       v-if="videoStream"
       autoplay
       playsinline
-      inset-0
+      rd-2
+      size-full
       absolute
       object-cover
       :srcObject.prop="videoStream"
       :muted="isSelf"
     />
-    <div v-else bg-surface-opacity-80 size-full flex items-center justify-center>
+    <StyledCard v-else rd-2 flex size-full items-center justify-center>
       <StyledAvatar :image="participant.image" :name="participant.name" :avatar-props="{ size: '6rem' }" />
-    </div>
-    <div m-2 px-2 py-1 rd bg-surface-opacity-80 flex gap-x-2 items-center bottom-0 left-0 absolute>
+    </StyledCard>
+    <StyledCard m-2 px-2 py-1 rd-2 flex gap-x-2 items-center bottom-0 left-0 absolute>
       <span font-medium truncate text-body-small>
         {{ displayName }}
       </span>
@@ -51,6 +52,6 @@ const displayName = computed(() => (isSelf ? `${participant.name} (You)` : parti
       <v-icon v-if="participant.isCameraEnabled" text-primary icon="mdi-video" size="small" />
       <v-icon v-if="participant.isMuted" icon="mdi-microphone-off" size="small" />
       <v-icon v-if="isDeafened" icon="mdi-headphones-off" size="small" />
-    </div>
+    </StyledCard>
   </div>
 </template>
--- packages/app/app/components/Message/Content/Call/ScreenShareStage.vue
+++ packages/app/app/components/Message/Content/Call/ScreenShareStage.vue
@@ -8,8 +8,8 @@ const { presenterName, stream } = defineProps<CallScreenShareStageProps>();
 </script>
 
 <template>
-  <div bg-black flex flex-1 min-h-0 items-center justify-center relative>
-    <video autoplay playsinline size-full object-contain :srcObject.prop="stream" />
+  <div flex flex-1 min-h-0 items-center justify-center relative>
+    <video autoplay playsinline rd-2 size-full object-contain :srcObject.prop="stream" />
     <div m-4 px-3 py-2 rd bg-surface-opacity-80 bottom-0 left-0 absolute>
       <span font-medium text-body-small>{{ presenterName }}'s screen</span>
     </div>
