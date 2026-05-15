d74be9ff1628b73a1bfac07444a43bd4735a3f18
Jimmy Chen <jimmyc@resonatesolutions.com.au>
2026-05-15 14:33:43 +1000

    fix: lint

--- packages/app/shared/models/db/message/ReadMetadataInput.ts
+++ packages/app/shared/models/db/message/ReadMetadataInput.ts
@@ -1,7 +1,6 @@
+import { MAX_READ_LIMIT } from "#shared/services/pagination/constants";
 import { selectRoomInMessageSchema, standardMessageEntitySchema } from "@esposter/db-schema";
 import { z } from "zod";
-
-import { MAX_READ_LIMIT } from "#shared/services/pagination/constants";
 
 export const readMetadataInputSchema = z.object({
   messageRowKeys: standardMessageEntitySchema.shape.rowKey.array().min(1).max(MAX_READ_LIMIT),
