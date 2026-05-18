ae146b95e269282e4f5d7d248cfc2d39b6c65170
Q16solver <jimmychen12138@gmail.com>
2026-05-18 19:32:14 +1000

    infra: add new dev resources

---
+++ packages/infra/src/resources/Microsoft.AlertsManagement/smartDetectorAlertRules/devAppiEsposterAuea001DependencyLatencyDegradation.ts
@@ -0,0 +1,52 @@
+import ApplicationTags from "@/constants/ApplicationTags";
+import AzureGlobalLocation from "@/constants/AzureGlobalLocation";
+import { devAgEsposterAuea002 } from "@/resources/Microsoft.Insights/actionGroups/devAgEsposterAuea002";
+import { devAppiEsposterAuea001 } from "@/resources/Microsoft.Insights/components/devAppiEsposterAuea001";
+import { devRgEsposterAuea001 } from "@/resources/Microsoft.Resources/resourceGroups/devRgEsposterAuea001";
+import { getSmartDetectorResourceId } from "@/services/getSmartDetectorResourceId";
+import * as azure_native from "@pulumi/azure-native";
+
+const alertRuleName = "Dependency Latency Degradation - dev-appi-esposter-auea-001";
+
+export const devAppiEsposterAuea001DependencyLatencyDegradation: azure_native.alertsmanagement.SmartDetectorAlertRule =
+  new azure_native.alertsmanagement.SmartDetectorAlertRule(
+    alertRuleName,
+    {
+      actionGroups: {
+        groupIds: [
+          getSmartDetectorResourceId(
+            devRgEsposterAuea001.name,
+            "microsoft.insights",
+            "actiongroups",
+            devAgEsposterAuea002.name,
+          ),
+        ],
+      },
+      alertRuleName,
+      description:
+        "Dependency Latency Degradation notifies you of an unusual increase in response by a dependency your app is calling (e.g. REST API or database)",
+      detector: {
+        id: "DependencyPerformanceDegradationDetector",
+      },
+      frequency: "P1D",
+      location: AzureGlobalLocation,
+      resourceGroupName: devRgEsposterAuea001.name,
+      scope: [
+        getSmartDetectorResourceId(
+          devRgEsposterAuea001.name,
+          "microsoft.insights",
+          "components",
+          devAppiEsposterAuea001.name,
+        ),
+      ],
+      severity: azure_native.alertsmanagement.Severity.Sev3,
+      state: azure_native.alertsmanagement.AlertRuleState.Enabled,
+      tags: {
+        ...ApplicationTags,
+      },
+    },
+    {
+      parent: devAppiEsposterAuea001,
+      protect: true,
+    },
+  );
