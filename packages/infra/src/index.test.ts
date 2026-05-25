d9b0f1a0cd27ac92e8921bc23e0ed0ddcf337a1d
Jimmy Chen <jimmyc@resonatesolutions.com.au>
2026-05-25 17:09:19 +1000

    feat: Add remaining bundle tests

---
+++ packages/infra/src/index.test.ts
@@ -0,0 +1,12 @@
+import { statSync } from "fs";
+import { resolve } from "path";
+import { describe, expect, test } from "vitest";
+
+const distFile = resolve(import.meta.dirname, "../dist/index.js");
+
+describe("@esposter/infra", () => {
+  test("bundle size", () => {
+    expect.hasAssertions();
+    expect(statSync(distFile).size).toMatchInlineSnapshot(`124151`);
+  });
+});
