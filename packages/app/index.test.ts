30f116d5214e48c4bbfd98f9a6934965b763193c
Q16solver <jimmychen12138@gmail.com>
2026-05-29 10:31:13 +1000

    chore: deps

--- packages/app/index.test.ts
+++ packages/app/index.test.ts
@@ -32,6 +32,6 @@ describe("@esposter/app", () => {
 
   test("client js bundle size", () => {
     expect.hasAssertions();
-    expect(getCrossPlatformDirectorySize(nuxtDir)).toMatchInlineSnapshot(`"_nuxt: 109381.13 KB (112006272 bytes)"`);
+    expect(getCrossPlatformDirectorySize(nuxtDir)).toMatchInlineSnapshot(`"_nuxt: 109380.74 KB (112005875 bytes)"`);
   });
 });
