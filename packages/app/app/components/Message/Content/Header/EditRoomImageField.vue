f86c48c9e67a8b615cec0364d7c8f7c29c745d35
Q16solver <jimmychen12138@gmail.com>
2026-05-30 15:24:25 +1000

    feat: Add remove icon

--- packages/app/app/components/Message/Content/Header/EditRoomImageField.vue
+++ packages/app/app/components/Message/Content/Header/EditRoomImageField.vue
@@ -19,60 +19,88 @@ const isLoading = ref(false);
 </script>
 
 <template>
-  <div flex justify-center>
-    <v-hover>
-      <template #default="{ isHovering, props: hoverProps }">
-        <v-tooltip text="Upload Room Image">
-          <template #activator="{ props: tooltipProps }">
-            <button
-              p-0
-              b-0
-              bg-transparent
-              relative
-              type="button"
-              :op-high-emphasis="!isLoading && !isHovering ? '' : undefined"
-              :op-loading="isLoading ? '' : undefined"
-              :="mergeProps(hoverProps, tooltipProps)"
-              @click="input?.click()"
-            >
-              <v-avatar color="background" size="7rem">
-                <v-img v-if="image" :src="image" :alt="name" cover />
-                <v-icon v-else icon="mdi-account-multiple" size="3rem" />
-              </v-avatar>
-              <div v-if="isLoading" flex items-center inset-0 justify-center absolute>
-                <v-progress-circular indeterminate />
-              </div>
-              <v-avatar b-4 b-background b-solid bg-surface right--1 top--1 absolute size="2.5rem">
-                <v-icon icon="mdi-pencil" size="1.25rem" />
-              </v-avatar>
-            </button>
-          </template>
-        </v-tooltip>
-        <input
-          ref="input"
-          type="file"
-          accept="image/*"
-          hidden
-          @change="
-            async (event) => {
-              const file = (event.target as HTMLInputElement).files?.[0];
-              if (!file || file.size > MAX_FILE_REQUEST_SIZE) return;
-              isLoading = true;
-              await withFinalizerAsync(
-                async () => {
-                  const { publicUrl, sasUrl } = await $trpc.room.generateProfileImageUploadUrl.mutate({ roomId });
-                  await uploadBlocks(file, sasUrl);
-                  await $trpc.room.updateRoom.mutate({ id: roomId, image: publicUrl });
-                },
-                () => {
-                  isLoading = false;
-                  if (input) input.value = '';
-                },
-              );
-            }
-          "
-        />
-      </template>
-    </v-hover>
+  <div flex flex-col gap-y-3 items-center>
+    <div flex justify-center>
+      <v-hover>
+        <template #default="{ isHovering, props: hoverProps }">
+          <v-tooltip text="Upload Room Image">
+            <template #activator="{ props: tooltipProps }">
+              <button
+                p-0
+                b-0
+                bg-transparent
+                relative
+                type="button"
+                :disabled="isLoading"
+                :op-high-emphasis="!isLoading && !isHovering ? '' : undefined"
+                :op-loading="isLoading ? '' : undefined"
+                :="mergeProps(hoverProps, tooltipProps)"
+                @click="input?.click()"
+              >
+                <v-avatar color="background" size="7rem">
+                  <v-img v-if="image" :src="image" :alt="name" cover />
+                  <v-icon v-else icon="mdi-account-multiple" size="3rem" />
+                </v-avatar>
+                <div v-if="isLoading" flex items-center inset-0 justify-center absolute>
+                  <v-progress-circular indeterminate />
+                </div>
+                <v-avatar b-4 b-background b-solid bg-surface right--1 top--1 absolute size="2.5rem">
+                  <v-icon icon="mdi-pencil" size="1.25rem" />
+                </v-avatar>
+              </button>
+            </template>
+          </v-tooltip>
+          <input
+            ref="input"
+            type="file"
+            accept="image/*"
+            hidden
+            @change="
+              async (event) => {
+                const file = (event.target as HTMLInputElement).files?.[0];
+                if (!file || file.size > MAX_FILE_REQUEST_SIZE) return;
+                isLoading = true;
+                await withFinalizerAsync(
+                  async () => {
+                    const { publicUrl, sasUrl } = await $trpc.room.generateProfileImageUploadUrl.mutate({ roomId });
+                    await uploadBlocks(file, sasUrl);
+                    await $trpc.room.updateRoom.mutate({ id: roomId, image: publicUrl });
+                  },
+                  () => {
+                    isLoading = false;
+                    if (input) input.value = '';
+                  },
+                );
+              }
+            "
+          />
+        </template>
+      </v-hover>
+    </div>
+    <button
+      v-if="image"
+      text-error
+      font-bold
+      type="button"
+      :disabled="isLoading"
+      :op-loading="isLoading ? '' : undefined"
+      hover:underline
+      @click="
+        async () => {
+          if (!image || isLoading) return;
+          isLoading = true;
+          await withFinalizerAsync(
+            async () => {
+              await $trpc.room.deleteProfileImage.mutate({ roomId });
+            },
+            () => {
+              isLoading = false;
+            },
+          );
+        }
+      "
+    >
+      Remove Icon
+    </button>
   </div>
 </template>
