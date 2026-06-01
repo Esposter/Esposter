42c1162627ddf6b71047bfe7b7f708c127a983a2
Q16solver <jimmychen12138@gmail.com>
2026-06-01 21:51:01 +1000

    fix: vite

--- packages/configuration/src/getViteConfiguration.ts
+++ packages/configuration/src/getViteConfiguration.ts
@@ -2,7 +2,7 @@ import type { UserConfig } from "vite";
 
 import vue from "@vitejs/plugin-vue";
 import AutoImport from "unplugin-auto-import/rolldown";
-import dts from "unplugin-dts/rolldown";
+import dts from "unplugin-dts/vite";
 import mkcert from "vite-plugin-mkcert";
 
 import { external } from "./external/external";
