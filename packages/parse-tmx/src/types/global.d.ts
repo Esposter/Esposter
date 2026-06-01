a0e84485b2ff8c50b8511a86f71006bbeb71d382
Q16solver <jimmychen12138@gmail.com>
2026-06-01 22:17:37 +1000

    fix: ts6

--- packages/parse-tmx/src/types/global.d.ts
+++
@@ -1,8 +0,0 @@
-declare global {
-  interface Uint8ArrayConstructor {
-    // @TODO: Remove this when TypeScript's bundled lib declares Uint8Array.fromBase64.
-    fromBase64(source: string): Uint8Array;
-  }
-}
-
-export {};
