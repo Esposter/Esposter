ca9ccfae2446211b2e332c9fda708f9b72857dea
Q16solver <jimmychen12138@gmail.com>
2026-06-02 10:37:09 +1000

    fix: types

--- packages/app/app/models/tableEditor/file/dataSource/DataSourceConfiguration.ts
+++ packages/app/app/models/tableEditor/file/dataSource/DataSourceConfiguration.ts
@@ -2,11 +2,11 @@ import type { MimeType } from "#shared/models/file/MimeType";
 import type { DataSource } from "#shared/models/tableEditor/file/datasource/DataSource";
 import type { DataSourceItem } from "#shared/models/tableEditor/file/datasource/DataSourceItem";
 import type { z } from "zod";
-
+/* oxlint-disable typescript/method-signature-style -- method signatures required for bivariant parameter checking so DataSourceConfiguration<Subtype> is assignable to DataSourceConfiguration<DataSourceItem> in the map */
 export interface DataSourceConfiguration<TDataSourceItem extends DataSourceItem> {
   accept: string;
-  deserialize: (file: File, item: TDataSourceItem) => Promise<DataSource>;
+  deserialize(file: File, item: TDataSourceItem): Promise<DataSource>;
   mimeType: MimeType;
   schema: z.ZodObject;
-  serialize: (dataSource: DataSource, item: TDataSourceItem, mimeType: MimeType) => Promise<Blob>;
+  serialize(dataSource: DataSource, item: TDataSourceItem, mimeType: MimeType): Promise<Blob>;
 }
