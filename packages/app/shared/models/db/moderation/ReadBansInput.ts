d634e1d352e15ed52908a1f17286ffc57ad8d2f6
Q16solver <jimmychen12138@gmail.com>
2026-06-04 20:04:23 +1000

    refactor: default sort bys

--- packages/app/shared/models/db/moderation/ReadBansInput.ts
+++ packages/app/shared/models/db/moderation/ReadBansInput.ts
@@ -1,9 +1,13 @@
 import { createCursorPaginationParamsSchema } from "#shared/models/pagination/cursor/CursorPaginationParams";
-import { roomIdSchema } from "@esposter/db-schema";
+import { SortOrder } from "#shared/models/pagination/sorting/SortOrder";
+import { roomIdSchema, selectBanInMessageSchema } from "@esposter/db-schema";
+import { ItemMetadataPropertyNames } from "@esposter/shared";
 import { z } from "zod";
 
 export const readBansInputSchema = z.object({
   ...roomIdSchema.shape,
-  ...createCursorPaginationParamsSchema(z.string(), []).omit({ sortBy: true }).shape,
+  ...createCursorPaginationParamsSchema(selectBanInMessageSchema.keyof(), [
+    { key: ItemMetadataPropertyNames.createdAt, order: SortOrder.Desc },
+  ]).omit({ sortBy: true }).shape,
 });
 export type ReadBansInput = z.infer<typeof readBansInputSchema>;
