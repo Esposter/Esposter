b2219ba18193880d35cd04c5d20f35fd8e47dad0
Q16solver <jimmychen12138@gmail.com>
2026-06-12 23:02:29 +1000

    fix: snapshots

--- packages/azure-mock/src/index.test.ts
+++ packages/azure-mock/src/index.test.ts
@@ -8,11 +8,11 @@ const distDtsFile = resolve(import.meta.dirname, "../dist/index.d.ts");
 describe("azure-mock", () => {
   test("bundle size", () => {
     expect.hasAssertions();
-    expect(getFileSize(distFile)).toMatchInlineSnapshot(`"index.js: 40.58 KB (41549 bytes)"`);
+    expect(getFileSize(distFile)).toMatchInlineSnapshot(`"index.js: 41.50 KB (42492 bytes)"`);
   });
 
   test("types size", () => {
     expect.hasAssertions();
-    expect(getFileSize(distDtsFile)).toMatchInlineSnapshot(`"index.d.ts: 47.08 KB (48211 bytes)"`);
+    expect(getFileSize(distDtsFile)).toMatchInlineSnapshot(`"index.d.ts: 47.08 KB (48210 bytes)"`);
   });
 });
