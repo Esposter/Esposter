b8ab2fae9b7decbee3f3c75efb6be71494a756f9
Jimmy Chen <jimmyc@resonatesolutions.com.au>
2026-06-19 17:55:44 +1000

    fix: remove unnecessary ruleset

--- packages/infra/src/index.test.ts
+++ packages/infra/src/index.test.ts
@@ -8,11 +8,11 @@ const distDtsFile = resolve(import.meta.dirname, "../dist/index.d.ts");
 describe("@esposter/infra", () => {
   test("bundle size", () => {
     expect.hasAssertions();
-    expect(getFileSize(distFile)).toMatchInlineSnapshot(`"index.js: 133.99 KB (137208 bytes)"`);
+    expect(getFileSize(distFile)).toMatchInlineSnapshot(`"index.js: 133.41 KB (136608 bytes)"`);
   });
 
   test("types size", () => {
     expect.hasAssertions();
-    expect(getFileSize(distDtsFile)).toMatchInlineSnapshot(`"index.d.ts: 25.65 KB (26268 bytes)"`);
+    expect(getFileSize(distDtsFile)).toMatchInlineSnapshot(`"index.d.ts: 25.49 KB (26097 bytes)"`);
   });
 });
