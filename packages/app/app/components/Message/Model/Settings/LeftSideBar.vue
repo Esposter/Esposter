0f62fc73399c252422695a7385d39b6c9d7d33f0
Jimmy Chen <jimmyc@resonatesolutions.com.au>
2026-06-23 10:56:40 +1000

    fix: wip

--- packages/app/app/components/Message/Model/Settings/LeftSideBar.vue
+++ packages/app/app/components/Message/Model/Settings/LeftSideBar.vue
@@ -1,13 +1,9 @@
 <script setup lang="ts">
-import { useLayoutStore } from "@/store/layout";
-
 defineSlots<{ default: () => VNode }>();
-const layoutStore = useLayoutStore();
-const { isDesktop } = storeToRefs(layoutStore);
 </script>
 
 <template>
-  <v-navigation-drawer location="left" :rail="!isDesktop" permanent>
+  <v-navigation-drawer location="left" permanent>
     <slot />
   </v-navigation-drawer>
 </template>
--- packages/app/app/components/Message/Model/User/Settings/Content.vue
+++ packages/app/app/components/Message/Model/User/Settings/Content.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import { useSettingsScrollSpy } from "@/composables/message/user/settings/useSettingsScrollSpy";
+import { SETTINGS_HEADER_ID } from "@/services/message/settings/constants";
 import { UserSettingsContentMap } from "@/services/message/user/settings/UserSettingsContentMap";
 import { useUserSettingsDialogStore } from "@/store/message/user/settings/dialog";
 
@@ -16,7 +17,7 @@ useSettingsScrollSpy();
 
 <template>
   <MessageModelSettingsContent>
-    <header mb-4 pb-4 bg-surface flex items-center top-0 justify-between sticky z-1>
+    <header :id="SETTINGS_HEADER_ID" mb-4 pb-4 bg-surface flex items-center top-0 justify-between sticky z-1>
       <div font-bold text-headline-medium>{{ settingsType }}</div>
       <v-tooltip text="Close">
         <template #activator="{ props: tooltipProps }">
