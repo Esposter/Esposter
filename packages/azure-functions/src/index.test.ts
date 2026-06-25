7cd9131241b3ff293449168624a3f7c094084824
Q16solver <jimmychen12138@gmail.com>
2026-06-25 11:35:52 +1000

    fix: snapshot

--- packages/azure-functions/src/index.test.ts
+++ packages/azure-functions/src/index.test.ts
@@ -15,6 +15,6 @@ describe("@esposter/azure-functions", () => {
 
   test.skipIf(process.platform === "win32")("bundle size (POSIX)", () => {
     expect.hasAssertions();
-    expect(getFileSize(distFile)).toMatchInlineSnapshot(`"index.js: 4664.34 KB (4776288 bytes)"`);
+    expect(getFileSize(distFile)).toMatchInlineSnapshot(`"index.js: 4664.12 KB (4776054 bytes)"`);
   });
 });
