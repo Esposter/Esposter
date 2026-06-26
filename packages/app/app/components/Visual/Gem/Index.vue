0a29565cf7e5b12d2145bf432fff526560bf2718
Q16solver <jimmychen12138@gmail.com>
2026-06-26 11:48:55 +1000

    chore + fix: deps

--- packages/app/app/components/Visual/Gem/Index.vue
+++ packages/app/app/components/Visual/Gem/Index.vue
@@ -1,7 +1,14 @@
+<script setup lang="ts">
+import { Vector3 } from "three/webgpu";
+
+const cameraPosition = new Vector3(2, 2, 6);
+const cameraLookAt = new Vector3(0, 0.75, 0);
+</script>
+
 <template>
   <div size-48>
     <TresCanvas :clear-alpha="0" shadows>
-      <TresPerspectiveCamera :far="100" :fov="75" :look-at="[0, 0.75, 0]" :position="[2, 2, 6]" />
+      <TresPerspectiveCamera :far="100" :fov="75" :look-at="cameraLookAt" :position="cameraPosition" />
       <OrbitControls
         :enable-pan="false"
         :enable-zoom="false"
--- packages/app/app/components/Visual/Gem/Scene.vue
+++ packages/app/app/components/Visual/Gem/Scene.vue
@@ -1,8 +1,12 @@
 <script setup lang="ts">
 import type { BufferGeometry, Mesh, MeshBasicMaterial, MeshStandardMaterial } from "three/webgpu";
 
+import { Vector3 } from "three/webgpu";
+
 import { GEM_GLTF_PATH, ROUGHNESS_TEXTURE_PATH } from "@/services/visual/constants";
 
+const keyLightPosition = new Vector3(1, 1, 1);
+const fillLightPosition = new Vector3(-1, -1, -1);
 const { state: gltf } = useGLTF(GEM_GLTF_PATH);
 const { state: roughnessMap } = useTexture(ROUGHNESS_TEXTURE_PATH);
 const gem = computed(
@@ -26,7 +30,7 @@ watch([gem, roughnessMap], ([newGem, newRoughnessMap]) => {
 
 <template>
   <TresAmbientLight :intensity="4" />
-  <TresDirectionalLight :intensity="6" :position="[1, 1, 1]" />
-  <TresDirectionalLight :intensity="6" :position="[-1, -1, -1]" />
+  <TresDirectionalLight :intensity="6" :position="keyLightPosition" />
+  <TresDirectionalLight :intensity="6" :position="fillLightPosition" />
   <primitive v-if="gem" :object="gem" dispose />
 </template>
