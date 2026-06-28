d8372b2020c45b2a5ca8d0e4ff1a72f527189a20
Q16solver <jimmychen12138@gmail.com>
2026-06-28 15:40:59 +1000

    fix: test snapshot

--- packages/virrun/src/index.test.ts
+++ packages/virrun/src/index.test.ts
@@ -8,11 +8,11 @@ const distDtsFile = resolve(import.meta.dirname, "../dist/index.d.ts");
 describe("virrun", () => {
   test("bundle size", () => {
     expect.hasAssertions();
-    expect(getFileSize(distFile)).toMatchInlineSnapshot(`"index.js: 1.95 KB (1994 bytes)"`);
+    expect(getFileSize(distFile)).toMatchInlineSnapshot(`"index.js: 4.77 KB (4887 bytes)"`);
   });
 
   test("types size", () => {
     expect.hasAssertions();
-    expect(getFileSize(distDtsFile)).toMatchInlineSnapshot(`"index.d.ts: 8.58 KB (8783 bytes)"`);
+    expect(getFileSize(distDtsFile)).toMatchInlineSnapshot(`"index.d.ts: 10.85 KB (11107 bytes)"`);
   });
 });
