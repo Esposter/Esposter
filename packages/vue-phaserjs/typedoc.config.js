ae8c975eefa0c621834a8bc23e726a62bade7272
Jimmy Chen <jimmyc@resonatesolutions.com.au>
2026-06-30 17:08:32 +1000

    fix: cleanup configs

--- packages/vue-phaserjs/typedoc.config.js
+++ packages/vue-phaserjs/typedoc.config.js
@@ -1,6 +1,3 @@
-import { config } from "dotenv";
-
-config({ path: "packages/app/.env" });
 /** @satisfies {import('typedoc').TypeDocOptions} */
 const typedocConfiguration = {
   externalSymbolLinkMappings: {
