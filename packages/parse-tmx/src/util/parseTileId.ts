5b8fc29c7c0614f52501e153f8fb8b6e23215048
Jimmy Chen <jimmyc@resonatesolutions.com.au>
2026-07-01 13:33:46 +1000

    fix: remove unnecessary cast

--- packages/parse-tmx/src/util/parseTileId.ts
+++ packages/parse-tmx/src/util/parseTileId.ts
@@ -1,5 +1,5 @@
 import { Flippeds } from "@/models/Flipped";
 
-const FLIPPED_MASK = ~[...Flippeds].reduce((acc, curr) => acc | (curr as number), 0);
+const FLIPPED_MASK = ~[...Flippeds].reduce((acc, curr) => acc | curr, 0);
 
 export const parseTileId = (gid: number): number => gid & FLIPPED_MASK;
