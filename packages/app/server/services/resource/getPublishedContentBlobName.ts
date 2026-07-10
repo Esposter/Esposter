3f2b4eb593f313a62228ef0c376f7f4a9cec197b
Q16solver <jimmychen12138@gmail.com>
2026-07-10 18:22:25 +1000

    fix: json content type

--- packages/app/server/services/resource/getPublishedContentBlobName.ts
+++ packages/app/server/services/resource/getPublishedContentBlobName.ts
@@ -3,4 +3,4 @@ import type { Resource, ResourcePublication } from "@esposter/db-schema";
 export const getPublishedContentBlobName = (
   resourceId: Resource["id"],
   publishVersion: ResourcePublication["publishVersion"],
-) => `${resourceId}/published/${publishVersion}`;
+) => `${resourceId}/published/${publishVersion}.json`;
