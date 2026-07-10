1a341c4e45b737c6550a6c2d1239a6682f0fd0f0
Q16solver <jimmychen12138@gmail.com>
2026-07-10 17:11:47 +1000

    fix: snapshot

--- packages/azure-functions/src/index.test.ts
+++ packages/azure-functions/src/index.test.ts
@@ -7,6 +7,6 @@ const distFile = resolve(import.meta.dirname, "../dist/index.js");
 describe("@esposter/azure-functions", () => {
   test("bundle size", () => {
     expect.hasAssertions();
-    expect(getFileSize(distFile)).toMatchInlineSnapshot(`"index.js: 4541.52 KB (4650521 bytes)"`);
+    expect(getFileSize(distFile)).toMatchInlineSnapshot(`"index.js: 4541.21 KB (4650194 bytes)"`);
   });
 });
