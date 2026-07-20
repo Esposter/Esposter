ba935c539d05bee6423cc71fa3fcab21a3278708
Jimmy Chen <jimmyc@resonatesolutions.com.au>
2026-07-20 15:20:38 +1000

    fix: update snapshots

--- packages/shared-node/src/index.test.ts
+++ packages/shared-node/src/index.test.ts
@@ -8,11 +8,11 @@ const distDtsFile = resolve(import.meta.dirname, "../dist/index.d.ts");
 describe("@esposter/shared-node", () => {
   test("bundle size", () => {
     expect.hasAssertions();
-    expect(getFileSize(distFile)).toMatchInlineSnapshot(`"index.js: 6.54 KB (6698 bytes)"`);
+    expect(getFileSize(distFile)).toMatchInlineSnapshot(`"index.js: 6.66 KB (6818 bytes)"`);
   });
 
   test("types size", () => {
     expect.hasAssertions();
-    expect(getFileSize(distDtsFile)).toMatchInlineSnapshot(`"index.d.ts: 3.67 KB (3758 bytes)"`);
+    expect(getFileSize(distDtsFile)).toMatchInlineSnapshot(`"index.d.ts: 4.48 KB (4590 bytes)"`);
   });
 });
