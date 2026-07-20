93de98d1d88430a847d292137293c8c66ac0f2a7
Jimmy Chen <jimmyc@resonatesolutions.com.au>
2026-07-20 17:19:47 +1000

    fix: types

--- packages/app/app/composables/vuetify/useToggleTheme.ts
+++ packages/app/app/composables/vuetify/useToggleTheme.ts
@@ -4,7 +4,7 @@ export const useToggleTheme = () => {
   const theme = useVTheme();
   const themeCookie = useCookie(THEME_COOKIE_NAME);
   return async (event: MouseEvent) => {
-    theme.setTransitionOrigin(event instanceof window.PointerEvent ? event : null);
+    theme.setTransitionOrigin(event.target as HTMLElement);
     await theme.cycle();
     themeCookie.value = theme.name.value;
   };
