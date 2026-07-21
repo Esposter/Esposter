ebdaee8198895a48c1ab275fd544d45f106f7a33
Jimmy Chen <jimmyc@resonatesolutions.com.au>
2026-07-21 14:41:55 +1000

    fix: snapshots

--- packages/db/src/index.test.ts
+++ packages/db/src/index.test.ts
@@ -8,11 +8,11 @@ const distDtsFile = resolve(import.meta.dirname, "../dist/index.d.ts");
 describe("@esposter/db", () => {
   test("bundle size", () => {
     expect.hasAssertions();
-    expect(getFileSize(distFile)).toMatchInlineSnapshot(`"index.js: 1157.28 KB (1185051 bytes)"`);
+    expect(getFileSize(distFile)).toMatchInlineSnapshot(`"index.js: 1157.89 KB (1185681 bytes)"`);
   });
 
   test("types size", () => {
     expect.hasAssertions();
-    expect(getFileSize(distDtsFile)).toMatchInlineSnapshot(`"index.d.ts: 41.47 KB (42466 bytes)"`);
+    expect(getFileSize(distDtsFile)).toMatchInlineSnapshot(`"index.d.ts: 41.65 KB (42650 bytes)"`);
   });
 });
