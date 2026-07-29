633d3f268b26221a5178efb0b9639480494fc738
Q16solver <jimmychen12138@gmail.com>
2026-07-29 17:45:10 +1000

    fix: snapshots

--- packages/azure-functions/src/index.test.ts
+++ packages/azure-functions/src/index.test.ts
@@ -7,6 +7,6 @@ const distFile = resolve(import.meta.dirname, "../dist/index.js");
 describe("@esposter/azure-functions", () => {
   test("bundle size", () => {
     expect.hasAssertions();
-    expect(getFileSize(distFile)).toMatchInlineSnapshot(`"index.js: 6255.97 KB (6406118 bytes)"`);
+    expect(getFileSize(distFile)).toMatchInlineSnapshot(`"index.js: 6256.11 KB (6406254 bytes)"`);
   });
 });
--- packages/shared/src/index.test.ts
+++ packages/shared/src/index.test.ts
@@ -8,7 +8,7 @@ const distDtsFile = resolve(import.meta.dirname, "../dist/index.d.ts");
 describe("@esposter/shared", () => {
   test("bundle size", () => {
     expect.hasAssertions();
-    expect(getFileSize(distFile)).toMatchInlineSnapshot(`"index.js: 525.89 KB (538516 bytes)"`);
+    expect(getFileSize(distFile)).toMatchInlineSnapshot(`"index.js: 525.98 KB (538600 bytes)"`);
   });
 
   test("types size", () => {
