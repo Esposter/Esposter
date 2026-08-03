8d14ca320c7140c97490877839af7daa36a221d7
Jimmy Chen <jimmyc@resonatesolutions.com.au>
2026-08-04 09:40:15 +1000

    fix: snapshot

--- packages/azure-functions/src/index.test.ts
+++ packages/azure-functions/src/index.test.ts
@@ -7,6 +7,6 @@ const distFile = resolve(import.meta.dirname, "../dist/index.js");
 describe("@esposter/azure-functions", () => {
   test("bundle size", () => {
     expect.hasAssertions();
-    expect(getFileSize(distFile)).toMatchInlineSnapshot(`"index.js: 7207.82 KB (7380809 bytes)"`);
+    expect(getFileSize(distFile)).toMatchInlineSnapshot(`"index.js: 7207.16 KB (7380134 bytes)"`);
   });
 });
